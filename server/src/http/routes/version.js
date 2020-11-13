7c1391a8a862ebb14c8cf25d697cbd16b58792ef
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-13 16:09:41 +0100

    Version sur infra

--- server/src/http/routes/version.js
+++ server/src/http/routes/version.js
@@ -14,9 +14,8 @@ module.exports = () => {
   router.get(
     "/",
     tryCatch(async (req, res) => {
-     // let data = await parseChangelog(path.join(__dirname, '../../../../CHANGELOG.md'))
-      //return res.json(_.get(data, 'versions.0', {}));
-      return res.json(__dirname);
+      let data = await parseChangelog(path.join(__dirname, '../../../CHANGELOG.md'))
+      return res.json(_.get(data, 'versions.0', {}));
     })
   );
 
