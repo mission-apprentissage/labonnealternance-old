c2d1d4586f74e94d71875b627f0a50345c9b2236
David Boureau <bdavidxyz@gmail.com>
2020-11-13 11:03:13 +0100

    test ok

---
+++ server/tests/integration/http/version-test.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+const httpTests = require("../../utils/httpTests");
+const isSemver = require("is-semver");
+
+httpTests(__filename, ({ startServer }) => {
+  
+  it("Vérifie que la route répond", async () => {
+    const { httpClient } = await startServer();
+
+    const response = await httpClient.get("/version");
+
+    assert.strictEqual(response.status, 200);
+  });
+
+  it("Vérifie que la route répond avec une version au format semver", async () => {
+    const { httpClient } = await startServer();
+
+    const response = await httpClient.get("/version");
+
+    assert(isSemver(response.data.version));
+  });
+
+});
