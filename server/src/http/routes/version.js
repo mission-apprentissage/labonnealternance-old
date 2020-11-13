441c764cff983c084348efe62f85d950a61cee6b
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-13 16:02:13 +0100

    Essai recette version

--- server/src/http/routes/version.js
+++ server/src/http/routes/version.js
@@ -14,7 +14,7 @@ module.exports = () => {
   router.get(
     "/",
     tryCatch(async (req, res) => {
-      let data = await parseChangelog(path.join(__dirname, '../../../../CHANGELOG.md'))
+     // let data = await parseChangelog(path.join(__dirname, '../../../../CHANGELOG.md'))
       //return res.json(_.get(data, 'versions.0', {}));
       return res.json(path.join(__dirname, '../../../../CHANGELOG.md'));
     })
