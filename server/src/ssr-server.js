02586844a15b1a533ec571cf9a0aa3d41c3bf243
David Boureau <bdavidxyz@gmail.com>
2020-11-13 18:14:17 +0100

    typo

--- server/src/ssr-server.js
+++ server/src/ssr-server.js
@@ -6,19 +6,19 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
-    .then(() => {
-      const server = express()
+  .then(() => {
+    const server = express()
 
-      server.get('*', (req, res) => {
-        return handle(req, res)
-      })
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
 
-      server.listen(5000, (err) => {
-        if (err) throw err
-          console.log('> Ready on http://localhost:5000')
-      })
+    server.listen(5000, (err) => {
+      if (err) throw err
+        console.log('> Ready on http://localhost:5000')
     })
-    .catch((ex) => {
-      console.error(ex.stack)
-      process.exit(1)
-    })
+  })
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
