21fbc9d8da02d233c13d2d884ac54df8100ab96b
David Boureau <bdavidxyz@gmail.com>
2020-11-13 17:32:04 +0100

    a first draft for nexstjs

--- server/src/http/server.js
+++ server/src/http/server.js
@@ -16,87 +16,83 @@ const job = require("./routes/job");
 const jobV1 = require("./routes/jobV1");
 const jobEtFormationV1 = require("./routes/jobEtFormationV1");
 const rateLimit = require("express-rate-limit");
-
+const { parse } = require('url')
 
 
 
 
 const next = require('next')
-    
+
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const nextHandle = nextApp.getRequestHandler()
 
 
-
-
-
-
-
 module.exports = async (components) => {
-  const { db } = components;
-  const expressServer = express();
+  nextApp.prepare().then(() => {
+    const { db } = components;
+    const expressServer = express();
 
-  Sentry.init({
-    dsn: "https://ca61634412164c598d0c583198eaa62e@o154210.ingest.sentry.io/5417826",
-    integrations: [
+    Sentry.init({
+      dsn: "https://ca61634412164c598d0c583198eaa62e@o154210.ingest.sentry.io/5417826",
+      integrations: [
       // enable HTTP calls tracing
       new Sentry.Integrations.Http({ tracing: true }),
       // enable Express.js middleware tracing
       new Tracing.Integrations.Express({ expressServer }),
-    ],
-    tracesSampleRate: 1.0,
-  });
+      ],
+      tracesSampleRate: 1.0,
+    });
 
   // RequestHandler creates a separate execution context using domains, so that every
   // transaction/span/breadcrumb is attached to its own Hub instance
-  expressServer.use(Sentry.Handlers.requestHandler());
+    expressServer.use(Sentry.Handlers.requestHandler());
   // TracingHandler creates a trace for every incoming request
-  expressServer.use(Sentry.Handlers.tracingHandler());
+    expressServer.use(Sentry.Handlers.tracingHandler());
 
-  expressServer.use(bodyParser.json());
-  expressServer.use(corsMiddleware());
+    expressServer.use(bodyParser.json());
+    expressServer.use(corsMiddleware());
   //expressServer.use(logMiddleware());
 
-  const limiter3PerSecond = rateLimit({
+    const limiter3PerSecond = rateLimit({
     windowMs: 1000, // 1 second
     max: 3, // limit each IP to 3 requests per windowMs
-  });
-  const limiter5PerSecond = rateLimit({
+    });
+    const limiter5PerSecond = rateLimit({
     windowMs: 1000, // 1 second
     max: 5, // limit each IP to 5 requests per windowMs
-  });
-  const limiter10PerSecond = rateLimit({
+    });
+    const limiter10PerSecond = rateLimit({
     windowMs: 1000, // 1 second
     max: 10, // limit each IP to 10 requests per windowMs
-  });
+    });
 
-  const swaggerUi = require("swagger-ui-express");
-  const swaggerDocument = require('../api-docs/swagger.json');
+    const swaggerUi = require("swagger-ui-express");
+    const swaggerDocument = require('../api-docs/swagger.json');
 
-  expressServer.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    expressServer.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-  expressServer.use("/api/formations", limiter5PerSecond, formation());
+    expressServer.use("/api/formations", limiter5PerSecond, formation());
 
-  expressServer.use("/api/jobs", limiter3PerSecond, job());
+    expressServer.use("/api/jobs", limiter3PerSecond, job());
 
-  expressServer.use("/api/v1/formations", limiter5PerSecond, formationV1());
+    expressServer.use("/api/v1/formations", limiter5PerSecond, formationV1());
 
-  expressServer.use("/api/v1/formationsParRegion", limiter5PerSecond, formationRegionV1());
+    expressServer.use("/api/v1/formationsParRegion", limiter5PerSecond, formationRegionV1());
 
-  expressServer.use("/api/v1/jobs", limiter3PerSecond, jobV1());
+    expressServer.use("/api/v1/jobs", limiter3PerSecond, jobV1());
 
-  expressServer.use("/api/v1/jobsEtFormations", limiter3PerSecond, jobEtFormationV1());
+    expressServer.use("/api/v1/jobsEtFormations", limiter3PerSecond, jobEtFormationV1());
 
-  expressServer.use("/api/jobsdiplomas", limiter10PerSecond, jobDiploma());
+    expressServer.use("/api/jobsdiplomas", limiter10PerSecond, jobDiploma());
 
-  expressServer.use("/api/romelabels", limiter10PerSecond, rome());
+    expressServer.use("/api/romelabels", limiter10PerSecond, rome());
 
-  expressServer.get(
-    "/api",
-    tryCatch(async (req, res) => {
-      let mongodbStatus;
-      await db
+    expressServer.get(
+      "/api",
+      tryCatch(async (req, res) => {
+        let mongodbStatus;
+        await db
         .collection("sampleEntity")
         .stats()
         .then(() => {
@@ -107,28 +103,46 @@ module.exports = async (components) => {
           logger.error("Healthcheck failed", e);
         });
 
-      return res.json({
-        name: `Serveur MNA - ${config.appName}`,
-        version: packageJson.version,
-        env: config.env,
-        healthcheck: {
-          mongodb: mongodbStatus,
-        },
-      });
-    })
-  );
+        return res.json({
+          name: `Serveur MNA - ${config.appName}`,
+          version: packageJson.version,
+          env: config.env,
+          healthcheck: {
+            mongodb: mongodbStatus,
+          },
+        });
+      })
+      );
 
-  expressServer.get(
-    "/api/config",
-    tryCatch(async (req, res) => {
-      return res.json({
-        config: {
-          ...config,
-          private: "",
-        },
-      });
-    })
-  );
+    expressServer.get(
+      "/api/config",
+      tryCatch(async (req, res) => {
+        return res.json({
+          config: {
+            ...config,
+            private: "",
+          },
+        });
+      })
+      );
 
-  return expressServer;
+    expressServer.get('*', (req,res) => {
+      return nextHandle(req,res) // for all the react stuff
+    })
+
+  // expressServer.get('/pages/:id', (req, res) => {
+  //   const params = route('/pages/:id')(parse(req.url).pathname);
+  //   return nextApp.render(req, res, '/pages', params);
+  // });
+
+  // expressServer.get('*', (req, res) => {
+  //   // nextApp.render(req, res, '/pages/home', {});
+  //   const parsedUrl = parse(req.url, true)
+  //   console.log(parsedUrl)
+  //   return nextHandle(req, res, parsedUrl)
+  // });
+
+    return expressServer;
+  })
 };
+
