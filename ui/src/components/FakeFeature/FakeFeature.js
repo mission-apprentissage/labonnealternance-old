6c2ccf2dc67d338919b55e60d5f6d96322a6dd74
Valerie Montoya <valeriemontoya90@gmail.com>
2020-11-16 18:22:12 +0100

    fakeFeature : set color, text and scenario

--- ui/src/components/FakeFeature/FakeFeature.js
+++ ui/src/components/FakeFeature/FakeFeature.js
@@ -2,9 +2,19 @@ import React, { useState } from "react";
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
 import "./FakeFeature.css";
 
-const FakeFeature = ({ buttonText, tagName, modalTitle, modalText, questionsAndTags }) => {
+const FakeFeature = ({
+  buttonText,
+  tagName,
+  modalTitleBeforeSelection,
+  modalTextBeforeSelection,
+  modalTitleAfterSelection,
+  modalTextAfterSelection,
+  questionsAndTags,
+  isOptionSelected,
+  setIsOptionSelected,
+}) => {
   const handleClick = () => {
-    toggle();
+    setIsOptionSelected(true);
   };
 
   const [modal, setModal] = useState(false);
@@ -19,20 +29,30 @@ const FakeFeature = ({ buttonText, tagName, modalTitle, modalText, questionsAndT
         </button>
       </div>
       <Modal isOpen={modal} toggle={toggle} backdrop="static" className="avenirModale">
-        <ModalHeader toggle={toggle}>{modalTitle}</ModalHeader>
-        {questionsAndTags ? (
-          <ModalBody>
-            {modalText}
-            {questionsAndTags.map((question, idx) => (
-              <div key={idx}>
-                <Button color="primary" className={`${question.tagName} question`} onClick={toggle}>
-                  {question.question}
-                </Button>
-              </div>
-            ))}
-          </ModalBody>
-        ) : (
-          ""
+        {isOptionSelected && (
+          <>
+            <ModalHeader toggle={toggle}>{modalTitleAfterSelection}</ModalHeader>
+            <ModalBody>{modalTextAfterSelection}</ModalBody>
+          </>
+        )}
+        {!isOptionSelected && (
+          <>
+            <ModalHeader toggle={toggle}>{modalTitleBeforeSelection}</ModalHeader>
+            {questionsAndTags ? (
+              <ModalBody>
+                {modalTextBeforeSelection}
+                {questionsAndTags.map((question, idx) => (
+                  <div key={idx}>
+                    <Button color="primary" className={`${question.tagName} question`} onClick={toggle}>
+                      {question.question}
+                    </Button>
+                  </div>
+                ))}
+              </ModalBody>
+            ) : (
+              ""
+            )}
+          </>
         )}
         <ModalFooter>
           <Button color="secondary" onClick={toggle}>
