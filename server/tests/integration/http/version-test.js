f44d417fb56e94e77c275050f64545b454c07cc1
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-17 12:10:59 +0100

    Correction tests /api/version

--- server/tests/integration/http/version-test.js
+++ server/tests/integration/http/version-test.js
@@ -7,7 +7,7 @@ httpTests(__filename, ({ startServer }) => {
   it("Vérifie que la route répond", async () => {
     const { httpClient } = await startServer();
 
-    const response = await httpClient.get("/version");
+    const response = await httpClient.get("/api/version");
 
     assert.strictEqual(response.status, 200);
   });
@@ -15,7 +15,7 @@ httpTests(__filename, ({ startServer }) => {
   it("Vérifie que la route répond avec une version au format semver", async () => {
     const { httpClient } = await startServer();
 
-    const response = await httpClient.get("/version");
+    const response = await httpClient.get("/api/version");
 
     assert(isSemver(response.data.version));
   });
