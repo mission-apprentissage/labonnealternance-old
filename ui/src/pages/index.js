a62707b43f6a6575f79416d57dc59502e2ef5bce
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-19 15:46:41 +0100

    Suppression page obsolète apiTester

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -1,6 +1,5 @@
 import NotFound from "./NotFound";
-import ApiTester from "./ApiTester";
 import WidgetTester from "./WidgetTester";
 import SearchForTrainingsAndJobs from "./SearchForTrainingsAndJobs";
 
-export { NotFound, ApiTester, WidgetTester, SearchForTrainingsAndJobs };
+export { NotFound, WidgetTester, SearchForTrainingsAndJobs };
