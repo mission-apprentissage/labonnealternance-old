6da94b0fa5fac67c9da714e515e3e2c86879f554
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-23 17:49:13 +0100

    Fonction utilitaire détection si origin requête est interne LBA

---
+++ server/src/common/utils/isOriginLocal.js
@@ -0,0 +1,11 @@
+const localOrigin = [
+  "https://labonnealternance-recette.beta.gouv.fr",
+  "https://labonnealternance.beta.gouv.fr",
+  "http://localhost:3003",
+];
+
+const isOriginLocal = (origin) => {
+  return localOrigin.indexOf(origin) >= 0;
+};
+
+module.exports = { isOriginLocal };
---
+++ server/tests/unit/util-test.js
@@ -0,0 +1,24 @@
+const assert = require("assert");
+const { isOriginLocal } = require("../../src/common/utils/isOriginLocal");
+
+describe(__filename, () => {
+  it("Détection origine autorisée - retourne false si undefined ", () => {
+    const result = isOriginLocal(undefined);
+    assert.strictEqual(result, false);
+  });
+
+  it("Détection origine autorisée - retourne false si origine inconnue ", () => {
+    const result = isOriginLocal("fauxDomaine");
+    assert.strictEqual(result, false);
+  });
+
+  it("Détection origine autorisée - retourne true si origine connue localhost ", () => {
+    const result = isOriginLocal("http://localhost:3003");
+    assert.strictEqual(result, true);
+  });
+
+  it("Détection origine autorisée - retourne true si origine connue labonnealternance.apprentissage.beta.gouv.fr", () => {
+    const result = isOriginLocal("https://labonnealternance.beta.gouv.fr");
+    assert.strictEqual(result, true);
+  });
+});
