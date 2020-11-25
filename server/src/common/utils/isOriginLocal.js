217b79a3b854fd5f11087ba8b962849838d1fe6b
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-25 14:02:49 +0100

    Hotfix détection origin via referer

--- server/src/common/utils/isOriginLocal.js
+++ server/src/common/utils/isOriginLocal.js
@@ -1,11 +1,11 @@
 const localOrigin = [
-  "https://labonnealternance-recette.apprentissage.beta.gouv.fr/",
-  "https://labonnealternance.apprentissage.beta.gouv.fr/",
-  "http://localhost:3003/",
+  "https://labonnealternance-recette.apprentissage.beta.gouv.fr",
+  "https://labonnealternance.apprentissage.beta.gouv.fr",
+  "http://localhost:3003",
 ];
 
 const isOriginLocal = (origin) => {
-  return localOrigin.indexOf(origin) >= 0;
+  return localOrigin.findIndex((element) => origin.includes(element));
 };
 
 module.exports = { isOriginLocal };
