ba2b58053b461fc8f8870a132bfb99ece9433f6a
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-25 17:25:09 +0100

    Hotfix isOriginLocal (#40)

--- server/src/common/utils/isOriginLocal.js
+++ server/src/common/utils/isOriginLocal.js
@@ -1,11 +1,11 @@
 const localOrigin = [
-  "https://labonnealternance-recette.apprentissage.beta.gouv.fr/",
-  "https://labonnealternance.apprentissage.beta.gouv.fr/",
-  "http://localhost:3003/",
+  "https://labonnealternance-recette.apprentissage.beta.gouv.fr",
+  "https://labonnealternance.apprentissage.beta.gouv.fr",
+  "http://localhost:3003",
 ];
 
 const isOriginLocal = (origin) => {
-  return localOrigin.indexOf(origin) >= 0;
+  return origin ? localOrigin.findIndex((element) => origin.includes(element)) >= 0 : false;
 };
 
 module.exports = { isOriginLocal };
