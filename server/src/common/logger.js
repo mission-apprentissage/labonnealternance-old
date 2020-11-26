898f1864c395296eeeaf40c5acd476e00442e4d9
Alan Le Ruyet <alan.leruyet@free.fr>
2020-11-26 14:16:44 +0100

    Branchement winston sur sentry et sur deux fichiers de log

--- server/src/common/logger.js
+++ server/src/common/logger.js
@@ -1,79 +1,43 @@
+const winston = require("winston");
+const { combine, timestamp, printf } = winston.format;
 const config = require("config");
-const util = require("util");
-const bunyan = require("bunyan");
-const PrettyStream = require("bunyan-prettystream");
-const BunyanSlack = require("bunyan-slack");
-const BunyanMongodbStream = require("bunyan-mongodb-stream");
-const { Log } = require("./model");
+const Sentry = require("winston-transport-sentry-node").default;
 
-const createStreams = () => {
-  const { type, level } = config.log;
-  const envName = config.env;
+const createLogger = () => {
+  const lbaFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level.toUpperCase()}: ${message}`;
+  });
 
-  const jsonStream = () => {
-    return {
-      name: "json",
-      level,
-      stream: process.stdout,
-    };
+  const sentryOptions = {
+    sentry: {
+      dsn: "https://ca61634412164c598d0c583198eaa62e@o154210.ingest.sentry.io/5417826",
+    },
+    level: "error",
   };
 
-  const consoleStream = () => {
-    const pretty = new PrettyStream();
-    pretty.pipe(process.stdout);
-    return {
-      name: "console",
-      level,
-      stream: pretty,
-    };
-  };
+  const logger = winston.createLogger({
+    level: "info",
+    format: combine(timestamp(), lbaFormat),
+    transports: [
+      //
+      // - Write all logs with level `error` and below to `error.log`
+      // - Write all logs with level `info` and below to `combined.log`
+      //
+      new winston.transports.File({ filename: "error.log", level: "error" }),
+      new winston.transports.File({ filename: "combined.log" }),
+      new Sentry(sentryOptions),
+    ],
+  });
 
-  const mongoDBStream = () => {
-    return {
-      name: "mongodb",
-      level,
-      stream: BunyanMongodbStream({ model: Log }),
-    };
-  };
-
-  const slackStream = () => {
-    const stream = new BunyanSlack(
-      {
-        webhook_url: config.slackWebhookUrl,
-        customFormatter: (record, levelName) => {
-          if (record.type === "http") {
-            record = {
-              url: record.request.url.relative,
-              statusCode: record.response.statusCode,
-              ...(record.error ? { message: record.error.message } : {}),
-            };
-          }
-          return {
-            text: util.format(`[%s][${envName}] %O`, levelName.toUpperCase(), record),
-          };
-        },
-      },
-      (error) => {
-        console.log("Unable to send log to slack", error);
-      }
-    );
-
-    return {
-      name: "slack",
-      level: "error",
-      stream,
-    };
-  };
-
-  const streams = [type === "console" ? consoleStream() : jsonStream(), mongoDBStream()];
-  if (config.slackWebhookUrl) {
-    streams.push(slackStream());
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  //
+  if (config.env === "local") {
+    logger.add(new winston.transports.Console());
   }
-  return streams;
+
+  return logger;
 };
 
-module.exports = bunyan.createLogger({
-  name: config.appName,
-  serializers: bunyan.stdSerializers,
-  streams: createStreams(),
-});
+module.exports = createLogger();
