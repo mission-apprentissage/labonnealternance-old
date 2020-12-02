acc639536918eddc612795741a3a3bb449a22392
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-02 10:32:34 +0100

    Retrait isTrainingOnly

--- ui/basic/pages/recherche-apprentissage.js
+++ ui/basic/pages/recherche-apprentissage.js
@@ -8,7 +8,7 @@ const RechercheApprentissage = () => (
     <Head>
       <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
     </Head>
-    <SearchForTrainingsAndJobs isTrainingOnly='false' />
+    <SearchForTrainingsAndJobs />
   </>
 )
 
