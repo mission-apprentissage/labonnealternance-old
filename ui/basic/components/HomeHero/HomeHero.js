50316bfb8ac2e0c21920d98533715451cfd660de
David Boureau <bdavidxyz@gmail.com>
2020-12-04 17:19:10 +0100

    loadable fonts

--- ui/basic/components/HomeHero/HomeHero.js
+++ ui/basic/components/HomeHero/HomeHero.js
@@ -6,6 +6,16 @@ import styles from "./HomeHero.module.scss";
 const HomeHero = () => {
   return (
       <div className="c-home-hero">
+        <div className="c-home-hero__card">
+          <div className="card-body">
+            <h1 className="card-title">
+              <span className="d-block c-home-hero__title c-home-hero__title1">Se former et travailler</span>
+              <span className="d-block c-home-hero__title c-home-hero__title2">En alternance</span>
+            </h1>
+            <p className="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
         <p>
           1) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
