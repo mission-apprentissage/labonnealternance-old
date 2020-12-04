fa965f2cb489619ebaaf705622f41a82b2a46d8e
David Boureau <bdavidxyz@gmail.com>
2020-12-04 17:43:08 +0100

    wip colors

--- ui/basic/components/HomeHero/HomeHero.js
+++ ui/basic/components/HomeHero/HomeHero.js
@@ -6,112 +6,28 @@ import styles from "./HomeHero.module.scss";
 const HomeHero = () => {
   return (
       <div className="c-home-hero">
-        <div className="c-home-hero__card">
-          <div className="card-body">
-            <h1 className="card-title">
-              <span className="d-block c-home-hero__title c-home-hero__title1">Se former et travailler</span>
-              <span className="d-block c-home-hero__title c-home-hero__title2">En alternance</span>
-            </h1>
-            <p className="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
-            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+
+        <div className="container py-5">
+          <div className="card c-home-hero__card px-4 py-4">
+            <div className="card-body">
+              <h1 className="card-title">
+                <span className="d-block c-home-hero__title c-home-hero__title1">Se former et travailler</span>
+                <span className="d-block c-home-hero__title c-home-hero__title2">En alternance</span>
+              </h1>
+              <p className="card-text mb-5">
+                <span className="d-block">Trouvez la formation et l’entreprise pour</span>
+                <span className="d-block">réaliser votre projet d'alternance</span>
+              </p>
+              <div class="form-group mx-auto" className="c-home-hero__input">
+                <input type="text" required="required" value="" class="form-control form-control-lg" aria-describedby="name" placeholder="ex : boulanger" />
+              </div>
+            </div>
           </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugiat quasi, voluptatum error sequi corrupti, molestiae in at dolorum totam, atque placeat nulla sunt reiciendis hic saepe ab, numquam veniam!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugiat quasi, voluptatum error sequi corrupti, molestiae in at dolorum totam, atque placeat nulla sunt reiciendis hic saepe ab, numquam veniam!</p>
+
         </div>
-        <p>
-          1) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          2) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          3) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          4) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          5) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          6) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          7) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          8) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          9) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          10) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          11) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
-        <p>
-          12) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.          
-        </p>
       </div>
   );
 };
