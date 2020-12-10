48d9aee4ee480e860d5f6922cd0d28de8f97060e
David Boureau <bdavidxyz@gmail.com>
2020-12-10 11:54:01 +0100

    wip

--- ui/basic/components/DescriptionMissionApprentissage/DescriptionMissionApprentissage.js
+++ ui/basic/components/DescriptionMissionApprentissage/DescriptionMissionApprentissage.js
@@ -3,11 +3,11 @@ import styles from "./DescriptionMissionApprentissage.module.scss";
 
 const DescriptionMissionApprentissage = () => {
   return (
-    <section className="c-home-recall text-center">
+    <section className="c-home-descr text-center">
       <div className="container py-5">
-        <h2 className="c-home-recall__title">La mission apprentissage</h2>
-        <p className="c-home-recall__subtitle">Faciliter les entrées en apprentissage</p>
-        <p className="c-home-recall__text">
+        <h2 className="c-home-descr__title pt-5">La mission apprentissage</h2>
+        <p className="c-home-descr__subtitle">Faciliter les entrées en apprentissage</p>
+        <p className="c-home-descr__text mx-auto pt-4 pb-5">
           La Mission apprentissage vise à lever les freins à l'essor de l'apprentissage et faciliter la mise en contact
           des jeunes, des CFA et des entreprises. Elle est conjointement engagée par le ministère de l' Education
           nationale, le ministère de l'Enseignement supérieur, de la Recherche et de l'Innovation, le ministère du
@@ -15,7 +15,8 @@ const DescriptionMissionApprentissage = () => {
           nationale pour l'emploi et la transition écologique.
         </p>
 
-        <a>En savoir plus sur la mission apprentissage</a>
+        <a className="btn btn-outline-primary c-home-descr__more mb-5">En savoir plus sur la mission apprentissage</a>
+        <div className="mb-5">&nbsp;</div>
       </div>
     </section>
   );
