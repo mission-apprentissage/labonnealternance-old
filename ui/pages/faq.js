b19e2b2634a4966a4d1b644a2d9bd4f75e94ae7b
David Boureau <bdavidxyz@gmail.com>
2020-12-11 11:54:28 +0100

    first question

--- ui/pages/faq.js
+++ ui/pages/faq.js
@@ -5,7 +5,22 @@ import Footer from "components/footer";
 const FAQ = () => (
   <div>
     <Navigation />
-    FAQ
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5">
+            <h1>
+              <span className="d-block c-page-title is-color-1">Questions</span>
+              <span className="d-block c-page-title is-color-2">fréquemment</span>
+              <span className="d-block">posées</span>
+            </h1>
+            <hr/>
+          </div>
+          <div className="col-12 col-md-7">
+            <h2 className="c-faq-question">Il manque des entreprises dans la liste.</h2>
+            <p className="c-faq-answer">La Bonne Alternance est bien plus qu’un simple annuaire. La Bonne Alternance effectue un ciblage spécifique des entreprises à fort potentiel d’embauche afin de vous faire gagner du temps dans la sélection des entreprises à démarcher. Toutes les entreprises ne sont donc pas indiquées.</p>
+          </div>
+        </div>
+      </div>
     <Footer />
   </div>
 );
