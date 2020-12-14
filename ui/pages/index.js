56922e72cd36c7b7ed630723ceb8acfea4356226
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-14 17:14:30 +0100

    Initialisation paramètres widget

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "components/navigation";
 import HomeHero from "components/HomeHero";
-import { getWidgetParameters, isTrainingOnly } from "services/config"
+import { getWidgetParameters } from "services/config";
 //import ServicesMissionApprentissage from "components/ServicesMissionApprentissage";
 import DescriptionMissionApprentissage from "components/DescriptionMissionApprentissage";
 import Footer from "components/footer";
-const Home = ({a}) =>{ 
-  
-  
-  console.log("aaa ",a);
+import { push } from "connected-next-router";
+import { useDispatch } from "react-redux";
+import { setWidgetParameters } from "store/actions";
+
+const Home = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (props.applyWidgetParameters) {
+      dispatch(setWidgetParameters(props));
+      dispatch(push({ pathname: "/recherche-apprentissage" }));
+    }
+  }, []);
 
   return (
-  <div>
-    <Navigation />
-    <HomeHero />
-    <DescriptionMissionApprentissage />
-    <Footer />
-  </div>
-);}
+    <div>
+      <Navigation />
+      <HomeHero />
+      <DescriptionMissionApprentissage />
+      <Footer />
+    </div>
+  );
+};
 
-// add getStaticProps() function
 export async function getServerSideProps({ query }) {
-  const category = query.category;
-  console.log("cat : ",query, category);
-  //console.log("context : ", context.query);
-
-  return {props:{a:5}};
+  return { props: getWidgetParameters(query) };
 }
 
 export default Home;
