f8652940f6b561998306c8a9bcee0bd4967af9a2
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-14 17:40:06 +0100

    Récupération des paramètres pour next sur /recherche-apprentissage

--- ui/pages/recherche-apprentissage.js
+++ ui/pages/recherche-apprentissage.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchForTrainingsAndJobs from "../components/SearchForTrainingsAndJobs";
+import { useDispatch } from "react-redux";
+import { setWidgetParameters } from "store/actions";
+import { getWidgetParameters } from "services/config";
 
 import Head from "next/head";
 
-const RechercheApprentissage = () => (
-  <>
-    <Head>
-      <link href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css" rel="stylesheet" />
-    </Head>
-    <SearchForTrainingsAndJobs />
-  </>
-);
+const RechercheApprentissage = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (props && props.applyWidgetParameters) {
+      dispatch(setWidgetParameters(props));
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css" rel="stylesheet" />
+      </Head>
+      <SearchForTrainingsAndJobs />
+    </>
+  );
+};
+
+export async function getServerSideProps({ query }) {
+  return { props: getWidgetParameters(query) };
+}
 
 export default RechercheApprentissage;
