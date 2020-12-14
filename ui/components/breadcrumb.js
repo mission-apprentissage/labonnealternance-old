d57dde57f56e3d2e717b41c5f8e826105dfc78d0
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-14 09:35:31 +0100

    Passage en props de forName et label

--- ui/components/breadcrumb.js
+++ ui/components/breadcrumb.js
@@ -1,35 +1,24 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
 
 import separator from "public/images/breadcrumb_separator.svg";
 
-
-const Breadcrumb = (props) => {
-  let suffix = <div></div>
-
-  if (props.forPage === 'faq') {
-    suffix = <Link href={{ pathname: '/faq' }}>FAQ</Link>
-  } else if (props.forPage === 'organisme') {
-    suffix = <Link href={{ pathname: '/organisme' }}>Organisme de formation</Link>
-  }
+const Breadcrumb = ({ forPage, label }) => {
+  let suffix = forPage ? <Link href={{ pathname: `/${forPage}` }}>{label}</Link> : <div></div>;
 
   return (
     <div className="c-breadcrumb d-none d-sm-block">
       <div className="container d-flex pl-0 pt-5 pb-4">
-
-        <Link href={{ pathname: '/' }}>
-          <a>Accueil</a>
-        </Link>
+        <Link href={{ pathname: "/" }}>Accueil</Link>
 
         <div className="c-breadcrumb-separator mx-3">
-          <img className="c-breadcrumb-separator-img" src={separator} alt="Separateur du fil d'ariane" />
+          <img className="c-breadcrumb-separator-img" src={separator} alt="" />
         </div>
 
         {suffix}
-
       </div>
     </div>
   );
-}
+};
 
 export default Breadcrumb;
