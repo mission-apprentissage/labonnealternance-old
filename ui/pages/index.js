92e2bae843203e81a1b22bf25793c4b703329d8d
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-14 09:03:39 +0100

    Essai recup de params

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Navigation from "components/navigation";
 import HomeHero from "components/HomeHero";
-import ServicesMissionApprentissage from "components/ServicesMissionApprentissage";
+import { getWidgetParameters, isTrainingOnly } from "services/config"
+//import ServicesMissionApprentissage from "components/ServicesMissionApprentissage";
 import DescriptionMissionApprentissage from "components/DescriptionMissionApprentissage";
 import Footer from "components/footer";
-const Home = () => (
+const Home = ({a}) =>{ 
+  
+  
+  console.log("aaa ",a);
+
+  return (
   <div>
     <Navigation />
     <HomeHero />
     <DescriptionMissionApprentissage />
     <Footer />
   </div>
-);
+);}
+
+// add getStaticProps() function
+export async function getServerSideProps({ query }) {
+  const category = query.category;
+  console.log("cat : ",query, category);
+  //console.log("context : ", context.query);
+
+  return {props:{a:5}};
+}
 
 export default Home;
