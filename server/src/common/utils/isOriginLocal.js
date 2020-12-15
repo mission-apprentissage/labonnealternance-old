8ab0647f5bb495c53a8de406eff46faab90151d6
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-15 01:19:48 +0100

    Ajout des urls de PE pour les apis

--- server/src/common/utils/isOriginLocal.js
+++ server/src/common/utils/isOriginLocal.js
@@ -1,6 +1,8 @@
 const localOrigin = [
   "https://labonnealternance-recette.apprentissage.beta.gouv.fr",
   "https://labonnealternance.apprentissage.beta.gouv.fr",
+  "https://labonnealternance.beta.pole-emploi.fr",
+  "https://labonnealternance.pole-emploi.fr",
   "http://localhost:3003",
   "http://localhost:3000", //TODO: temporaire jusqu'à suppression de :3000
 ];
