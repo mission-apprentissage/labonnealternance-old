fbd9f2a3e46749b258c44d26a72d3620bf3b5b56
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-15 12:11:54 +0100

    scrollToTop sur la faq

--- ui/pages/faq.js
+++ ui/pages/faq.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "components/navigation";
 import Breadcrumb from "components/breadcrumb";
+import { scrollToTop } from "utils/tools";
 
 import Footer from "components/footer";
 
 const FAQ = (props) => {
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
   return (
     <div>
       <Navigation bgcolor="is-white" />
