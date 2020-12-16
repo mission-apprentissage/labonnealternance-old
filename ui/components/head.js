28185c9611742066f3ce6af94ae94ae2e25614e3
David Boureau <bdavidxyz@gmail.com>
2020-12-16 17:32:47 +0100

    un-hardcoded GTM ID

--- ui/components/head.js
+++ ui/components/head.js
@@ -11,7 +11,7 @@ const HeadLaBonneAlternance = (props) => {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', 'GTM-T9G5QSC');`,
+        })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');`,
         }}
       />
       {/* End Google Tag Manager */}
