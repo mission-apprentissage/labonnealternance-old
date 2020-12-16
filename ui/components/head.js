36769123724d8e41bc2b4ddecdfa1effe25b5e6a
David Boureau <bdavidxyz@gmail.com>
2020-12-16 17:34:22 +0100

    hidden in noframe

--- ui/components/head.js
+++ ui/components/head.js
@@ -18,7 +18,7 @@ const HeadLaBonneAlternance = (props) => {
       {/* Google Tag Manager (noscript) */}
       <noscript
         dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9G5QSC" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
       }}
       />
       {/* End Google Tag Manager (noscript) */}
