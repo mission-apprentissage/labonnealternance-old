4ccba7b7c0ae20a1b192063659113758c685a0d3
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-17 10:17:06 +0100

    Favicon

--- ui/components/head.js
+++ ui/components/head.js
@@ -19,27 +19,34 @@ const HeadLaBonneAlternance = (props) => {
       <noscript
         dangerouslySetInnerHTML={{
           __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-      }}
+        }}
       />
       {/* End Google Tag Manager (noscript) */}
-      
+
       <title>La Bonne Alternance | Trouvez votre alternance</title>
       <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
       <link rel="canonical" href="http://labonnealternance.pole-emploi.fr" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
       <meta id="robots-meta" name="robots" content="index, follow" />
       <meta name="keywords" content="contrat offres alternance" />
       <meta
         name="description"
         content="Vous ne trouvez pas de contrat ou d'offres d'alternance ? Essayez La Bonne Alternance ! Trouvez ici les formations en alternance et les entreprises qui recrutent régulièrement en alternance"
-        />
-        <meta property="og:site_name" content="La Bonne Alternance" />
-        <meta property="og:title" content="La Bonne Alternance - Trouvez votre alternance" />
-        <meta property="og:type" content="site" />
-        <meta property="og:url" content="https://labonnealternance.pole-emploi.fr" />
-        <meta
-          property="og:description"
-          content="Vous ne trouvez pas de contrat ou d'offres d'alternance ? Essayez La Bonne Alternance ! Trouvez ici les formations en alternance et les entreprises qui recrutent régulièrement en alternance"
-          />
+      />
+      <meta property="og:site_name" content="La Bonne Alternance" />
+      <meta property="og:title" content="La Bonne Alternance - Trouvez votre alternance" />
+      <meta property="og:type" content="site" />
+      <meta property="og:url" content="https://labonnealternance.pole-emploi.fr" />
+      <meta
+        property="og:description"
+        content="Vous ne trouvez pas de contrat ou d'offres d'alternance ? Essayez La Bonne Alternance ! Trouvez ici les formations en alternance et les entreprises qui recrutent régulièrement en alternance"
+      />
     </Head>
   );
 };
