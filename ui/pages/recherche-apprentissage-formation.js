8f38ab99c03e610bf572344e8410130fc58ef3ce
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-17 16:26:49 +0100

    Fix paramètre formation seulement

--- ui/pages/recherche-apprentissage-formation.js
+++ ui/pages/recherche-apprentissage-formation.js
@@ -1,15 +1,16 @@
-import Navigation from '../components/navigation'
-import SearchForTrainingsAndJobs from '../components/SearchForTrainingsAndJobs'
+import React from "react";
+import Navigation from "../components/navigation";
+import SearchForTrainingsAndJobs from "../components/SearchForTrainingsAndJobs";
 
-import Head from 'next/head';
+import Head from "next/head";
 
 const RechercheApprentissageFormation = () => (
   <>
     <Head>
-      <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
+      <link href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css" rel="stylesheet" />
     </Head>
-    <SearchForTrainingsAndJobs isTrainingOnly="true" />
+    <SearchForTrainingsAndJobs isTrainingOnly="1" />
   </>
-)
+);
 
-export default RechercheApprentissageFormation
+export default RechercheApprentissageFormation;
