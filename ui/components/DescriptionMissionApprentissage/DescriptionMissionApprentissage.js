9fb2bde67a61387c486cfeecf547bcbf8b7258da
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-17 15:37:02 +0100

    Aération texte MNA

--- ui/components/DescriptionMissionApprentissage/DescriptionMissionApprentissage.js
+++ ui/components/DescriptionMissionApprentissage/DescriptionMissionApprentissage.js
@@ -8,7 +8,7 @@ const DescriptionMissionApprentissage = () => {
         <p className="c-home-descr__subtitle">Faciliter les entrées en apprentissage</p>
         <p className="c-home-descr__text mx-auto pt-4 pb-5">
           La Mission apprentissage vise à lever les freins à l'essor de l'apprentissage et faciliter la mise en contact
-          des jeunes, des CFA et des entreprises. Elle est conjointement engagée par le ministère de l' Education
+          des jeunes, des CFA et des entreprises.<br />Elle est conjointement engagée par le ministère de l' Education
           nationale, le ministère de l'Enseignement supérieur, de la Recherche et de l'Innovation, le ministère du
           Travail et le ministère de la Transformation et de la Fonction publique, dans le cadre de la mobilisation
           nationale pour l'emploi et la transition écologique.
