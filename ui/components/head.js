7c313dc264d11e5c5401991269842af457e6f0e9
Alan Le Ruyet <alan.leruyet@free.fr>
2020-12-17 11:36:23 +0100

    GTM ID en dur

--- ui/components/head.js
+++ ui/components/head.js
@@ -11,14 +11,14 @@ const HeadLaBonneAlternance = (props) => {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');`,
+        })(window,document,'script','dataLayer', 'GTM-T9G5QSC');`,
         }}
       />
       {/* End Google Tag Manager */}
       {/* Google Tag Manager (noscript) */}
       <noscript
         dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9G5QSC" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         }}
       />
       {/* End Google Tag Manager (noscript) */}
