c557d612c9d2f9d781b78fc98f4ac088634b7db3
David Boureau <bdavidxyz@gmail.com>
2020-12-18 15:11:12 +0100

    skeleton

---
+++ ui/components/HomeServices/HomeHero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeServices = () => {
+  return (
+    <section className="c-home-services">
+      <div className="container">
+        Services
+      </div>
+    </section>
+  );
+};
+
+export default HomeServices;
