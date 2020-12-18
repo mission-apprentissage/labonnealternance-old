21ee1145faad3998a9efd8b39c5d785b0665eb2d
David Boureau <bdavidxyz@gmail.com>
2020-12-18 17:37:20 +0100

    kind

---
+++ ui/components/HomeServices/HomeCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HomeCard = (props) => {
+  return (
+
+        <div className="card c-home-services__card mb-4">
+          <div className="card-body c-home-services__card-body">
+            <div className="c-home-services__circle mx-auto mt-3">
+              <img aria-hidden="true" className={'c-home-services__img--' + props.kind} src={props.logo} alt="Logo Sirius" />
+            </div>
+            <h3 className="c-home-services__title mt-3 mb-5">{props.title}</h3>
+            <p className="c-home-services__text mb-4">{props.text}</p>
+          </div>
+          <footer className="card-footer c-home-services__footer">
+            <button type="button" className="c-home-services__btn btn btn-block btn-primary mb-2 mx-auto">Découvrir</button>
+          </footer>
+        </div>
+
+      );
+};
+
+export default HomeCard;
--- ui/components/HomeServices/HomeServices.js
+++ ui/components/HomeServices/HomeServices.js
@@ -2,6 +2,7 @@ import React from "react";
 import logoSirius from "public/images/logo_sirius.svg";
 import logoMatcha from "public/images/logo_matcha.svg";
 import logoCatalogue from "public/images/logo_catalogue.svg";
+import HomeCard from "./HomeCard.js";
 
 const HomeServices = () => {
   return (
@@ -11,47 +12,12 @@ const HomeServices = () => {
         <span className="c-home-services__main-title--dark">Les services de </span>
         <span className="c-home-services__main-title--colored">La Bonne Alternance </span>
       </h2>
-      
+
       <div className="card-deck c-home-services__container">
 
-        <div className="card c-home-services__card mb-4">
-          <div className="card-body c-home-services__card-body">
-            <div className="c-home-services__circle mx-auto mt-3">
-              <img aria-hidden="true" className="c-home-services__img--sirius" src={logoSirius} alt="Logo Sirius" />
-            </div>
-            <h3 className="c-home-services__title mt-3 mb-5">Sirius</h3>
-            <p className="c-home-services__text mb-4">Avis et témoignages d'apprentis</p>
-          </div>
-          <footer className="card-footer c-home-services__footer">
-            <button type="button" className="c-home-services__btn btn btn-block btn-primary mb-2 mx-auto">Découvrir</button>
-          </footer>
-        </div>
-
-        <div className="card c-home-services__card mb-4">
-          <div className="card-body c-home-services__card-body">
-            <div className="c-home-services__circle mx-auto mt-3">
-              <img aria-hidden="true" className="c-home-services__img--matcha" src={logoMatcha} alt="Logo Sirius" />
-            </div>
-            <h3 className="c-home-services__title mt-3 mb-5">Matcha</h3>
-            <p className="c-home-services__text mb-4">Service bientôt disponible</p>
-          </div>
-          <footer className="card-footer c-home-services__footer">
-            <button type="button" className="c-home-services__btn btn btn-block btn-primary mb-2 mx-auto">Découvrir</button>
-          </footer>
-        </div>
-
-        <div className="card c-home-services__card mb-4">
-          <div className="card-body c-home-services__card-body">
-            <div className="c-home-services__circle mx-auto mt-3">
-              <img aria-hidden="true" className="c-home-services__img--catalog" src={logoCatalogue} alt="Logo Sirius" />
-            </div>
-            <h3 className="c-home-services__title mt-3 mb-3">Catalogue des formations</h3>
-            <p className="c-home-services__text mb-4">Un catalogue élargi de formations en apprentissage</p>
-          </div>
-          <footer className="card-footer c-home-services__footer">
-            <button type="button" className="c-home-services__btn btn btn-block btn-primary mb-2 mx-auto">Découvrir</button>
-          </footer>
-        </div>
+        <HomeCard logo={logoSirius} kind="sirius" title='Sirius' text="Avis et témoignages d'apprentis" />
+        <HomeCard logo={logoMatcha} kind="matcha" title='Matcha' text="Service bientôt disponible" />
+        <HomeCard logo={logoCatalogue} kind="catalog" title='Catalogue des formations' text="Un catalogue élargi de formations en apprentissage" />
 
       </div>
 
