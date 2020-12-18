3ee340f1185ec098a77c21b14c154cc22ecc45b8
David Boureau <bdavidxyz@gmail.com>
2020-12-18 16:32:03 +0100

    gudimgs

--- ui/components/HomeServices/HomeServices.js
+++ ui/components/HomeServices/HomeServices.js
@@ -1,66 +1,53 @@
 import React from "react";
+import logoSirius from "public/images/logo_sirius.svg";
+import logoMatcha from "public/images/logo_matcha.svg";
+import logoCatalogue from "public/images/logo_catalogue.svg";
 
 const HomeServices = () => {
   return (
     <section className="c-home-services">
-      <div className="">
 
-        <div class="">
-          <div class="">
+      <div className="card-deck c-home-services__container">
 
-            <div class="card-deck c-home-services__container">
-
-              <div class="card c-home-services__card mb-4 shadow-sm">
-                <div class="card-header">
-                  <h4 class="my-0 font-weight-normal">Free</h4>
-                </div>
-                <div class="card-body">
-                  <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-                  <ul class="list-unstyled mt-3 mb-4">
-                    <li>10 users included</li>
-                    <li>2 GB of storage</li>
-                    <li>Email support</li>
-                    <li>Help center access</li>
-                  </ul>
-                  <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-                </div>
-              </div>
-
-              <div class="card c-home-services__card mb-4 shadow-sm">
-                <div class="card-header">
-                  <h4 class="my-0 font-weight-normal">Pro</h4>
-                </div>
-                <div class="card-body">
-                  <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
-                  <ul class="list-unstyled mt-3 mb-4">
-                    <li>20 users included</li>
-                    <li>10 GB of storage</li>
-                    <li>Priority email support</li>
-                    <li>Help center access</li>
-                  </ul>
-                  <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-                </div>
-              </div>
-
-              <div class="card c-home-services__card mb-4 shadow-sm">
-                <div class="card-header">
-                  <h4 class="my-0 font-weight-normal">Enterprise</h4>
-                </div>
-                <div class="card-body">
-                  <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-                  <ul class="list-unstyled mt-3 mb-4">
-                    <li>30 users included</li>
-                    <li>15 GB of storage</li>
-                    <li>Phone and email support</li>
-                    <li>Help center access</li>
-                  </ul>
-                  <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
-                </div>
-              </div>
-
-            </div> {/*card-deck*/}
-          </div> {/*col-offset etc*/}
+        <div className="card c-home-services__card mb-4">
+          <div className="card-body c-home-services__card-body">
+            <div className="c-home-services__circle mx-auto mt-3">
+              <img aria-hidden="true" className="c-home-services__img--sirius" src={logoSirius} alt="Logo Sirius" />
+            </div>
+            <h2 className="c-home-services__title mt-3 mb-5">Sirius</h2>
+            <p className="c-home-services__text mb-4">Avis et témoignages d'apprentis</p>
+          </div>
+          <footer className="card-footer c-home-services__footer">
+            <button type="button" className="c-home-services__btn btn btn-lg btn-block btn-primary mb-2">Découvrir</button>
+          </footer>
         </div>
+
+        <div className="card c-home-services__card mb-4">
+          <div className="card-body c-home-services__card-body">
+            <div className="c-home-services__circle mx-auto mt-3">
+              <img aria-hidden="true" className="c-home-services__img--matcha" src={logoMatcha} alt="Logo Sirius" />
+            </div>
+            <h2 className="c-home-services__title mt-3 mb-5">Matcha</h2>
+            <p className="c-home-services__text mb-4">Service bientôt disponible</p>
+          </div>
+          <footer className="card-footer c-home-services__footer">
+            <button type="button" className="c-home-services__btn btn btn-lg btn-block btn-primary mb-2">Découvrir</button>
+          </footer>
+        </div>
+
+        <div className="card c-home-services__card mb-4">
+          <div className="card-body c-home-services__card-body">
+            <div className="c-home-services__circle mx-auto mt-3">
+              <img aria-hidden="true" className="c-home-services__img--catalog" src={logoCatalogue} alt="Logo Sirius" />
+            </div>
+            <h2 className="c-home-services__title mt-3 mb-3">Catalogue des formations</h2>
+            <p className="c-home-services__text mb-4">Un catalogue élargi de formations en apprentissage</p>
+          </div>
+          <footer className="card-footer c-home-services__footer">
+            <button type="button" className="c-home-services__btn btn btn-lg btn-block btn-primary mb-2">Découvrir</button>
+          </footer>
+        </div>
+
 
 
       </div>
