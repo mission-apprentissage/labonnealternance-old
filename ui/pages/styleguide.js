13b87adf738ce26728659de553025a49e69cd7a7
David Boureau <bdavidxyz@gmail.com>
2021-01-08 14:37:43 +0100

    more examples

--- ui/pages/styleguide.js
+++ ui/pages/styleguide.js
@@ -153,8 +153,8 @@ const Styleguide = () => {
           <img className="c-input-work-img" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cGF0aCBkPSJNMTAgMThDMTEuNzc1IDE3Ljk5OTYgMTMuNDk4OCAxNy40MDU0IDE0Ljg5NyAxNi4zMTJMMTkuMjkzIDIwLjcwOEwyMC43MDcgMTkuMjk0TDE2LjMxMSAxNC44OThDMTcuNDA1IDEzLjQ5OTcgMTcuOTk5NiAxMS43NzU0IDE4IDEwQzE4IDUuNTg5IDE0LjQxMSAyIDEwIDJDNS41ODkgMiAyIDUuNTg5IDIgMTBDMiAxNC40MTEgNS41ODkgMTggMTAgMThaTTEwIDRDMTMuMzA5IDQgMTYgNi42OTEgMTYgMTBDMTYgMTMuMzA5IDEzLjMwOSAxNiAxMCAxNkM2LjY5MSAxNiA0IDEzLjMwOSA0IDEwQzQgNi42OTEgNi42OTEgNCAxMCA0WiIgZmlsbD0iIzM4MzgzOCIvPgogIDxwYXRoIGQ9Ik0xMS40MTIgOC41ODYwOUMxMS43OTEgOC45NjYwOSAxMiA5LjQ2ODA5IDEyIDEwLjAwMDFIMTRDMTQuMDAwOSA5LjQ3NDUxIDEzLjg5NzYgOC45NTM5OCAxMy42OTYxIDguNDY4NTdDMTMuNDk0NiA3Ljk4MzE2IDEzLjE5ODkgNy41NDI1MSAxMi44MjYgNy4xNzIwOUMxMS4zMTIgNS42NjAwOSA4LjY4NzAxIDUuNjYwMDkgNy4xNzQwMSA3LjE3MjA5TDguNTg2MDEgOC41ODgwOUM5LjM0NjAxIDcuODMwMDkgMTAuNjU2IDcuODMyMDkgMTEuNDEyIDguNTg2MDlaIiBmaWxsPSIjMzgzODM4Ii8+Cjwvc3ZnPgo=" alt=""/>
         </div>
         <ul id="downshift-1-menu" role="listbox" aria-labelledby="downshift-1-label" className="c-autocomplete__menu mb-5">
-          <li className="" role="option" aria-selected="false">Boulangerie, viennoiserie, pâtisserie, chocolaterie, confiserie, glacerie</li>
-          <li className="" role="option" aria-selected="false">Boulangerie et autres, alimentation, conserverie, pain frais, etc</li>
+          <li className="" role="option" aria-selected="false"><strong>Bou</strong>langerie, viennoiserie, pâtisserie, chocolaterie, confiserie, glacerie</li>
+          <li className="" role="option" aria-selected="false"><strong>Bou</strong>langerie et autres, alimentation, conserverie, pain frais, etc</li>
           <li className="" role="option" aria-selected="false">Vente, conseil en magasin (alimentation, produits frais, boissons)</li>
         </ul>
 
