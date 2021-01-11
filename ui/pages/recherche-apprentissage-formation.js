ef9f78c843895e55bab8f4d00dd708aeee0757b3
Alan Le Ruyet <alan.leruyet@free.fr>
2021-01-11 14:58:57 +0100

    Application context Scope

--- ui/pages/recherche-apprentissage-formation.js
+++ ui/pages/recherche-apprentissage-formation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "../components/navigation";
 import SearchForTrainingsAndJobs from "../components/SearchForTrainingsAndJobs";
+import { ScopeContextProvider } from "context/ScopeContext.js";
 
 import Head from "next/head";
 
@@ -9,7 +10,9 @@ const RechercheApprentissageFormation = () => (
     <Head>
       <link href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css" rel="stylesheet" />
     </Head>
-    <SearchForTrainingsAndJobs isTrainingOnly="1" />
+    <ScopeContextProvider value={{ isJob: false, isTraining: true }}>
+      <SearchForTrainingsAndJobs />
+    </ScopeContextProvider>
   </>
 );
 
