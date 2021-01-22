5f67eb810a83eb8854ee2a1164e509d7322a8f76
David Boureau <bdavidxyz@gmail.com>
2021-01-22 14:47:29 +0100

    lower case etc

--- ui/components/ItemDetail/Training.js
+++ ui/components/ItemDetail/Training.js
@@ -59,8 +59,8 @@ const Training = ({ training, handleSelectItem, showTextOnly, searchForJobsOnNew
 
         <div className="c-media-body">
           <div className="title d-inline-block">{training.title ? training.title : training.longTitle}</div>
-          <div className="cardText text-capitalize pt-1">{training.company.name.toLowerCase()}</div>
-          <div className="cardText text-capitalize pt-2">{training.place.fullAddress.toLowerCase()}</div>
+          <div className="cardText text-capitalize pt-1">{training.company.name}</div>
+          <div className="cardText text-capitalize pt-2">{training.place.fullAddress}</div>
           <span className="cardDistance pt-1">
             {Math.round(training.place.distance)} km(s) du lieu de recherche
 
