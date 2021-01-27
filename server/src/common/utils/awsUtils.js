4ee7b0924385fe88fe194cf9581b362f50c06e0b
Alan Le Ruyet <alan.leruyet@free.fr>
2021-01-27 17:38:40 +0100

    Repositionnement config

--- server/src/common/utils/awsUtils.js
+++ server/src/common/utils/awsUtils.js
@@ -7,8 +7,8 @@ const s3 = new AWS.S3({
   apiVersion: "2006-03-01",
   region: "eu-west-3",
   credentials: {
-    accessKeyId: config.aws.accessKeyId,
-    secretAccessKey: config.aws.secretAccessKey,
+    accessKeyId: config.private.awsAccessKeyId,
+    secretAccessKey: config.private.awsSecretAccessKey,
   },
 });
 
