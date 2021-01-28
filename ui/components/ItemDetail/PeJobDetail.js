85c718a5a1d3623da8133f9b8e645b8195657cad
David Boureau <bdavidxyz@gmail.com>
2021-01-28 16:56:23 +0100

    better ESLint

--- ui/components/ItemDetail/PeJobDetail.js
+++ ui/components/ItemDetail/PeJobDetail.js
@@ -12,53 +12,43 @@ const PeJobDetail = ({ job }) => {
     } catch (err) {}
   });
 
-  console.log('the job is..')
-  console.log(job)
-
-  const description = get(job, 'job.description', undefined)
-  const contractDuration = get(job, 'job.contractDescription', undefined)
-  const contractRythm = get(job, 'job.duration', undefined)
+  const description = get(job, "job.description", undefined)
+  const contractDuration = get(job, "job.contractDescription", undefined)
+  const contractRythm = get(job, "job.duration", undefined)
   const creationDate = job?.job?.creationDate ? moment(job.job.creationDate).format("DD / MM / YYYY") : undefined 
 
   return (
     <>
       <div className="c-detail-body">
         <div className="c-detail-company">
-          {get(job, 'company.name', ReactHtmlParser('<em>Entreprise non précisée</em>'))} <span className="c-detail-proposal"> propose actuellement cette offre</span>
+          {get(job, "company.name", ReactHtmlParser("<em>Entreprise non précisée</em>"))}
+          <span className="c-detail-proposal"> propose actuellement cette offre</span>
         </div>
-        <h2 className="c-detail-jobtitle">
-          {get(job, 'title', ReactHtmlParser('<em>Titre non précisé</em>'))}
-        </h2>        
+        <h2 className="c-detail-jobtitle">{get(job, "title", ReactHtmlParser("<em>Titre non précisé</em>"))}</h2>
         <div className="c-detail-meta">
           <div className="c-detail-metadate">
-            Publiée le  : {defaultTo(creationDate, ReactHtmlParser('<em>Donnée manquante</em>'))}
-          </div>                  
+            Publiée le  : {defaultTo(creationDate, ReactHtmlParser("<em>Donnée manquante</em>"))}
+          </div>
           <div className="c-detail-metaduration">
-            Durée  : {defaultTo(contractDuration, ReactHtmlParser('<em>Donnée manquante</em>'))}
-          </div>                  
+            Durée : {defaultTo(contractDuration, ReactHtmlParser("<em>Donnée manquante</em>"))}
+          </div>
           <div className="c-detail-metarythm">
-            Rythme  : {defaultTo(contractRythm, ReactHtmlParser('<em>Donnée manquante</em>'))}
-          </div>                  
-        </div>        
+            Rythme : {defaultTo(contractRythm, ReactHtmlParser("<em>Donnée manquante</em>"))}
+          </div>
+        </div>
 
         {description ? (
           <div className="c-detail-description">
-            <h3 className="c-detail-description-title">
-              Description de l'offre
-            </h3>            
-            <div className="c-detail-description-text">
-              {ReactHtmlParser(md.render(description))}
-            </div>
+            <h3 className="c-detail-description-title">Description de l'offre</h3>
+            <div className="c-detail-description-text">{ReactHtmlParser(md.render(description))}</div>
           </div>
         ) : (
           ""
         )}
 
-        <hr className="c-detail-header-separator mt-5"/>
+        <hr className="c-detail-header-separator mt-5" />
 
-        <h3 className="c-detail-description-title">
-          Postuler
-        </h3>            
+        <h3 className="c-detail-description-title">Postuler</h3>
 
         <div className="c-detail-pelink mt-3">
           <a target="poleemploi" href={job.url}>
@@ -73,9 +63,7 @@ const PeJobDetail = ({ job }) => {
           </div>
         </div>
 
-        <div className="mt-3">
-          &nbsp;
-        </div>
+        <div className="mt-3">&nbsp;</div>
       </div>
     </>
   );
