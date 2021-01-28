c7225ee2574329cff62ddf0686f582e3f7b972b8
David Boureau <bdavidxyz@gmail.com>
2021-01-28 10:34:48 +0100

    reseted pejob detail

--- ui/components/ItemDetail/PeJobDetail.js
+++ ui/components/ItemDetail/PeJobDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import moment from "moment";
 import infoIcon from "../../public/images/icons/info.svg";
 import linkIcon from "../../public/images/icons/link.svg";
+import { get } from "lodash";
 
 const PeJobDetail = ({ job }) => {
   useEffect(() => {
@@ -13,62 +14,12 @@ const PeJobDetail = ({ job }) => {
 
   return (
     <>
-      <div className="itemDetailBody">
-        <div className="title">En savoir plus</div>
-        <div className="sectionTitle">{job.title}</div>
-        <br />
-        Publié le {moment(job.job.creationDate).format("DD / MM / YYYY")}
-        <br />
-        Durée : {job.job.contractDescription}
-        <br />
-        Rythme : {job.job.duration}
-        <br />
-        <br />
-        <div className="sectionTitle">Description de l'offre</div>
-        <div className="description">{job.job.description}</div>
-        <br />
-        {job.contact ? (
-          <>
-            <div className="sectionTitle">Postuler</div>
-            <div className="description">
-              {job.contact.name ? (
-                <>
-                  {job.contact.name}
-                  <br />
-                </>
-              ) : (
-                ""
-              )}
-              {job.contact.info ? (
-                <>
-                  {job.contact.info}
-                  <br />
-                </>
-              ) : (
-                ""
-              )}
-              <br />
-            </div>
-          </>
-        ) : (
-          ""
-        )}
-        <div className="sectionTitle">Retrouvez l'offre sur Pôle emploi</div>
-        <div className="ellipsisLink">
-          <a target="poleemploi" href={job.url} className="gtmLienOffrePE">
-            <img className="linkIcon" src={linkIcon} alt="" />
-            {job.url}
-          </a>
+      <div className="c-detail-body">
+        <div className="c-detail-company">
+          {get(job, 'company.name', 'Une entreprise')} <span className="c-detail-proposal"> propose actuellement cette offre</span>
         </div>
-        <div className="blueAdvice">
-          <div className="floatLeft">
-            <img src={infoIcon} alt="" />
-          </div>
-          <div className="paragraph">
-            Optimisez votre recherche en envoyant aussi des candidatures spontanées aux entreprises qui n’ont pas
-            diffusé d’offre !
-          </div>
-        </div>
+        <div className="c-detail-jobname">
+        </div>        
       </div>
     </>
   );
