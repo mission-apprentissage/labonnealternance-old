9b6bcdfa4a1a3785c79c99b28575742ec91dbcdf
David Boureau <bdavidxyz@gmail.com>
2021-01-28 15:16:15 +0100

    not working /use-scroll-position

--- ui/components/ItemDetail/MobileNavbar.js
+++ ui/components/ItemDetail/MobileNavbar.js
@@ -1,24 +1,30 @@
 import React, { useEffect } from 'react';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 
 const MobileNavbar = () => {
 
-  const [scrolled, setScrolled] = React.useState(false);
-
-  const handleScroll = () => {
-
-    console.log('handing effect')
-    const offset = window.scrollY;
-    if (offset > 200) {
-      setScrolled(true);
-    } else {
-      setScrolled(false);
-    }
-
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+  useScrollPosition(({ prevPos, currPos }) => {
+    console.log(currPos.x)
+    console.log(currPos.y)
   })
+
+  // const [scrolled, setScrolled] = React.useState(false);
+
+  // const handleScroll = () => {
+
+  //   console.log('handing effect')
+  //   const offset = window.scrollY;
+  //   if (offset > 200) {
+  //     setScrolled(true);
+  //   } else {
+  //     setScrolled(false);
+  //   }
+
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('scroll', useScrollPosition)
+  // })
 
   let navbarClasses = ['navbar'];
   if (scrolled) {
