2003406233e05f1543f8cdc1c3a89e28da59a615
David Boureau <bdavidxyz@gmail.com>
2021-02-12 14:09:55 +0100

    a first form

--- ui/components/LogoIdea/LogoBar.js
+++ ui/components/LogoIdea/LogoBar.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { AutoCompleteField } from "../";
 
+
+const renderFormik = () => {
+  return (
+    <Formik>
+      {({ }) => (
+        <Form className="c-logobar-form">
+          <div className="formGroup">
+            <label htmlFor="jobField" className="sr-only">Choisissez le domaine de votre projet</label>
+            <div className="fieldContainer">
+              <AutoCompleteField
+                items={[]}
+                itemToStringFunction={() => {}}
+                onSelectedItemChangeFunction={() => {}}
+                compareItemFunction={() => {}}
+                onInputValueChangeFunction={() => {}}
+                name="jobField"
+                placeholder="ex: plomberie"
+              />
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  )
+}
 
 const LogoBar = ({ showSearchForm, showResultList }) => {
   return <div className="c-logobar">
-    actual logobar
+    {renderFormik()}
   </div>
 };
 
