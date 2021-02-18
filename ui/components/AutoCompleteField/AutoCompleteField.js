ca3c4239a2a3adde53bd1b876c057338a385ed0a
Alan Le Ruyet <alan.leruyet@free.fr>
2021-02-18 11:33:53 +0100

    prettier

--- ui/components/AutoCompleteField/AutoCompleteField.js
+++ ui/components/AutoCompleteField/AutoCompleteField.js
@@ -3,8 +3,8 @@ import { useFormikContext } from "formik";
 import { useCombobox } from "downshift";
 import { debounce } from "lodash";
 import onInputValueChangeService from "./onInputValueChangeService";
-import highlightItem  from "../../services/hightlightItem";
-import ReactHtmlParser from 'react-html-parser'; 
+import highlightItem from "../../services/hightlightItem";
+import ReactHtmlParser from "react-html-parser";
 
 let debouncedOnInputValueChange = null;
 
