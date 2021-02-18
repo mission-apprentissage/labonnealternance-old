696dd5ff5d26592a04328fd65ac8b206c97d9fe9
Alan Le Ruyet <alan.leruyet@free.fr>
2021-02-18 10:13:13 +0100

    Autorisation nock pour tester les apis

--- server/tests/utils/httpTests.js
+++ server/tests/utils/httpTests.js
@@ -4,8 +4,8 @@ const { connectToMongoForTests, cleanAll } = require("./testUtils.js");
 const server = require("../../src/http/server");
 const nock = require("nock");
 
-nock.disableNetConnect();
-nock.enableNetConnect("127.0.0.1");
+nock.enableNetConnect();
+//nock.enableNetConnect("127.0.0.1");
 
 const startServer = async () => {
   const { db } = await connectToMongoForTests();
