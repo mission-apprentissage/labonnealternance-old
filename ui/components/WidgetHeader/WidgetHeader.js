5d07f55375e8cafd2b3bf95f9a4cf7d1bcb14c1c
Alan Le Ruyet <alan.leruyet@free.fr>
2021-02-25 18:52:31 +0100

    Handlesubmit en props

--- ui/components/WidgetHeader/WidgetHeader.js
+++ ui/components/WidgetHeader/WidgetHeader.js
@@ -4,14 +4,14 @@ import LogoIdea from "components/LogoIdea/LogoIdea";
 
 import { Row } from "reactstrap";
 
-const WidgetHeader = () => {
+const WidgetHeader = ({handleSubmit}) => {
   return (
     <>
       <div className="c-widgetheader d-none d-md-block container container-fluid">
         <Row className="c-widgetheader-bar d-none d-md-flex py-2 pl-3">
           <LogoIdea />
           <div className="ml-4">
-            <HeaderForm />
+            <HeaderForm handleSubmit={handleSubmit} />
           </div>
         </Row>
       </div>
