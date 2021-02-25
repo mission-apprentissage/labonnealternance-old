ce1b009dfc4472abb48b3e3ac44d499ce61e8cb0
Alan Le Ruyet <alan.leruyet@free.fr>
2021-02-25 12:26:52 +0100

    Rationalisation header

--- ui/components/WidgetHeader/WidgetHeader.js
+++ ui/components/WidgetHeader/WidgetHeader.js
@@ -1,49 +1,17 @@
 import React from "react";
-import logoLBA from "public/images/logo-noir-seul.svg";
-import { push } from "connected-next-router";
-import { useDispatch, useSelector } from "react-redux";
-import LogoBar from "components/LogoIdea/LogoBar";
+import HeaderForm from "components/HeaderForm/HeaderForm";
+import LogoIdea from "components/LogoIdea/LogoIdea";
 
-import { Row, Col } from "reactstrap";
+import { Row } from "reactstrap";
 
-const WidgetHeader = ({ showSearchForm, showResultList }) => {
-  const dispatch = useDispatch();
-
-  const { widgetParameters } = useSelector((state) => state.trainings);
-
-  const goToLbaHome = (e) => {
-    if (widgetParameters) {
-      let p = {
-        type: "goToPage",
-        page: widgetParameters && widgetParameters?.parameters?.returnURI ? widgetParameters.parameters.returnURI : "/",
-      };
-      if (typeof window !== "undefined") {
-        window.parent.postMessage(p, "*");
-      }
-    } else {
-      e.preventDefault();
-      dispatch(push({ pathname: "/" }));
-    }
-  };
-
+const WidgetHeader = () => {
   return (
     <>
       <div className="c-widgetheader d-none d-md-block container container-fluid">
-        <Row className="d-none d-md-flex c-logo-idea py-2 pl-3">
-          <div>
-            <a href="/" onClick={goToLbaHome} className="ml-3 d-none d-lg-inline">
-              <img
-                src={
-                  widgetParameters && widgetParameters?.parameters?.returnLogoURL
-                    ? widgetParameters.parameters.returnLogoURL
-                    : logoLBA
-                }
-                alt="Retour page d'accueil de La Bonne Alternance"
-              />
-            </a>
-          </div>
+        <Row className="c-widgetheader-bar d-none d-md-flex py-2 pl-3">
+          <LogoIdea />
           <div className="ml-4">
-            <LogoBar />
+            <HeaderForm />
           </div>
         </Row>
       </div>
