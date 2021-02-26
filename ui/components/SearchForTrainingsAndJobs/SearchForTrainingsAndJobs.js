2f8a49bc9cc6b020d1d5e55921cd201c6f487f92
Alan Le Ruyet <alan.leruyet@free.fr>
2021-02-26 11:49:54 +0100

    WIP refacto

--- ui/components/SearchForTrainingsAndJobs/SearchForTrainingsAndJobs.js
+++ ui/components/SearchForTrainingsAndJobs/SearchForTrainingsAndJobs.js
@@ -1,26 +1,63 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import { Row, Col } from "reactstrap";
+import axios from "axios";
 
 import { MapListSwitchButton, ChoiceColumn } from "./components";
+import baseUrl from "utils/baseUrl";
 import {
   setSelectedItem,
   setItemToScrollTo,
   setIsFormVisible,
   setVisiblePane,
   setShouldMapBeVisible,
+  setTrainings,
+  setJobs,
+  setFormValues,
+  setExtendedSearch,
+  setHasSearch,
+  setWidgetParameters,
 } from "store/actions";
+import {
+  flyToMarker,
+  flyToLocation,
+  closeMapPopups,
+  factorTrainingsForMap,
+  factorJobsForMap,
+  computeMissingPositionAndDistance,
+  setJobMarkers,
+  setTrainingMarkers,
+} from "utils/mapTools";
+import { scrollToTop, scrollToElementInContainer, logError, getItemElement } from "utils/tools";
+import { useScopeContext } from "context/ScopeContext";
+
 import { useDispatch, useSelector } from "react-redux";
 import { resizeMap, isMapInitialized } from "utils/mapTools";
 import WidgetHeader from "components/WidgetHeader/WidgetHeader";
 import Map from "components/Map";
 
+const allJobSearchErrorText = "Problème momentané d'accès aux opportunités d'emploi";
+const partialJobSearchErrorText = "Problème momentané d'accès à certaines opportunités d'emploi";
+const trainingErrorText = "Oups ! Les résultats formation ne sont pas disponibles actuellement !";
+const technicalErrorText = "Error technique momentanée";
+
+const trainingsApi = baseUrl + "/api/v1/formations";
+const jobsApi = baseUrl + "/api/v1/jobs";
+
 const SearchForTrainingsAndJobs = () => {
   const dispatch = useDispatch();
+  const scopeContext = useScopeContext();
 
-  const { selectedItem, visiblePane } = useSelector((state) => state.trainings);
+  const { hasSearch, selectedItem, widgetParameters, visiblePane } = useSelector((state) => state.trainings);
 
   const [shouldShowWelcomeMessage, setShouldShowWelcomeMessage] = useState(true);
+  const [searchRadius, setSearchRadius] = useState(30);
+  const [isTrainingSearchLoading, setIsTrainingSearchLoading] = useState(hasSearch ? false : true);
+
+  const [isJobSearchLoading, setIsJobSearchLoading] = useState(hasSearch ? false : true);
+  const [jobSearchError, setJobSearchError] = useState("");
+  const [allJobSearchError, setAllJobSearchError] = useState(false);
+  const [trainingSearchError, setTrainingSearchError] = useState("");
 
   // See https://www.robinwieruch.de/react-useeffect-only-on-update
   /*const didMount = React.useRef(false);
@@ -57,6 +94,158 @@ const SearchForTrainingsAndJobs = () => {
       searchForJobsWithStrictRadius(values);
     }
     dispatch(setIsFormVisible(false));
+  };
+
+  const searchForTrainings = async (values) => {
+    setIsTrainingSearchLoading(true);
+    setTrainingSearchError("");
+    clearTrainings();
+    try {
+      const response = await axios.get(trainingsApi, {
+        params: {
+          romes: getRomeFromParameters(values),
+          longitude: values.location.value.coordinates[0],
+          latitude: values.location.value.coordinates[1],
+          radius: values.radius || 30,
+          diploma: values.diploma,
+        },
+      });
+
+      if (response.data.result === "error") {
+        logError("Training Search Error", `${response.data.message}`);
+        setTrainingSearchError(trainingErrorText);
+      }
+
+      dispatch(setTrainings(response.data.results));
+      dispatch(setHasSearch(true));
+      dispatch(setIsFormVisible(false));
+
+      if (response.data.results.length) {
+        setTrainingMarkers(factorTrainingsForMap(response.data.results));
+      }
+    } catch (err) {
+      console.log(
+        `Erreur interne lors de la recherche de formations (${err.response ? err.response.status : ""} : ${
+          err.response.data ? err.response.data.error : ""
+        })`
+      );
+      logError("Training search error", err);
+      setTrainingSearchError(trainingErrorText);
+    }
+
+    setIsTrainingSearchLoading(false);
+  };
+
+  const searchForJobsWithStrictRadius = async (values) => {
+    searchForJobs(values, "strict");
+  };
+
+  const searchForJobs = async (values, strictRadius) => {
+    setIsJobSearchLoading(true);
+    setJobSearchError("");
+    setAllJobSearchError(false);
+
+    try {
+      const searchCenter = [values.location.value.coordinates[0], values.location.value.coordinates[1]];
+
+      const response = await axios.get(jobsApi, {
+        params: {
+          romes: getRomeFromParameters(values),
+          longitude: values.location.value.coordinates[0],
+          latitude: values.location.value.coordinates[1],
+          insee: values.location.insee,
+          zipcode: values.location.zipcode,
+          radius: values.radius || 30,
+          strictRadius: strictRadius ? "strict" : null,
+        },
+      });
+
+      let peJobs = null;
+
+      let results = {};
+
+      if (response.data === "romes_missing") {
+        setJobSearchError(technicalErrorText);
+        logError("Job search error", `Missing romes`);
+      } else {
+        if (!response.data.peJobs.result || response.data.peJobs.result !== "error")
+          peJobs = await computeMissingPositionAndDistance(searchCenter, response.data.peJobs.results);
+
+        results = {
+          peJobs: response.data.peJobs.result && response.data.peJobs.result === "error" ? null : peJobs,
+          lbbCompanies:
+            response.data.lbbCompanies.result && response.data.lbbCompanies.result === "error"
+              ? null
+              : response.data.lbbCompanies.results,
+          lbaCompanies:
+            response.data.lbaCompanies.result && response.data.lbaCompanies.result === "error"
+              ? null
+              : response.data.lbaCompanies.results,
+        };
+      }
+
+      // gestion des erreurs
+      let jobErrorMessage = "";
+      if (
+        response.data.peJobs.result === "error" &&
+        response.data.lbbCompanies.result === "error" &&
+        response.data.lbaCompanies.result === "error"
+      ) {
+        //TODO: définition niveau d'erreur JOB total
+        setAllJobSearchError(true);
+        jobErrorMessage = allJobSearchErrorText;
+        logError(
+          "Job Search Error",
+          `All job sources in error. PE : ${response.data.peJobs.message} - LBB : ${response.data.lbbCompanies.message} - LBA : ${response.data.lbaCompanies.message}`
+        );
+      } else {
+        if (
+          response.data.peJobs.result === "error" ||
+          response.data.lbbCompanies.result === "error" ||
+          response.data.lbaCompanies.result === "error"
+        ) {
+          jobErrorMessage = partialJobSearchErrorText;
+          if (response.data.peJobs.result === "error")
+            logError("Job Search Error", `PE Error : ${response.data.peJobs.message}`);
+          if (response.data.lbbCompanies.result === "error")
+            logError("Job Search Error", `LBB Error : ${response.data.lbbCompanies.message}`);
+          if (response.data.lbaCompanies.result === "error")
+            logError("Job Search Error", `LBA Error : ${response.data.lbaCompanies.message}`);
+        }
+      }
+
+      if (jobErrorMessage) setJobSearchError(jobErrorMessage);
+
+      dispatch(setJobs(results));
+      dispatch(setHasSearch(true));
+
+      setJobMarkers(factorJobsForMap(results), scopeContext.isTraining ? null : searchCenter);
+    } catch (err) {
+      console.log(
+        `Erreur interne lors de la recherche d'emplois (${
+          err.response && err.response.status ? err.response.status : ""
+        } : ${err.response && err.response.data ? err.response.data.error : err.message})`
+      );
+      logError("Job search error", err);
+      setJobSearchError(allJobSearchErrorText);
+      setAllJobSearchError(true);
+    }
+
+    setIsJobSearchLoading(false);
+  };
+
+  const clearTrainings = () => {
+    dispatch(setTrainings([]));
+    setTrainingMarkers(null);
+    closeMapPopups();
+  };
+
+  const getRomeFromParameters = (values) => {
+    return widgetParameters?.parameters?.jobName &&
+      widgetParameters?.parameters?.romes &&
+      widgetParameters?.parameters?.frozenJob
+      ? widgetParameters?.parameters?.romes
+      : values.job.romes.join(",");
   };
 
   const showSearchForm = (e) => {
@@ -114,6 +303,15 @@ const SearchForTrainingsAndJobs = () => {
             showResultList={showResultList}
             showSearchForm={showSearchForm}
             unSelectItem={unSelectItem}
+            searchRadius={searchRadius}
+            isTrainingSearchLoading={isTrainingSearchLoading}
+            searchForTrainings={searchForTrainings}
+            trainingSearchError={trainingSearchError}
+            searchForJobs={searchForJobs}
+            searchForJobsWithStrictRadius={searchForJobsWithStrictRadius}
+            isJobSearchLoading={isJobSearchLoading}
+            jobSearchError={jobSearchError}
+            allJobSearchError={allJobSearchError}
           />
         </Col>
         <Col className={`vh-100 ${visiblePane === "resultMap" ? "activeXSPane" : "inactiveXSPane"}`} xs="12" md="7">
--- ui/components/SearchForTrainingsAndJobs/components/ChoiceColumn.js
+++ ui/components/SearchForTrainingsAndJobs/components/ChoiceColumn.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
 import distance from "@turf/distance";
-import baseUrl from "utils/baseUrl";
 import { scrollToTop, scrollToElementInContainer, logError, getItemElement } from "utils/tools";
 import ItemDetail from "components/ItemDetail/ItemDetail";
 import LoadingScreen from "components/LoadingScreen";
@@ -10,41 +8,33 @@ import SearchForm from "./SearchForm";
 import ResultLists from "./ResultLists";
 import {
   setTrainings,
-  setJobs,
   setSelectedItem,
   setItemToScrollTo,
   setFormValues,
   setExtendedSearch,
-  setHasSearch,
-  setIsFormVisible,
   setWidgetParameters,
+  setJobs,
 } from "store/actions";
-import {
-  flyToMarker,
-  flyToLocation,
-  closeMapPopups,
-  factorTrainingsForMap,
-  factorJobsForMap,
-  computeMissingPositionAndDistance,
-  setJobMarkers,
-  setTrainingMarkers,
-} from "utils/mapTools";
-import { useScopeContext } from "context/ScopeContext";
-
+import { flyToMarker, flyToLocation, closeMapPopups } from "utils/mapTools";
 import { fetchAddressFromCoordinates } from "services/baseAdresse";
 
-const allJobSearchErrorText = "Problème momentané d'accès aux opportunités d'emploi";
-const partialJobSearchErrorText = "Problème momentané d'accès à certaines opportunités d'emploi";
-const trainingErrorText = "Oups ! Les résultats formation ne sont pas disponibles actuellement !";
-const technicalErrorText = "Error technique momentanée";
-
-const trainingsApi = baseUrl + "/api/v1/formations";
-const jobsApi = baseUrl + "/api/v1/jobs";
-
-const ChoiceColumn = ({ showResultList, unSelectItem, showSearchForm, handleSubmit, shouldShowWelcomeMessage }) => {
+const ChoiceColumn = ({
+  showResultList,
+  unSelectItem,
+  showSearchForm,
+  handleSubmit,
+  shouldShowWelcomeMessage,
+  searchRadius,
+  isTrainingSearchLoading,
+  searchForTrainings,
+  trainingSearchError,
+  searchForJobs,
+  searchForJobsWithStrictRadius,
+  isJobSearchLoading,
+  jobSearchError,
+  allJobSearchError,
+}) => {
   const dispatch = useDispatch();
-
-  const scopeContext = useScopeContext();
 
   const {
     hasSearch,
@@ -58,17 +48,7 @@ const ChoiceColumn = ({ showResultList, unSelectItem, showSearchForm, handleSubm
   } = useSelector((state) => state.trainings);
 
   const [isLoading, setIsLoading] = useState(hasSearch ? false : true);
-  const [isTrainingSearchLoading, setIsTrainingSearchLoading] = useState(hasSearch ? false : true);
-  const [isJobSearchLoading, setIsJobSearchLoading] = useState(hasSearch ? false : true);
-  const [searchRadius, setSearchRadius] = useState(30);
-  const [jobSearchError, setJobSearchError] = useState("");
-  const [allJobSearchError, setAllJobSearchError] = useState(false);
   
-
-  //TODO: définition niveau d'erreur JOB partiel  ou total
-
-  const [trainingSearchError, setTrainingSearchError] = useState("");
-
   useEffect(() => {
     if (itemToScrollTo) {
       const itemElement = getItemElement(itemToScrollTo);
@@ -150,14 +130,20 @@ const ChoiceColumn = ({ showResultList, unSelectItem, showSearchForm, handleSubm
     }
   };
 
-  
-
   const searchForJobsOnNewCenter = async (newCenter) => {
     searchOnNewCenter(newCenter, null, "jobs");
   };
 
   const searchForTrainingsOnNewCenter = async (newCenter) => {
     searchOnNewCenter(newCenter, "trainings", null);
+  };
+
+  const searchForJobsWithLooseRadius = async () => {
+    dispatch(setExtendedSearch(true));
+    scrollToTop("choiceColumn");
+
+    dispatch(setJobs([]));
+    searchForJobs(formValues, null);
   };
 
   const searchOnNewCenter = async (newCenter, isTrainingSearch, isJobSearch) => {
@@ -178,7 +164,9 @@ const ChoiceColumn = ({ showResultList, unSelectItem, showSearchForm, handleSubm
 
     searchForJobsWithStrictRadius(formValues);
 
-    if (isTrainingSearch) searchForTrainings(formValues);
+    if (isTrainingSearch) {
+      searchForTrainings(formValues);
+    }
   };
 
   const updateTrainingDistanceWithNewCenter = (coordinates) => {
@@ -188,166 +176,6 @@ const ChoiceColumn = ({ showResultList, unSelectItem, showSearchForm, handleSubm
         Math.round(10 * distance(coordinates, [trainings[i].place.longitude, trainings[i].place.latitude])) / 10;
     }
     dispatch(setTrainings(trainings));
-  };
-
-  const clearTrainings = () => {
-    dispatch(setTrainings([]));
-    setTrainingMarkers(null);
-    closeMapPopups();
-  };
-
-  const getRomeFromParameters = (values) => {
-    return widgetParameters?.parameters?.jobName &&
-      widgetParameters?.parameters?.romes &&
-      widgetParameters?.parameters?.frozenJob
-      ? widgetParameters?.parameters?.romes
-      : values.job.romes.join(",");
-  };
-
-  const searchForTrainings = async (values) => {
-    setIsTrainingSearchLoading(true);
-    setTrainingSearchError("");
-    clearTrainings();
-    try {
-      const response = await axios.get(trainingsApi, {
-        params: {
-          romes: getRomeFromParameters(values),
-          longitude: values.location.value.coordinates[0],
-          latitude: values.location.value.coordinates[1],
-          radius: values.radius || 30,
-          diploma: values.diploma,
-        },
-      });
-
-      if (response.data.result === "error") {
-        logError("Training Search Error", `${response.data.message}`);
-        setTrainingSearchError(trainingErrorText);
-      }
-
-      dispatch(setTrainings(response.data.results));
-      dispatch(setHasSearch(true));
-      dispatch(setIsFormVisible(false));
-
-      if (response.data.results.length) {
-        setTrainingMarkers(factorTrainingsForMap(response.data.results));
-      }
-    } catch (err) {
-      console.log(
-        `Erreur interne lors de la recherche de formations (${err.response ? err.response.status : ""} : ${
-          err.response.data ? err.response.data.error : ""
-        })`
-      );
-      logError("Training search error", err);
-      setTrainingSearchError(trainingErrorText);
-    }
-
-    setIsTrainingSearchLoading(false);
-  };
-
-  const searchForJobsWithStrictRadius = async (values) => {
-    searchForJobs(values, "strict");
-  };
-
-  const searchForJobsWithLooseRadius = async () => {
-    dispatch(setExtendedSearch(true));
-    scrollToTop("choiceColumn");
-
-    dispatch(setJobs([]));
-    searchForJobs(formValues, null);
-  };
-
-  const searchForJobs = async (values, strictRadius) => {
-    setIsJobSearchLoading(true);
-    setJobSearchError("");
-    setAllJobSearchError(false);
-
-    try {
-      const searchCenter = [values.location.value.coordinates[0], values.location.value.coordinates[1]];
-
-      const response = await axios.get(jobsApi, {
-        params: {
-          romes: getRomeFromParameters(values),
-          longitude: values.location.value.coordinates[0],
-          latitude: values.location.value.coordinates[1],
-          insee: values.location.insee,
-          zipcode: values.location.zipcode,
-          radius: values.radius || 30,
-          strictRadius: strictRadius ? "strict" : null,
-        },
-      });
-
-      let peJobs = null;
-
-      let results = {};
-
-      if (response.data === "romes_missing") {
-        setJobSearchError(technicalErrorText);
-        logError("Job search error", `Missing romes`);
-      } else {
-        if (!response.data.peJobs.result || response.data.peJobs.result !== "error")
-          peJobs = await computeMissingPositionAndDistance(searchCenter, response.data.peJobs.results);
-
-        results = {
-          peJobs: response.data.peJobs.result && response.data.peJobs.result === "error" ? null : peJobs,
-          lbbCompanies:
-            response.data.lbbCompanies.result && response.data.lbbCompanies.result === "error"
-              ? null
-              : response.data.lbbCompanies.results,
-          lbaCompanies:
-            response.data.lbaCompanies.result && response.data.lbaCompanies.result === "error"
-              ? null
-              : response.data.lbaCompanies.results,
-        };
-      }
-
-      // gestion des erreurs
-      let jobErrorMessage = "";
-      if (
-        response.data.peJobs.result === "error" &&
-        response.data.lbbCompanies.result === "error" &&
-        response.data.lbaCompanies.result === "error"
-      ) {
-        //TODO: définition niveau d'erreur JOB total
-        setAllJobSearchError(true);
-        jobErrorMessage = allJobSearchErrorText;
-        logError(
-          "Job Search Error",
-          `All job sources in error. PE : ${response.data.peJobs.message} - LBB : ${response.data.lbbCompanies.message} - LBA : ${response.data.lbaCompanies.message}`
-        );
-      } else {
-        if (
-          response.data.peJobs.result === "error" ||
-          response.data.lbbCompanies.result === "error" ||
-          response.data.lbaCompanies.result === "error"
-        ) {
-          jobErrorMessage = partialJobSearchErrorText;
-          if (response.data.peJobs.result === "error")
-            logError("Job Search Error", `PE Error : ${response.data.peJobs.message}`);
-          if (response.data.lbbCompanies.result === "error")
-            logError("Job Search Error", `LBB Error : ${response.data.lbbCompanies.message}`);
-          if (response.data.lbaCompanies.result === "error")
-            logError("Job Search Error", `LBA Error : ${response.data.lbaCompanies.message}`);
-        }
-      }
-
-      if (jobErrorMessage) setJobSearchError(jobErrorMessage);
-
-      dispatch(setJobs(results));
-      dispatch(setHasSearch(true));
-
-      setJobMarkers(factorJobsForMap(results), scopeContext.isTraining ? null : searchCenter);
-    } catch (err) {
-      console.log(
-        `Erreur interne lors de la recherche d'emplois (${
-          err.response && err.response.status ? err.response.status : ""
-        } : ${err.response && err.response.data ? err.response.data.error : err.message})`
-      );
-      logError("Job search error", err);
-      setJobSearchError(allJobSearchErrorText);
-      setAllJobSearchError(true);
-    }
-
-    setIsJobSearchLoading(false);
   };
 
   const getResultLists = () => {
