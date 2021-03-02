6535ab5c08539056ec680b4823975762c619425b
Alan Le Ruyet <alan.leruyet@free.fr>
2021-03-02 12:38:22 +0100

    Utilitaire pour factorisation

---
+++ ui/utils/getItemId.js
@@ -0,0 +1,22 @@
+export const getItemId = (item) => {
+  return getItemIdAndType(item).itemId;
+};
+
+export const getItemIdAndType = (item) => {
+  let itemId = item.id;
+  let type = "training";
+  if (item.ideaType === "peJob") {
+    itemId = item.job.id;
+    type = item.ideaType;
+  } else if (item.ideaType !== "formation") {
+    itemId = item.company.siret;
+    type = item.ideaType;
+  }
+
+  return { itemId, type };
+};
+
+export const getItemQueryParameters = (item) => {
+  const idAndType = getItemIdAndType(item);
+  return `type=${idAndType.type}&itemId=${idAndType.itemId}`;
+};
