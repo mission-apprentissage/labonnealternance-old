5d7107eaa94e30c963631f6da03a02c8dd97a039
Alan Le Ruyet <alan.leruyet@free.fr>
2021-03-05 13:03:38 +0100

    Externalisation searchForTrainings

---
+++ ui/components/SearchForTrainingsAndJobs/services/searchForTrainings.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { logError } from "utils/tools";
+
+import {
+  trainingsApi,
+  trainingErrorText,
+  getRomeFromParameters,
+} from "components/SearchForTrainingsAndJobs/services/utils";
+
+export const searchForTrainingsFunction = async ({
+  values,
+  dispatch,
+  setIsTrainingSearchLoading,
+  setTrainingSearchError,
+  clearTrainings,
+  setTrainings,
+  setHasSearch,
+  setIsFormVisible,
+  setTrainingMarkers,
+  factorTrainingsForMap,
+  widgetParameters,
+}) => {
+  setIsTrainingSearchLoading(true);
+  setTrainingSearchError("");
+  clearTrainings();
+  try {
+    const response = await axios.get(trainingsApi, {
+      params: {
+        romes: getRomeFromParameters({ values, widgetParameters }),
+        longitude: values.location.value.coordinates[0],
+        latitude: values.location.value.coordinates[1],
+        radius: values.radius || 30,
+        diploma: values.diploma,
+      },
+    });
+
+    if (response.data.result === "error") {
+      logError("Training Search Error", `${response.data.message}`);
+      setTrainingSearchError(trainingErrorText);
+    }
+
+    dispatch(setTrainings(response.data.results));
+    dispatch(setHasSearch(true));
+    dispatch(setIsFormVisible(false));
+
+    if (response.data.results.length) {
+      setTrainingMarkers(factorTrainingsForMap(response.data.results));
+    }
+  } catch (err) {
+    console.log(
+      `Erreur interne lors de la recherche de formations (${err.response ? err.response.status : ""} : ${
+        err.response.data ? err.response.data.error : ""
+      })`
+    );
+    logError("Training search error", err);
+    setTrainingSearchError(trainingErrorText);
+  }
+
+  setIsTrainingSearchLoading(false);
+};
