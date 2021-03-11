817077ea6fb96d0ed5375607798cf78cedcb7277
David Boureau <bdavidxyz@gmail.com>
2021-03-11 16:43:19 +0100

    noopener

--- ui/pages/developpeurs.js
+++ ui/pages/developpeurs.js
@@ -21,11 +21,11 @@ const developpeurs = () => (
         </div>
         <div className="col-12 col-md-7">
           <h3>Code source ouvert</h3>
-          <a href="https://github.com/mission-apprentissage/labonnealternance">LBA - Recherche d'une formation et/ou d'un organisme de formation en apprentissage</a>
+          <a href="https://github.com/mission-apprentissage/labonnealternance" target="_blank" rel="noopener noreferrer">LBA - Recherche d'une formation et/ou d'un organisme de formation en apprentissage</a>
           <h3 className="mt-3">Données ouvertes</h3>
           <p>Afin de faciliter l’accès aux informations pour les publics là où ils se trouvent (notamment sur votre site internet !), nous avons développé 4 API et un Widget, disponibles en open source.</p>
           <p>Testez le widget et les API et accédez à leur documentation sur le site API.gouv</p> 
-          <a href="https://api.gouv.fr/les-api/api-la-bonne-alternance">https://api.gouv.fr/les-api/api-la-bonne-alternance</a>
+          <a href="https://api.gouv.fr/les-api/api-la-bonne-alternance" target="_blank" rel="noopener noreferrer">https://api.gouv.fr/les-api/api-la-bonne-alternance</a>
         </div>
       </div>
     </div>
