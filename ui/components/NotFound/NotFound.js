189a536bb679cba70c21ffa25f565b4c389299b9
David Boureau <bdavidxyz@gmail.com>
2021-03-18 15:16:32 +0100

    margins

--- ui/components/NotFound/NotFound.js
+++ ui/components/NotFound/NotFound.js
@@ -1,6 +1,4 @@
 import React from "react";
-import noop from "lodash";
-import LostHeader from "components/LostHeader/LostHeader";
 import Footer from "components/footer";
 import lostCat from "public/images/lostCat.svg";
 
@@ -8,9 +6,6 @@ const NotFound = () => (
   <>
 
     <div className="not-found-container">
-      <div className="not-found-header">
-        <LostHeader handleSubmit={noop} />
-      </div>
       <div className="not-found-content">
         <div className="row">
           <div className="col-12 col-lg-6 order-lg-2">
@@ -20,8 +15,8 @@ const NotFound = () => (
             <h1 className="not-found-title">404</h1>
             <strong className="not-found-subtitle">Vous êtes perdu ?</strong>
             <p className="not-found-text">Il semble que la page que vous essayez de rejoindre n’existe pas. En cas de problème pour retrouver la page, essayez de repartir de la page d’accueil en cliquant sur le lien ci-dessous.</p>
-            <div className=''>
-                <a href="https://labonnealternance.pole-emploi.fr" className="btn btn-block btn-primary mb-2 mx-auto w-25">
+            <div className='mt-4 mb-5 mb-lg-0'>
+                <a href="https://labonnealternance.pole-emploi.fr" className="btn btn-block btn-primary mb-2 mx-auto w-50">
                 Page d'accueil
               </a>
             </div>
