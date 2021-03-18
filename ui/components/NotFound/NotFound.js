831e0420ce133c506a072670aee80a4eaf7b8dd8
David Boureau <bdavidxyz@gmail.com>
2021-03-18 11:10:30 +0100

    reset not found

--- ui/components/NotFound/NotFound.js
+++ ui/components/NotFound/NotFound.js
@@ -1,26 +1,11 @@
 import React from "react";
-import peopleIcon from "../../public/images/icons/searchingPeople.svg";
-import errorAlertIcon from "../../public/images/icons/errorAlert.svg";
 
-import styles from "./notFound.module.scss";
 
 const NotFound = () => (
-  <div className={styles.root}>
-    <div className={styles.error}>
-      <img className={styles.errorBannerImage} src={peopleIcon} alt="" />
-      <div id="error-text">
-        <span className={styles.span}>404</span>
-        <h1 className={styles.h1}>PAGE INCONNUE !</h1>
-        <div className={styles.errorMessage}>
-          <img src={errorAlertIcon} alt="" className={styles.errorImg} />
-          Aucun contenu ne correspond à cette adresse
-        </div>
-      </div>
-
-      <a href="https://labonnealternance.pole-emploi.fr" className="back">
-        Retour à l'accueil
-      </a>
-    </div>
+  <div className='not-found'>
+    <a href="https://labonnealternance.pole-emploi.fr" className="back">
+      Retour à l'accueil
+    </a>
   </div>
 );
 
