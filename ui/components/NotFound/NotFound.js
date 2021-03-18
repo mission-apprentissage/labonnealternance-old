468c1edd47b6789eeac1191154317f343c41d8de
David Boureau <bdavidxyz@gmail.com>
2021-03-18 14:10:28 +0100

    LostHeader

--- ui/components/NotFound/NotFound.js
+++ ui/components/NotFound/NotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
 import noop from "lodash";
-import { WidgetHeader } from "components/WidgetHeader";
+import LostHeader from "components/LostHeader";
 import Footer from "components/footer";
 import lostCat from "public/images/lostCat.svg";
 
@@ -9,20 +9,21 @@ const NotFound = () => (
 
     <div className="not-found-container">
       <div className="not-found-header">
-        <WidgetHeader handleSubmit={noop} />
+        <LostHeader handleSubmit={noop} />
       </div>
-      <div className="not-found-content-container">
-        <div className="not-found-content">
-          <div className="row">
-            <div className="col-12 col-lg-6 order-lg-2">
-              <img src={lostCat} alt="Chat perdu" />
-            </div>
-            <div className="col-12 col-lg-6 order-lg-1">
-              <div className='not-found'>
-                <a href="https://labonnealternance.pole-emploi.fr" className="back">
-                  Retour à l'accueil
-            </a>
-              </div>
+      <div className="not-found-content">
+        <div className="row">
+          <div className="col-12 col-lg-6 order-lg-2">
+            <img src={lostCat} alt="Chat perdu" className="w-75"/>
+          </div>
+          <div className="col-12 col-lg-6 order-lg-1">
+            <h1 className="not-found-title">404</h1>
+            <strong className="not-found-subtitle">Vous êtes perdu ?</strong>
+            <p className="not-found-text">Il semble que la page que vous essayez de rejoindre n’existe pas. En cas de problème pour retrouver la page, essayez de repartir de la page d’accueil en cliquant sur le lien ci-dessous.</p>
+            <div className=''>
+                <a href="https://labonnealternance.pole-emploi.fr" className="btn btn-block btn-primary mb-2 mx-auto w-25">
+                Page d'accueil
+              </a>
             </div>
           </div>
         </div>
