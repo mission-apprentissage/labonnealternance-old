d884309d9bf1580ea2aa69a36e465054c859a5f0
David Boureau <bdavidxyz@gmail.com>
2021-03-19 11:59:25 +0100

    page 500 ok

--- ui/pages/500.js
+++ ui/pages/500.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import lostWoman from "public/images/lostWoman.svg";
+import simpleLogo from "public/images/simpleLogo.svg";
+
+
 const Error500 = () => {
-  return <h1>Error 500 occured</h1>;
+  return <>
+    <div className="c-500">
+      <img src={simpleLogo} alt="Logo La Bonne Alternance" className="c-500-logo" onClick={(e) => {
+        e.preventDefault();
+        window.location.href="/";
+      }}/>
+      <h1 className="c-500-title">Oops</h1>
+      <p>Le site la bonne alternance n'est pas disponible pour le moment</p>
+      <img src={lostWoman} alt="Personne perdue" className="c-500-img" />
+    </div>
+  </>;
 };
 
 export default Error500;
