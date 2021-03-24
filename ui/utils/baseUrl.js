5c02ab9209b787a9265e9e298781d388736ff9db
Alan Le Ruyet <alan.leruyet@free.fr>
2021-03-24 11:50:22 +0100

    Clean

--- ui/utils/baseUrl.js
+++ ui/utils/baseUrl.js
@@ -1,6 +1,5 @@
 import config from "./config";
 
 const baseUrl = config.baseUrl;
-console.log("baseUrl ", baseUrl);
 
 export default baseUrl;
--- ui/utils/env.js
+++ ui/utils/env.js
@@ -1,6 +1,5 @@
 import config from "./config";
 
 const env = config.env;
-console.log("env ", env);
 
 export default env;
