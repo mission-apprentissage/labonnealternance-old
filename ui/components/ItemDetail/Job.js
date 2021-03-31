0ed063da7ecf78cc4bb4752bff7dce93ca0256d1
Alan Le Ruyet <alan.leruyet@free.fr>
2021-03-31 18:15:06 +0200

    Futur remplacement de peJob.js

---
+++ ui/components/ItemDetail/Job.js
@@ -0,0 +1,87 @@
+import React from "react";
+import jobIcon from "../../public/images/icons/job.svg";
+import { useSelector } from "react-redux";
+import extendedSearchPin from "../../public/images/icons/trainingPin.svg";
+import { fetchAddresses } from "../../services/baseAdresse";
+import ReactHtmlParser from "react-html-parser";
+
+const Job = ({ job, handleSelectItem, showTextOnly, searchForTrainingsOnNewCenter }) => {
+  const { formValues } = useSelector((state) => state.trainings);
+
+  const currentSearchRadius = formValues?.radius || 30;
+
+  const onSelectItem = () => {
+    handleSelectItem(job, "peJob");
+  };
+
+  const getCenterSearchOnPeJobButton = () => {
+    return (
+      <button className="extendedTrainingSearchButton" onClick={centerSearchOnJob}>
+        <img src={extendedSearchPin} alt="" /> <span>Voir les formations proches</span>
+      </button>
+    );
+  };
+
+  const centerSearchOnJob = async (e) => {
+    if (e) {
+      e.stopPropagation();
+    }
+
+    let lT = job.place;
+
+    if (!lT.insee) {
+      const addresses = await fetchAddresses(job.place.address, "municipality");
+      lT.insee = "";
+      lT.zipCode = "";
+      if (addresses.length) {
+        lT.insee = addresses[0].insee;
+        lT.zipCode = addresses[0].zipcode;
+      }
+    }
+
+    const newCenter = {
+      insee: lT.insee,
+      label: lT.fullAddress,
+      zipcode: lT.zipCode,
+      value: {
+        type: "Point",
+        coordinates: [lT.longitude, lT.latitude],
+      },
+    };
+
+    searchForTrainingsOnNewCenter(newCenter);
+  };
+  return (
+    <div className="resultCard gtmSavoirPlus gtmPeJob gtmListe" onClick={onSelectItem}>
+      <div className="c-media" id={`${job.ideaType}${job.ideaType === "matcha" ? job.id : job.job.id}`}>
+        <div className="c-media-figure">
+          <img className="cardIcon" src={jobIcon} alt="" />
+        </div>
+
+        <div className="c-media-body">
+          <div className="title d-inline-block">
+            {job.company && job.company.name ? job.company.name : ReactHtmlParser("<i>Offre anonyme</i>")}
+          </div>
+          <div className="cardText pt-0">{job.title}</div>
+          <div className="cardText pt-2">{job.place.fullAddress}</div>
+
+          <span className="cardDistance pt-1">
+            {Math.round(job.place.distance)} km(s) du lieu de recherche
+            {showTextOnly ? (
+              ""
+            ) : (
+              <>
+                <span className="knowMore d-none d-md-block">
+                  <button className={`c-resultcard-knowmore`}>En savoir plus</button>
+                </span>
+              </>
+            )}
+          </span>
+          {Math.round(job.place.distance) > currentSearchRadius ? getCenterSearchOnPeJobButton() : ""}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Job;
