e83f9fb5eb108514ce803d07170941a07f6b398f
Alan Le Ruyet <alan.leruyet@free.fr>
2021-04-06 18:24:22 +0200

    Fin page mentions-legales

--- ui/pages/mentions-legales.js
+++ ui/pages/mentions-legales.js
@@ -118,6 +118,35 @@ const MentionsLegales = () => (
             </a>
             .
           </p>
+
+          <p>
+            Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du
+            site, merci de nous en faire part.
+            <br />
+            Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances ou
+            une demande de saisine au Défenseur des droits.
+          </p>
+
+          <h3>En savoir plus</h3>
+          <p>
+            Pour en savoir plus sur la politique d’accessibilité numérique de l’État :<br />
+            <a
+              href="http://references.modernisation.gouv.fr/accessibilite-numerique"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              http://references.modernisation.gouv.fr/accessibilite-numerique
+            </a>
+          </p>
+
+          <h3>Sécurité</h3>
+          <p>
+            Le site est protégé par un certificat électronique, matérialisé pour la grande majorité des navigateurs par
+            un cadenas. Cette protection participe à la confidentialité des échanges.
+            <br />
+            En aucun cas les services associés à au site ne seront à l’origine d’envoi de courriels pour demander la
+            saisie d’informations personnelles.
+          </p>
         </div>
       </div>
     </div>
