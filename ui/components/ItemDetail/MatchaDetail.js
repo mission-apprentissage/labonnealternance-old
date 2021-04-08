0a75c43fc26290633ad838800769ce7f001c80a8
Alan Le Ruyet <alan.leruyet@free.fr>
2021-04-08 12:06:07 +0200

    Infos contact et google pour Matcha

--- ui/components/ItemDetail/MatchaDetail.js
+++ ui/components/ItemDetail/MatchaDetail.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
-import moment from "moment";
+//import moment from "moment";
 import bulbIcon from "../../public/images/icons/bulb.svg";
-import { get, defaultTo } from "lodash";
+import { get } from "lodash";
+import contactIcon from "../../public/images/icons/contact_icon.svg";
 import ReactHtmlParser from "react-html-parser";
+import { capitalizeFirstLetter } from "../../utils/strutils";
 
 let md = require("markdown-it")().disable(["link", "image"]);
 
@@ -13,9 +15,39 @@ const MatchaDetail = ({ job, seeInfo, setSeeInfo }) => {
   }, []); // Utiliser le useEffect une seule fois : https://css-tricks.com/run-useeffect-only-once/
 
   const description = get(job, "job.description", undefined);
-  const creationDate = job?.job?.creationDate ? moment(job.job.creationDate).format("DD / MM / YYYY") : undefined;
+  //const creationDate = job?.job?.creationDate ? moment(job.job.creationDate).format("DD / MM / YYYY") : undefined;
 
   const kind = job?.ideaType;
+
+  let contactEmail = job?.contact?.email;
+  let contactPhone = job?.contact?.phone;
+
+  let contactInfo = (
+    <>
+      {contactEmail ? (
+        <p className="c-detail-km c-detail-contactlink">
+          <a href={`mailto:${contactEmail}`} className="ml-1">
+            {contactEmail}
+          </a>
+        </p>
+      ) : (
+        ""
+      )}
+      {contactPhone ? (
+        <p className="c-detail-km c-detail-contactlink">
+          <a href={`tel:${contactPhone}`} className="ml-1">
+            {contactPhone}
+          </a>
+        </p>
+      ) : (
+        ""
+      )}
+    </>
+  );
+
+  const getGoogleSearchParameters = () => {
+    return encodeURIComponent(`${job.title} ${job.place.address}`);
+  };
 
   return (
     <>
@@ -26,11 +58,6 @@ const MatchaDetail = ({ job, seeInfo, setSeeInfo }) => {
           <span className="c-detail-proposal"> propose actuellement cette offre</span>
         </div>
         <h2 className="c-detail-jobtitle">{get(job, "title", ReactHtmlParser("<em>Titre non précisé</em>"))}</h2>
-        <div className="c-detail-meta">
-          <div className="c-detail-metadate">
-            Publiée le : {defaultTo(creationDate, ReactHtmlParser("<em>Donnée manquante</em>"))}
-          </div>
-        </div>
 
         <div className="c-detail-description">
           <h3 className="c-detail-description-title">Niveau requis</h3>
@@ -45,6 +72,46 @@ const MatchaDetail = ({ job, seeInfo, setSeeInfo }) => {
         ) : (
           ""
         )}
+
+        {contactPhone || contactEmail ? (
+          <p className="d-flex mt-4">
+            {seeInfo ? (
+              <>
+                <span className="d-block">
+                  <img className="cardIcon" src={contactIcon} alt="" />
+                </span>
+                <span className="ml-2 d-block">
+                  <span className="c-detail-address d-block">{contactInfo}</span>
+                </span>
+              </>
+            ) : (
+              <button
+                className={`d-block btn btn-outline-primary w-75 ml-3 mt-3 gtmContact gtm${capitalizeFirstLetter(
+                  kind
+                )}`}
+                onClick={() => setSeeInfo(true)}
+              >
+                Voir les informations de contact
+              </button>
+            )}
+          </p>
+        ) : (
+          ""
+        )}
+        <p className="mb-4">
+          <span className="c-detail-sizetext d-block">
+            <img className="mt-n1" src="/images/square_link.svg" alt="" />
+            <span className="ml-2">En savoir plus sur </span>
+            <a
+              href={`https://www.google.fr/search?q=${getGoogleSearchParameters()}`}
+              target="_blank"
+              className="c-detail-google-search gtmGoogleLink"
+              rel="noopener noreferrer"
+            >
+              {job.title}
+            </a>
+          </span>
+        </p>
 
         <hr className="c-detail-header-separator mt-5" />
 
