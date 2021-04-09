eed07fd3695bba91dab156ca039a173c63acb29e
David Boureau <bdavidxyz@gmail.com>
2021-04-09 10:32:19 +0200

    good, with flag

--- ui/components/ItemDetail/tagCfaEntreprise.js
+++ ui/components/ItemDetail/tagCfaEntreprise.js
@@ -2,9 +2,10 @@ import React from "react";
 
 import smileyIcon from "public/images/smiley.svg";
 
-const TagCfaEntreprise = (isCfa=false) => {
+const TagCfaEntreprise = (isCfa) => {
+  
   return <>
-    {isCfa ? 
+    {isCfa === true ? 
       <span className="c-media-tag c-media-tag--smiley">
         <img src={smileyIcon} alt="sourire" />
         <span className="ml-1">CFA d'entreprise</span>
