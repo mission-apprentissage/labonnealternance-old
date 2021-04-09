13b410b18506e8ae512f914d1a4faccb34b8cb78
David Boureau <bdavidxyz@gmail.com>
2021-04-09 10:24:39 +0200

    added flag

--- ui/components/ItemDetail/tagCfaEntreprise.js
+++ ui/components/ItemDetail/tagCfaEntreprise.js
@@ -2,11 +2,17 @@ import React from "react";
 
 import smileyIcon from "public/images/smiley.svg";
 
-const TagCfaEntreprise = () => {
-  return <span className="c-media-tag c-media-tag--smiley">
-    <img src={smileyIcon} alt="sourire" />
-    <span className="ml-1">CFA d'entreprise</span>
-  </span>
+const TagCfaEntreprise = (isCfa=false) => {
+  return <>
+    {isCfa ? 
+      <span className="c-media-tag c-media-tag--smiley">
+        <img src={smileyIcon} alt="sourire" />
+        <span className="ml-1">CFA d'entreprise</span>
+      </span>
+      : 
+      ""
+    }
+  </>
 }
 
 export default TagCfaEntreprise;
