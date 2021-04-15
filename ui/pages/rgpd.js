2eee4035858ed00efb91416e30066445256195d8
David Boureau <bdavidxyz@gmail.com>
2021-04-15 11:26:26 +0200

    ul,li

--- ui/pages/rgpd.js
+++ ui/pages/rgpd.js
@@ -27,10 +27,25 @@ const RGPD = () => (
             <span className="d-block">1-5 rue du docteur Gley</span>
             <span className="d-block">75987 Paris cedex 20</span>
           </p>
+
           <h2 class="h3">Finalité</h2>
           <p>
             <span className="d-block">Le site « La Bonne Alternance » vise à faciliter les entrées en alternance des usagers en informant sur les formations en apprentissage ainsi que les offres d’emplois et entreprises auprès desquelles adresser une candidature. Le traitement de données a donc pour finalité de fournir, sur requête de l’utilisateur, des résultats de recherche pertinents correspondant aux attentes de l’usager.</span>
           </p>
+          
+          <h2 class="h3">Données à caractère personnel traitées</h2>
+          <p>
+            <span className="d-block">Sont traitées les données suivantes :</span>
+            <ul>
+              <li>domaine/métier/formation envisagé ;</li>
+              <li>niveau de diplôme souhaité ;</li>
+              <li>périmètre géographique de recherche ;</li>
+              <li>coordonnées des interlocuteurs physiques des entreprises ;</li>
+              <li>données de connexion (et notamment, les identifiants de connexion, nature des opérations, date et heure de l’opération) ;</li>
+              <li>cookies.</li>
+            </ul>
+          </p>
+          
         </div>
       </div>
     </div>
