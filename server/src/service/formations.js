5c1e6be5f59280766ec9c3258b2c4e553f41c57e
Alan Le Ruyet <alan.leruyet@free.fr>
2021-04-28 17:38:56 +0200

    Rollback sur lieu_formation_geo_coordonnees

--- server/src/service/formations.js
+++ server/src/service/formations.js
@@ -67,7 +67,7 @@ const getFormations = async ({ romes, rncps, romeDomain, coords, radius, diploma
           filter: {
             geo_distance: {
               distance: `${distance}km`,
-              idea_geo_coordonnees_etablissement: {
+              lieu_formation_geo_coordonnees: {
                 lat: coords[1],
                 lon: coords[0],
               },
@@ -78,7 +78,7 @@ const getFormations = async ({ romes, rncps, romeDomain, coords, radius, diploma
       sort: [
         {
           _geo_distance: {
-            idea_geo_coordonnees_etablissement: [parseFloat(coords[0]), parseFloat(coords[1])],
+            lieu_formation_geo_coordonnees: [parseFloat(coords[0]), parseFloat(coords[1])],
             order: "asc",
             unit: "km",
             mode: "min",
@@ -398,11 +398,11 @@ const transformFormationForIdea = (formation) => {
   resultFormation.place = {
     distance: formation.sort ? formation.sort[0] : null,
     fullAddress: getTrainingAddress(formation.source), // adresse postale reconstruite à partir des éléments d'adresse fournis
-    latitude: formation.source.idea_geo_coordonnees_etablissement
-      ? formation.source.idea_geo_coordonnees_etablissement.split(",")[0]
+    latitude: formation.source.lieu_formation_geo_coordonnees
+      ? formation.source.lieu_formation_geo_coordonnees.split(",")[0]
       : null,
-    longitude: formation.source.idea_geo_coordonnees_etablissement
-      ? formation.source.idea_geo_coordonnees_etablissement.split(",")[1]
+    longitude: formation.source.lieu_formation_geo_coordonnees
+      ? formation.source.lieu_formation_geo_coordonnees.split(",")[1]
       : null,
     //city: formation.source.etablissement_formateur_localite,
     city: formation.source.localite,
@@ -580,7 +580,7 @@ const getFormationEsQueryIndexFragment = (limit) => {
       "_id",
       "email",
       "niveau",
-      "idea_geo_coordonnees_etablissement",
+      "lieu_formation_geo_coordonnees",
       "intitule_long",
       "intitule_court",
       "lieu_formation_adresse",
