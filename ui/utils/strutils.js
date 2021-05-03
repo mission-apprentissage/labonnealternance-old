d139bcafbffe628e129bc61c651338c99d25c705
Alan Le Ruyet <alan.leruyet@free.fr>
2021-05-03 15:27:45 +0200

    fct utilisatire formatage de date

--- ui/utils/strutils.js
+++ ui/utils/strutils.js
@@ -1,6 +1,16 @@
-import _ from 'lodash'
+import _ from "lodash";
 
 const isNonEmptyString = (val) => _.isString(val) && val.trim().length > 0;
 const capitalizeFirstLetter = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
-export { isNonEmptyString, capitalizeFirstLetter };
+const formatDate = (d) => {
+  let resultDate = "";
+
+  try {
+    resultDate = new Date(d).toLocaleDateString(undefined, { year: "numeric", month: "short", day: "numeric" });
+  } catch (err) {}
+
+  return resultDate;
+};
+
+export { isNonEmptyString, capitalizeFirstLetter, formatDate };
