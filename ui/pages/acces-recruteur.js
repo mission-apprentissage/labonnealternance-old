d296460059addaebb945e94c374b20a48b46543c
David Boureau <bdavidxyz@gmail.com>
2021-05-06 10:44:57 +0200

    page ok

---
+++ ui/pages/acces-recruteur.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Navigation from "components/navigation";
+import ScrollToTop from "components/ScrollToTop";
+import Breadcrumb from "components/breadcrumb";
+
+import Footer from "components/footer";
+
+const AccesRecruteur = () => (
+  <div>
+    <ScrollToTop />
+    <Navigation />
+    <Breadcrumb forPage="acces-recruteur" label="Accès recruteur" />
+
+    <div className="c-page-container container my-0 mb-sm-5 p-5">
+      <div className="row">
+        <div className="col-12 col-md-5">
+          <h1>
+            <span className="d-block c-page-title is-color-2">Accès Recruteur</span>
+          </h1>
+          <hr className="c-page-title-separator" align="left" />
+        </div>
+
+        <div className="col-12 col-md-7">
+
+        </div>
+      </div>
+    </div>
+    <div className="mb-3">&nbsp;</div>
+    <Footer />
+  </div>
+);
+
+export default AccesRecruteur;
