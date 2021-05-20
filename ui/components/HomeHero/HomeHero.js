ebe46819446567b4cc4fa88deebdbcbea7949273
David Boureau <bdavidxyz@gmail.com>
2021-05-20 15:14:16 +0200

    centering card

--- ui/components/HomeHero/HomeHero.js
+++ ui/components/HomeHero/HomeHero.js
@@ -6,7 +6,11 @@ const HomeHero = () => {
     <div className="c-home-hero">
       <div className="container c-home-hero__container pt-5 pb-0 pb-sm-5">
         <div className="card c-home-hero__card p-0 p-sm-4">
-          <StartForm />
+          <h1 className="card-title">
+            <span className="c-home-hero__title c-home-hero__title1">Se former et travailler</span>
+            <span className="c-home-hero__title c-home-hero__title2">&nbsp;en alternance</span>
+          </h1>
+          <StartForm isHome={true} />
         </div>
       </div>
     </div>
