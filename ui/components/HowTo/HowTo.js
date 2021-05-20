f19fc17d303f09c56cedf23e0da7603d2e2e8f57
David Boureau <bdavidxyz@gmail.com>
2021-05-20 17:07:49 +0200

    better margins

--- ui/components/HowTo/HowTo.js
+++ ui/components/HowTo/HowTo.js
@@ -8,31 +8,25 @@ const HowTo = () => {
     <>
       <section className="c-howto container">
         <div className="c-howto-cards">
-          <div className="">
-            <div className="card border-0">
-              <img src={howto1} className="card-img-top" alt="Première étape"/>
-              <div className="card-body">
-                <h5 className="card-title">Le job de vos rêves</h5>
-                <p className="card-text">Renseignez le métier que vous souhaitez faire et la localisation (Ville ou Code postal)</p>
-              </div>
+          <div className="card border-0">
+            <img src={howto1} className="card-img-top" alt="Première étape"/>
+            <div className="card-body">
+              <h5 className="card-title">Le job de vos rêves</h5>
+              <p className="card-text">Renseignez le métier que vous souhaitez faire et la localisation (Ville ou Code postal)</p>
             </div>
           </div>
-          <div className="">
-            <div className="card card--middle border-0">
-              <img src={howto2} className="card-img-top" alt="Deuxième étape"/>
-              <div className="card-body">
-                <h5 className="card-title">En un clin d’oeil</h5>
-                <p className="card-text">Obtenez en un clin d’oeil la liste des formations et entreprises proche de chez vous dans le domaine recherché.</p>
-              </div>
+          <div className="card card--middle border-0">
+            <img src={howto2} className="card-img-top" alt="Deuxième étape"/>
+            <div className="card-body">
+              <h5 className="card-title">En un clin d’oeil</h5>
+              <p className="card-text">Obtenez en un clin d’oeil la liste des formations et entreprises proche de chez vous dans le domaine recherché.</p>
             </div>
           </div>
-          <div className="">
-            <div className="card border-0">
-              <img src={howto3} className="card-img-top" alt="Troisième étape"/>
-              <div className="card-body">
-                <h5 className="card-title">Un contact facile</h5>
-                <p className="card-text">Contactez facilement les centres de formations ou les entreprises pour postuler </p>
-              </div>
+          <div className="card border-0">
+            <img src={howto3} className="card-img-top" alt="Troisième étape"/>
+            <div className="card-body">
+              <h5 className="card-title">Un contact facile</h5>
+              <p className="card-text">Contactez facilement les centres de formations ou les entreprises pour postuler </p>
             </div>
           </div>
         </div>
