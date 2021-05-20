942b087d18110b51929b194d9e6dea640d93be97
David Boureau <bdavidxyz@gmail.com>
2021-05-20 11:33:58 +0200

    cleaned

--- ui/components/StartForm/StartForm.js
+++ ui/components/StartForm/StartForm.js
@@ -1,19 +1,9 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Formik, Form, ErrorMessage } from "formik";
-import {
-  AutoCompleteField,
-  compareAutoCompleteValues,
-  autoCompleteToStringFunction,
-} from "components/AutoCompleteField/AutoCompleteField";
-import { fetchAddresses } from "services/baseAdresse";
-import domainChanged from "services/domainChanged";
-import { DomainError } from "../";
+import React from "react";
+import { useDispatch } from "react-redux";
+
 import { push } from "connected-next-router";
 import { setFormValues, setShouldExecuteSearch } from "store/actions";
-import glassImage from "public/images/glass.svg";
-import localisationImage from "public/images/localisation.svg";
-import { SendTrackEvent } from "utils/gtm";
+
 import SearchForm from "components/SearchForTrainingsAndJobs/components/SearchForm";
 
 const StartForm = (props) => {
