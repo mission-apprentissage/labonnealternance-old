b16a5179778378f928029a50b4b2ce75e165732e
David Boureau <bdavidxyz@gmail.com>
2021-05-20 15:16:38 +0200

    title again

--- ui/components/HomeHero/HomeHero.js
+++ ui/components/HomeHero/HomeHero.js
@@ -7,10 +7,10 @@ const HomeHero = () => {
       <div className="container c-home-hero__container pt-5 pb-0 pb-sm-5">
         <div className="card c-home-hero__card p-0 p-sm-4">
           <h1 className="card-title">
-            <span className="c-home-hero__title c-home-hero__title1">Se former et travailler</span>
-            <span className="c-home-hero__title c-home-hero__title2">&nbsp;en alternance</span>
+            <span className="c-home-hero__title c-home-hero__title1 d-block">Se former et travailler</span>
+            <span className="c-home-hero__title c-home-hero__title2 d-block">&nbsp;en alternance</span>
           </h1>
-          <StartForm isHome={true} />
+          <StartForm/>
         </div>
       </div>
     </div>
