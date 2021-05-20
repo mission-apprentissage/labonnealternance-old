1ab0cc34dc35998a59e880fcb166db09c2468eba
David Boureau <bdavidxyz@gmail.com>
2021-05-20 15:32:54 +0200

    ok responsive search done

--- ui/components/HomeHero/HomeHero.js
+++ ui/components/HomeHero/HomeHero.js
@@ -7,8 +7,8 @@ const HomeHero = () => {
       <div className="container c-home-hero__container pt-5 pb-0 pb-sm-5">
         <div className="card c-home-hero__card p-0 p-sm-4">
           <h1 className="card-title">
-            <span className="c-home-hero__title c-home-hero__title1 d-block d-md-inline">Se former et travailler</span>
-            <span className="c-home-hero__title c-home-hero__title2 d-block d-md-inline"><span className="d-none d-md-inline">&nbsp;</span>en alternance</span>
+            <span className="c-home-hero__title c-home-hero__title1 d-block d-lg-inline">Se former et travailler</span>
+            <span className="c-home-hero__title c-home-hero__title2 d-block d-lg-inline"><span className="d-none d-lg-inline">&nbsp;</span>en alternance</span>
           </h1>
           <StartForm/>
         </div>
