7b7ec10ebe4676fca6672742ad6682daaedb4bdf
David Boureau <bdavidxyz@gmail.com>
2021-05-21 14:50:04 +0200

    corrections paddings

--- ui/components/HomeHero/HomeHero.js
+++ ui/components/HomeHero/HomeHero.js
@@ -4,7 +4,7 @@ import StartForm from "../StartForm";
 const HomeHero = () => {
   return (
     <div className="c-home-hero">
-      <div className="container c-home-hero__container pt-5 pb-0 pb-sm-5">
+      <div className="container c-home-hero__container pt-3 pt-sm-5 pb-0 pb-sm-5">
         <div className="card c-home-hero__card p-0 p-sm-4">
           <h1 className="card-title">
             <span className="c-home-hero__title c-home-hero__title1 d-block d-lg-inline">Se former et travailler</span>
