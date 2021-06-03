83526d55a4da19f34a25a42debbc2d4db82fd7a2
Alan Le Ruyet <alan.leruyet@free.fr>
2021-06-03 16:57:10 +0200

    test

--- server/src/common/utils/sendTrackingEvent.js
+++ server/src/common/utils/sendTrackingEvent.js
@@ -1,15 +1,21 @@
 const { ApiCalls } = require("../../common/model");
+const logger = require("../common/logger");
 
 const trackEvent = async ({ action, label, value }) => {
   //visitor.event(category, action, label, value).send();
 
-  let apiCall = new ApiCalls({
-    caller: label,
-    api: action,
-    result_count: value,
-  });
+  logger.info("about to save event");
+  try {
+    let apiCall = new ApiCalls({
+      caller: label,
+      api: action,
+      result_count: value,
+    });
 
-  apiCall.save();
+    apiCall.save();
+  } catch (err) {
+    logger.error(`error saving event - ${err.message}`);
+  }
 };
 
 module.exports = { trackEvent };
