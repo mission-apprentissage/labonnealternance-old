6bfa211eb66a64a4b3e7ae8ea7e3a44e01c7c57b
David Boureau <bdavidxyz@gmail.com>
2021-06-10 10:34:51 +0200

    styling card

--- ui/pages/organisme-de-formation.js
+++ ui/pages/organisme-de-formation.js
@@ -24,6 +24,16 @@ const Organisme = (props) => {
             <hr className="c-page-title-separator" align="left" />
           </div>
           <div className="col-12 col-md-7">
+            <div className="c-organisme-hero py-4 px-5">
+              <h2 className="c-organisme-hero-title">Exprimez votre besoin en alternance</h2>
+              <p className="c-organisme-hero-text">Notre service Matcha vous permet de publier en quelques minutes vos offres sur <strong>La Bonne Alternance.</strong></p>
+              <div>
+                <button className="btn btn-primary ml-1">Publier une offre d'emploi en alternance</button>
+              </div>
+              <div>
+                <small>Aucune inscription ne vous sera demandé</small>
+              </div>
+            </div>
             <p>La Bonne Alternance expose des données provenant du réseau Carif-oref.</p>
             <p>Pour référencer votre formation, rendez-vous sur le site du Carif-Oref :</p>
             <p className="c-organisme-link">
