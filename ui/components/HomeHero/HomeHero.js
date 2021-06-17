69f09a0ff2c99beb9a42184f81b78290ae3fc476
David Boureau <bdavidxyz@gmail.com>
2021-06-17 11:18:21 +0200

    title aligned

--- ui/components/HomeHero/HomeHero.js
+++ ui/components/HomeHero/HomeHero.js
@@ -6,10 +6,6 @@ const HomeHero = () => {
     <div className="c-home-hero">
       <div className="container c-home-hero__container pt-3 pt-sm-5 pb-0 pb-sm-5">
         <div className="card c-home-hero__card p-0 p-sm-4">
-          <h1 className="card-title">
-            <span className="c-home-hero__title c-home-hero__title1 d-block d-lg-inline">Se former et travailler</span>
-            <span className="c-home-hero__title c-home-hero__title2 d-block d-lg-inline"><span className="d-none d-lg-inline">&nbsp;</span>en alternance</span>
-          </h1>
           <StartForm/>
         </div>
       </div>
