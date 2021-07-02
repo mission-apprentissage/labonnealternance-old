2c5433bc23c0fc1008005b366061397f9cd7e7f2
David Boureau <bdavidxyz@gmail.com>
2021-07-02 15:18:47 +0200

    extract per line

--- ui/pages/catalog.js
+++ ui/pages/catalog.js
@@ -20,15 +20,24 @@ export default function Catalog(props) {
   )
 }
 
+// See https://nextjs.org/learn/basics/data-fetching/with-data
+// Static data, please restart nextjs each time this function change
 export async function getStaticProps() {
+  const path = require('path');
+  const fs = require('fs');
+  const postsDirectory = path.join(process.cwd(), 'config')
+  const filePath = path.join(postsDirectory, 'metiers.txt')
+  const lineString = fs.readFileSync(filePath, 'utf8')
+  const arrayOfLines = lineString.match(/[^\r\n]+/g);
   // Get external data from the file system, API, DB, etc.
-  const data = 42;
+  const data = 48;
 
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props: {
-      dataval: 45
+      dataval: data,
+      datatxt: arrayOfLines
     }
   }
 }
