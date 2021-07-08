1818cb6750607532142d6f63d97d804a8989df15
Alan Le Ruyet <alan.leruyet@free.fr>
2021-07-08 15:09:26 +0200

    Gestion de la taille du sessionStorage

--- ui/components/SearchForTrainingsAndJobs/services/handleSessionStorage.js
+++ ui/components/SearchForTrainingsAndJobs/services/handleSessionStorage.js
@@ -1,13 +1,40 @@
 import { setJobMarkers, setTrainingMarkers, factorTrainingsForMap, factorJobsForMap } from "utils/mapTools";
 
 export const storeTrainingsInSession = ({ trainings, searchTimestamp }) => {
-  let search = JSON.parse(sessionStorage.getItem(searchTimestamp));
-  sessionStorage.setItem(searchTimestamp, JSON.stringify({ trainings, ...search }));
+  try {
+    let search = JSON.parse(sessionStorage.getItem(searchTimestamp));
+    sessionStorage.setItem(searchTimestamp, JSON.stringify({ trainings, ...search }));
+    trimSessionStorage();
+  } catch (err) {
+    console.log("sessionStorage error : ", err);
+  }
 };
 
 export const storeJobsInSession = ({ jobs, searchTimestamp }) => {
-  let search = JSON.parse(sessionStorage.getItem(searchTimestamp));
-  sessionStorage.setItem(searchTimestamp, JSON.stringify({ jobs, ...search }));
+  try {
+    let search = JSON.parse(sessionStorage.getItem(searchTimestamp));
+    sessionStorage.setItem(searchTimestamp, JSON.stringify({ jobs, ...search }));
+    trimSessionStorage();
+  } catch (err) {
+    console.log("sessionStorage error : ", err);
+  }
+};
+
+const trimSessionStorage = () => {
+  let oldest = 0;
+
+  if (sessionStorage.length > 15) {
+    for (let i = 0, l = sessionStorage.length; i < l; ++i) {
+      let currentKey = parseInt(sessionStorage.key(i));
+      if (!oldest || currentKey < oldest) {
+        oldest = currentKey;
+      }
+    }
+
+    if (oldest) {
+      sessionStorage.removeItem(oldest);
+    }
+  }
 };
 
 export const restoreSearchFromSession = ({ searchTimestamp, dispatch, setTrainings, setJobs }) => {
