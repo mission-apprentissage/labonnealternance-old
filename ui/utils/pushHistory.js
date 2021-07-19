d98a78163f885ac01693d6962065c69754e02d4c
Alan Le Ruyet <alan.leruyet@free.fr>
2021-07-19 10:35:21 +0200

    Conservation des paramètres utm_ (#350)

--- ui/utils/pushHistory.js
+++ ui/utils/pushHistory.js
@@ -1,10 +1,11 @@
 import { getItemQueryParameters } from "utils/getItemId";
 import { getSearchQueryParameters } from "utils/getSearchParameters";
+import { getCampaignParameters } from "utils/campaignParameters";
 
 const pushHistory = ({ router, scopeContext, item, page, display, searchParameters }) => {
   let params = `${display ? `&display=${display}` : ""}${page ? `&page=${page}` : ""}${
     item ? `&${getItemQueryParameters(item)}` : ""
-  }${searchParameters ? `&${getSearchQueryParameters(searchParameters)}` : ""}`;
+  }${searchParameters ? `&${getSearchQueryParameters(searchParameters)}` : ""}${getCampaignParameters()}`;
 
   router.push(`${scopeContext.path}${params ? `?${params}` : ""}`, undefined, {
     shallow: true,
