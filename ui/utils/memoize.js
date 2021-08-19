ec9551afe2f8c88302bd39015e4de0848c73440f
Alan Le Ruyet <alan.leruyet@free.fr>
2021-08-19 14:48:07 +0200

    notes

--- ui/utils/memoize.js
+++ ui/utils/memoize.js
@@ -1,12 +1,14 @@
+/* simple fonction de memoization utilisant tous les paramètres d'appels de la fonction sous jacente en clef
+Note : la fonction memoize de lodash n'utilise que le premier argument comme clef de cache ce qui n'est pas suffisant ici
+source : https://www.section.io/engineering-education/an-introduction-to-memoization-in-javascript/
+ */
+
 const memoize = (func) => {
   const results = {};
   return (...args) => {
     const argsKey = JSON.stringify(args);
     if (!results[argsKey]) {
-      console.log("pas memo");
       results[argsKey] = func(...args);
-    } else {
-      console.log("memo");
     }
     return results[argsKey];
   };
