c280fc70a16ecdf50c4c43c653106013c9c9b3f8
Alan Le Ruyet <alan.leruyet@free.fr>
2021-08-19 17:42:36 +0200

    Fix tests

--- ui/src/tests/fetchDiplomas.test.js
+++ ui/src/tests/fetchDiplomas.test.js
@@ -15,21 +15,11 @@ describe("fetchDiplomas", () => {
 
   it("nominal : Should return response.data if remote API replied correctly", async () => {
     // given
-    const mockedErrorFn = jest.fn();
     const mockedAxiosGet = jest.fn().mockReturnValue({ data: ["remotely_returned_array"] });
     const axiosStub = { get: mockedAxiosGet };
     // when
-    const res = await fetchDiplomas(
-      ["D1208", "D1203"],
-      ["D1208", "D1203"],
-      mockedErrorFn,
-      "urlMock",
-      axiosStub,
-      {},
-      _.noop
-    );
+    const res = await fetchDiplomas(["D1208", "D1203"], ["D1208", "D1203"], "urlMock", axiosStub, {}, _.noop);
     // then
-    expect(mockedErrorFn).not.toHaveBeenCalled();
     expect(mockedAxiosGet).toHaveBeenCalledWith("urlMock/api/jobsdiplomas", {
       params: { romes: "D1208,D1203", rncps: "D1208,D1203" },
     });
@@ -38,63 +28,40 @@ describe("fetchDiplomas", () => {
 
   it("error case : axios returns an non-empty data.error property", async () => {
     // given
-    const mockedErrorFn = jest.fn();
     const mockedLoggerFn = jest.fn();
     const axiosStub = { get: jest.fn().mockReturnValue({ data: { error: "remote_error_message" } }) };
     // when
-    const res = await fetchDiplomas(
-      ["D1208", "D1203"],
-      ["D1208", "D1203"],
-      mockedErrorFn,
-      "urlMock",
-      axiosStub,
-      {},
-      mockedLoggerFn
-    );
+    const res = await fetchDiplomas(["D1208", "D1203"], ["D1208", "D1203"], "urlMock", axiosStub, {}, mockedLoggerFn);
     // then
-    expect(mockedErrorFn).toHaveBeenCalled();
     expect(mockedLoggerFn).toHaveBeenCalledWith("Diploma API error", "Diploma API error");
     expect(res).toEqual([]);
   });
 
   it("error case : axios do NOT returns expected data", async () => {
     // given
-    const mockedErrorFn = jest.fn();
     const mockedLoggerFn = jest.fn();
     const axiosStub = { get: jest.fn().mockReturnValue({ data: { unexpected_prop: "unexpected_val" } }) };
     // when
-    const res = await fetchDiplomas(
-      ["D1208", "D1203"],
-      ["D1208", "D1203"],
-      mockedErrorFn,
-      "urlMock",
-      axiosStub,
-      {},
-      mockedLoggerFn
-    );
+    const res = await fetchDiplomas(["D1208", "D1203"], ["D1208", "D1203"], "urlMock", axiosStub, {}, mockedLoggerFn);
     // then
-    expect(mockedErrorFn).toHaveBeenCalled();
     expect(mockedLoggerFn).toHaveBeenCalledWith("Diploma API error : API call worked, but returned unexpected data");
     expect(res).toEqual([]);
   });
 
   it("error case : user simulated an error through URL query param", async () => {
     // given
-    const mockedErrorFn = jest.fn();
     const mockedLoggerFn = jest.fn();
     const axiosStub = { get: jest.fn().mockReturnValue({ data: ["remotely_returned_array"] }) };
     // when
     const res = await fetchDiplomas(
       ["D1208", "D1203"],
       ["D1208", "D1203"],
-      mockedErrorFn,
       "urlMock",
       axiosStub,
       { location: { href: "anyurl.com?diplomaError=true" } },
       mockedLoggerFn
     );
     // then
-    expect(mockedErrorFn).toHaveBeenCalled();
     expect(mockedLoggerFn).toHaveBeenCalledWith("Diploma API error simulated with a query param :)");
     expect(res).toEqual([]);
   });
