e35e5872e8d8c7cb0054615d35659bc607cbabc5
David Boureau <bdavidxyz@gmail.com>
2021-08-20 17:36:30 +0200

    back to naive impl

--- ui/components/ItemDetail/GoingToContactQuestion.js
+++ ui/components/ItemDetail/GoingToContactQuestion.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import thumbup from "public/images/thumbup.svg";
 import thumbdown from "public/images/thumbdown.svg";
 import { capitalizeFirstLetter } from "utils/strutils";
 
 const GoingToContactQuestion = ({ kind }) => {
+
+  const [sayThanks, setSayThanks] = useState(false);
+
+
   return (
     <div className="c-goingto mt-4">
       <span className="c-goingto-title">Allez-vous contacter cet établissement ?</span>
       <div className="d-flex-center mt-2">
-        <button type="button" className={`c-goingto-thumb gtmThumbUp gtm${capitalizeFirstLetter(kind)}`}>
-          <img src={thumbup} alt="oui : pouce vers le haut" />
-          <span className="ml-1">Oui</span>
-        </button>
-        <button type="button" className={`c-goingto-thumb ml-2 gtmThumbDown gtm${capitalizeFirstLetter(kind)}`}>
-          <img src={thumbdown} alt="non : pouce vers le bas" />
-          <span className="ml-1">Non</span>
-        </button>
+        {sayThanks ? (
+          <>
+            <span>Merci pour votre retour !</span>
+          </>
+        ) : (
+          <>
+            <button
+              type="button"
+              className={`c-goingto-thumb gtmThumbUp gtm${capitalizeFirstLetter(kind)}`}
+              onClick={() => {
+                setSayThanks(true);
+              }}>
+              <img src={thumbup} alt="oui : pouce vers le haut" />
+              <span className="ml-1">Oui</span>
+            </button>
+            <button
+              type="button"
+              className={`c-goingto-thumb ml-2 gtmThumbDown gtm${capitalizeFirstLetter(kind)}`}
+              onClick={() => {
+                setSayThanks(true);
+              }}>
+              <img src={thumbdown} alt="non : pouce vers le bas" />
+              <span className="ml-1">Non</span>
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
