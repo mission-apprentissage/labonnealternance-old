1053698c12d7a5637bd3428e5ee822739ef80efd
David Boureau <bdavidxyz@gmail.com>
2021-08-20 11:43:10 +0200

    reorg

--- ui/components/ItemDetail/GoingToContactQuestion.js
+++ ui/components/ItemDetail/GoingToContactQuestion.js
@@ -6,11 +6,11 @@ import thumbdown from "public/images/thumbdown.svg";
 const GoingToContactQuestion = ({  }) => {
   return (
     <div className="">
-      <button type="button" className="c-button-thumb">
+      <button type="button" className="c-goingto-thumb">
         <img src={thumbup} alt="oui : pouce vers le haut" />
         <span className="ml-1">Oui</span>
       </button>
-      <button type="button" className="c-button-thumb ml-2">
+      <button type="button" className="c-goingto-thumb ml-2">
         <img src={thumbdown} alt="non : pouce vers le bas" />
         <span className="ml-1">Non</span>
       </button>
