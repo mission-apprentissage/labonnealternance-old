862476335948001b873ea826c423d54c90879019
David Boureau <bdavidxyz@gmail.com>
2021-08-25 12:19:01 +0200

    ok test pass

--- ui/src/tests/useSessionStorage.test.js
+++ ui/src/tests/useSessionStorage.test.js
@@ -29,9 +29,16 @@ describe('useSessionStorage', () => {
     const { result } = renderHook(() => useSessionStorage('the_key', {a: 'default_value'}, fakeSessionStorage))
     expect(result.current[0]).toEqual({ a: "default_value"});
   });
-  it('Returns the stored value if matching key was found inside storage', () => {
+  it('Returns a JSON-parsed, stored value if matching key was found inside storage', () => {
     fakeSessionStorage.setItem('the_key', '{ "a": "predefined_value" }')
     const { result } = renderHook(() => useSessionStorage('the_key', { a: 'default_value' }, fakeSessionStorage))
     expect(result.current[0]).toEqual({ a: "predefined_value" });
+  });
+  it('When acting, session Storage is filled', () => {
+    const { result } = renderHook(() => useSessionStorage('the_key', { a: 'default_value' }, fakeSessionStorage))
+    act(() => {
+      result.current[1]({ a: 'fresh_new_value' })
+    })
+    expect(result.current[0]).toEqual({ a: "fresh_new_value" });
   });
 });
