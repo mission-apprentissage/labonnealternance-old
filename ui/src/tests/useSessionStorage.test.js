3c04326be3e3f2662b6eb1ebe78a17af279a505c
David Boureau <bdavidxyz@gmail.com>
2021-08-25 12:04:33 +0200

    passing tests

--- ui/src/tests/useSessionStorage.test.js
+++ ui/src/tests/useSessionStorage.test.js
@@ -1,10 +1,42 @@
 import { renderHook, act } from '@testing-library/react-hooks'
-import {useSessionStorage} from '../../utils/useSessionStorage'
+import { useSessionStorage } from '../../utils/useSessionStorage'
+import { noop } from 'lodash'
 
+
+function buildFakeStorage() {
+  let storage = {};
+
+  return {
+    setItem: function (key, value) {
+      storage[key] = value || '';
+    },
+    getItem: function (key) {
+      return key in storage ? storage[key] : null;
+    }
+  };
+}
 
 describe('useSessionStorage', () => {
 
-  it('nominal case', () => {
-    expect(2 + 2).toEqual(4);
+  // given
+  let fakeSessionStorage = null
+  
+  beforeEach(() => {
+    fakeSessionStorage = buildFakeStorage()
+  });
+
+  it('Returns the supplied default value is nothing is stored previously', () => {
+  
+    const {result} = renderHook(() => useSessionStorage('the_key', 'the_default_value', fakeSessionStorage))
+
+    expect(result.current[0]).toEqual('the_default_value');
+  });
+  it('Returns the stored value if matching key was found inside storage', () => {
+  
+    fakeSessionStorage.setItem('the_key', JSON.stringify('a_predefined_value'))
+
+    const {result} = renderHook(() => useSessionStorage('the_key', 'the_default_value', fakeSessionStorage))
+
+    expect(result.current[0]).toEqual('a_predefined_value');
   });
 });
--- ui/utils/useSessionStorage.js
+++ ui/utils/useSessionStorage.js
@@ -1,20 +1,24 @@
 import { useState, useEffect } from 'react';
 
-function getSessionStorageOrDefault(key, defaultValue) {
-  const stored = sessionStorage.getItem(key);
+function getSessionStorageOrDefault(key, defaultValue, stubbedSessionStorage = null) {
+  let localSessionStorage = stubbedSessionStorage || sessionStorage
+  const stored = localSessionStorage.getItem(key);
   if (!stored) {
     return defaultValue;
   }
   return JSON.parse(stored);
 }
 
-export function useSessionStorage(key, defaultValue) {
+export function useSessionStorage(key, defaultValue, stubbedSessionStorage = null) {
+
+  let localSessionStorage = stubbedSessionStorage || sessionStorage
+
   const [value, setValue] = useState(
-    getSessionStorageOrDefault(key, defaultValue)
+    getSessionStorageOrDefault(key, defaultValue, stubbedSessionStorage)
   );
 
   useEffect(() => {
-    sessionStorage.setItem(key, JSON.stringify(value));
+    localSessionStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
   return [value, setValue];
