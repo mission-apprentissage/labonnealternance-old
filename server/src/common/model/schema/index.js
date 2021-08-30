4f7814772e8f3c1876912d45eba8cf5a3b4408a7
Alan Le Ruyet <alan.leruyet@free.fr>
2021-08-30 15:43:44 +0200

    Schéma mnaFormation

--- server/src/common/model/schema/index.js
+++ server/src/common/model/schema/index.js
@@ -1,6 +1,9 @@
 const domainesMetiersSchema = require("./domainesmetiers");
 const apiCallSchema = require("./ApiCall");
+const mnaFormationSchema = require("./mnaFormation/mnaFormation");
+
 module.exports = {
   domainesMetiersSchema,
   apiCallSchema,
+  mnaFormationSchema,
 };
