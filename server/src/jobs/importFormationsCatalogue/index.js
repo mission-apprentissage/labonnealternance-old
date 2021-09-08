ffb572e5152e6633feaed971f1d629f19213d0c1
Alan Le Ruyet <alan.leruyet@free.fr>
2021-09-08 15:03:58 +0200

    import catalogue complet

--- server/src/jobs/importFormationsCatalogue/index.js
+++ server/src/jobs/importFormationsCatalogue/index.js
@@ -2,5 +2,5 @@ const importFormationsCatalogue = require("./importFormationsCatalogue");
 const { runScript } = require("../scriptWrapper");
 
 runScript(async () => {
-  await importFormationsCatalogue({ onlyChangeMasterIndex: true });
+  await importFormationsCatalogue();
 });
