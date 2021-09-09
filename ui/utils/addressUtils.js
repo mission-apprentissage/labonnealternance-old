e5d075da35e44eac46805b11c497b889fb42b274
David Boureau <bdavidxyz@gmail.com>
2021-09-09 11:55:22 +0200

    another utils

---
+++ ui/utils/addressUtils.js
@@ -0,0 +1,23 @@
+import _ from "lodash";
+import { isNonEmptyString, countInstances } from "./strutils";
+
+/*
+* Permet de ne retenir QUE l'adresse postale,
+* sans le nom du destinataire.
+*
+* Exemple : Service Ressource Humaine, 2 rue Truc, 32300 Mouches
+* Devient : 2 rue Truc, 32300 Mouches
+*/
+const rawPostalAddress = (address) => {
+  let result = ''
+  if (isNonEmptyString(address)) {
+    if (countInstances(address, ' ,') > 1) {
+      let elements = address.split(' ,');
+      elements.shift() // Retire le premier élément : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+      result = elements.join()
+    }
+  }
+  return result;
+}
+
+export { rawPostalAddress };
