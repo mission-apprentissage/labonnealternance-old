bc58bdcb66c8d83aaf047136e842302dc1575975
Alan Le Ruyet <alan.leruyet@free.fr>
2021-09-13 16:13:16 +0200

    Nouveau code google-site-verification

--- ui/components/head.js
+++ ui/components/head.js
@@ -25,7 +25,7 @@ const HeadLaBonneAlternance = (props) => {
         name="description"
         content="Vous ne trouvez pas de contrat ou d'offres d'alternance ? Essayez La Bonne Alternance ! Trouvez ici les formations en alternance et les entreprises qui recrutent régulièrement en alternance"
       />
-      <meta name="google-site-verification" content="jn0jqDBg1s2kxFXJTtRsGBTNUfBI8PErYF-_YgxxHDk" />
+      <meta name="google-site-verification" content="neOTrE-YKZ9LbgLlaX8UkYN6MJTPlWpeotPQqbrJ19Q" />
       <meta property="og:site_name" content="La Bonne Alternance" />
       <meta property="og:title" content="La Bonne Alternance - Trouvez votre alternance" />
       <meta property="og:type" content="site" />
