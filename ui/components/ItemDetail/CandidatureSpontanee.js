0a3e135479c8e03c0956afedad0eb06e7cef67d8
David Boureau <bdavidxyz@gmail.com>
2021-09-16 15:38:20 +0200

    header ok

--- ui/components/ItemDetail/CandidatureSpontanee.js
+++ ui/components/ItemDetail/CandidatureSpontanee.js
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const CandidatureSpontanee = (props) => {
-  const {
-    buttonLabel,
-    className
-  } = props;
 
   const [modal, setModal] = useState(false);
 
@@ -13,7 +9,6 @@ const CandidatureSpontanee = (props) => {
 
   return (
     <div className="c-candidature">
-
 
       <div className="c-detail-description-me col-12 col-md-5">
         <div className="c-detail-pelink my-3">
@@ -23,9 +18,8 @@ const CandidatureSpontanee = (props) => {
         </div>
       </div>
 
-
-      <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+      <Modal isOpen={modal} toggle={toggle} className={"c-candidature-modal"}>
+        <ModalHeader toggle={toggle} className={"c-candidature-modal-header"}></ModalHeader>
         <ModalBody>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </ModalBody>
