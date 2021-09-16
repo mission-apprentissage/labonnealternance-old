43f41bdeccdcfb32db1dd50301e59381bb9ac442
David Boureau <bdavidxyz@gmail.com>
2021-09-16 15:45:35 +0200

    one border less

--- ui/components/ItemDetail/CandidatureSpontanee.js
+++ ui/components/ItemDetail/CandidatureSpontanee.js
@@ -21,6 +21,7 @@ const CandidatureSpontanee = (props) => {
       <Modal isOpen={modal} toggle={toggle} className={"c-candidature-modal"}>
         <ModalHeader toggle={toggle} className={"c-candidature-modal-header"}></ModalHeader>
         <ModalBody>
+          <h1 className="c-candidature-title">Candidature spontanée</h1>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </ModalBody>
         <ModalFooter>
