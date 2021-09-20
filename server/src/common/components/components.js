e240f6d02eaab92d7e29d48f9cfdda751d9dc120
Alan Le Ruyet <alan.leruyet@free.fr>
2021-09-20 18:51:55 +0200

    Initialisation mailer

--- server/src/common/components/components.js
+++ server/src/common/components/components.js
@@ -1,7 +1,10 @@
 const { connectToMongo } = require("../mongodb");
+const createMailer = require("../../common/mailer");
+const config = require("config");
 
 module.exports = async (options = {}) => {
   return {
     db: options.db || (await connectToMongo()).db,
+    mailer: options.mailer || createMailer({ smtp: { ...config.smtp, secure: false } }),
   };
 };
---
+++ server/src/common/mailer.js
@@ -0,0 +1,46 @@
+const nodemailer = require("nodemailer");
+const htmlToText = require("nodemailer-html-to-text").htmlToText;
+const mjml = require("mjml");
+const { promisify } = require("util");
+const ejs = require("ejs");
+const renderFile = promisify(ejs.renderFile);
+
+/**
+ * @description Create transporter.
+ * @param {Object} smtp
+ * @param {String} smtp.host
+ * @param {String} smtp.port
+ * @param {Object} smtp.auth
+ * @param {String} smtp.user
+ * @param {String} smtp.pass
+ * @returns {Mail}
+ */
+const createTransporter = (smtp) => {
+  const transporter = nodemailer.createTransport(smtp);
+
+  transporter.use("compile", htmlToText({ ignoreImage: true }));
+
+  return transporter;
+};
+
+module.exports = (config, transporter = createTransporter(config.smtp)) => {
+  const renderEmail = async (template, data = {}) => {
+    const buffer = await renderFile(template, { data });
+    const { html } = mjml(buffer.toString(), { minify: true });
+
+    return html;
+  };
+
+  return {
+    renderEmail,
+    sendEmail: async (to, subject, template, data) => {
+      return transporter.sendMail({
+        from: "no-reply@apprentissage.beta.gouv.fr",
+        to,
+        subject,
+        html: await renderEmail(template, data),
+        list: {},
+      });
+    },
+  };
+};
