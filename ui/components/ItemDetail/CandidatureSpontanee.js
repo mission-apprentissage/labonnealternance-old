36471d4adb1264884ccb797f6be7584162cae0a5
David Boureau <bdavidxyz@gmail.com>
2021-09-23 10:55:04 +0200

    only one button

--- ui/components/ItemDetail/CandidatureSpontanee.js
+++ ui/components/ItemDetail/CandidatureSpontanee.js
@@ -34,7 +34,6 @@ const CandidatureSpontanee = (props) => {
             <ModalHeader toggle={toggle} className={"c-candidature-modal-header"}></ModalHeader>
             <ModalBody>
               <h1 className="c-candidature-title">Candidature spontanée</h1>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor   incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 <label htmlFor="email">Email Address</label>
                 <input
                   id="email"
@@ -46,8 +45,7 @@ const CandidatureSpontanee = (props) => {
 
             </ModalBody>
             <ModalFooter>
-                <Button type="submit" color="btn btn-dark btn-dark-action" onClick={toggle}>Je postule</Button>{' '}
-                <button type="submit">Submit</button>
+              <button className="btn btn-dark btn-dark-action" type="submit">Submit</button>
             </ModalFooter>
           </form>
       </Modal>
