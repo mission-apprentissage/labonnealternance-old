1d4093729f5a21245ee8d7a3e188ac3c7490a852
David Boureau <bdavidxyz@gmail.com>
2021-09-23 11:11:16 +0200

    added yup

--- ui/components/ItemDetail/CandidatureSpontanee.js
+++ ui/components/ItemDetail/CandidatureSpontanee.js
@@ -4,6 +4,7 @@ import { useFormik } from 'formik';
 
 const validate = values => {
   const errors = {};
+
   if (!values.firstName) {
     errors.firstName = 'Required';
   } else if (values.firstName.length > 15) {
@@ -60,35 +61,44 @@ const CandidatureSpontanee = (props) => {
             <ModalBody>
               <h1 className="c-candidature-title">Candidature spontanée</h1>
 
-              <label htmlFor="firstName">First Name</label>
-              <input
-                id="firstName"
-                name="firstName"
-                type="text"
-                onChange={formik.handleChange}
-                value={formik.values.firstName}
-              />
-              {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+            <label htmlFor="firstName">First Name</label>
+            <input
+              id="firstName"
+              name="firstName"
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.firstName}
+            />
+            {formik.touched.firstName && formik.errors.firstName ? (
+              <div>{formik.errors.firstName}</div>
+            ) : null}
 
-              <label htmlFor="lastName">Last Name</label>
-              <input
-                id="lastName"
-                name="lastName"
-                type="text"
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
-              />
-              {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              id="lastName"
+              name="lastName"
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.lastName}
+            />
+            {formik.touched.lastName && formik.errors.lastName ? (
+              <div>{formik.errors.lastName}</div>
+            ) : null}
 
-              <label htmlFor="email">Email Address</label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-              />
-              {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+            <label htmlFor="email">Email Address</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div>{formik.errors.email}</div>
+            ) : null}
 
             </ModalBody>
             <ModalFooter>
