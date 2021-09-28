7879f2b668c640180cd1f7a4cc4af0f3751431d1
Alan Le Ruyet <alan.leruyet@free.fr>
2021-09-28 16:46:02 +0200

    Template AR candidature (#411)

--- server/src/service/applications.js
+++ server/src/service/applications.js
@@ -5,7 +5,8 @@ const { Application } = require("../common/model");
 
 const images = {
   images: {
-    logo: `${config.publicUrl}/images/emails/logo_lba.png?raw=true`,
+    //logo: `${config.publicUrl}/images/emails/logo_lba.png`,
+    logo: `https://labonnealternance-recette.apprentissage.beta.gouv.fr/images/emails/logo_lba.png`,
   },
 };
 
@@ -25,32 +26,36 @@ const sendApplication = async ({ mailer, query, shouldCheckSecret }) => {
         company_siret: query.company_siret,
         company_email: query.company_email,
         company_name: query.company_name,
+        company_naf: query.company_naf,
+        company_address: query.company_address,
       });
 
+      //console.log("application ", { ...application._doc, ...images }, { ...application._doc, ...images }.images.logo);
+
       // Sends ack email to "candidate" and application email to "company"
-      const [emailCandidat, emailCompany] = await Promise.all([
-        mailer.sendEmail(
+      const [/*emailCandidat,*/ emailCompany] = await Promise.all([
+        /*mailer.sendEmail(
           application.applicant_email,
           `Votre candidature chez ${application.company_email}`,
           getEmailTemplate("mail-candidat"),
-          { ...application, ...images }
-        ),
+          { ...application._doc, ...images }
+        ) ,*/
         mailer.sendEmail(
           application.company_email,
           `Candidature spontanée via La bonne alternance`,
           getEmailTemplate("mail-spontanee"),
-          { ...application, ...images }
+          { ...application._doc, ...images }
         ),
       ]);
 
-      application.to_applicant_message_id = emailCandidat.messageId;
-      application.to_applicant_message_status = emailCandidat.accepted.length ? "accepted" : "rejected";
+      //application.to_applicant_message_id = emailCandidat.messageId;
+      //application.to_applicant_message_status = emailCandidat.accepted.length ? "accepted" : "rejected";
       application.to_company_message_id = emailCompany.messageId;
       application.to_company_message_status = emailCompany.accepted.length ? "accepted" : "rejected";
 
       await application.save();
 
-      return { emailCandidat, emailCompany, application };
+      return { /*emailCandidat , */ emailCompany, application };
     } catch (err) {
       Sentry.captureException(err);
       return { error: "error_sending_application" };
