1d2de44a7ca0c844f05d61847c788ced26238964
Alan Le Ruyet <alan.leruyet@free.fr>
2021-10-05 17:30:08 +0200

    Récupération du naf et de l'adresse société

--- ui/services/extractCompanyValues.js
+++ ui/services/extractCompanyValues.js
@@ -1,9 +1,10 @@
-
 export default function extractCompanyValues(item_h) {
-  let res = {}
-  let contact_h = item_h?.contact || {}
-  let company_h = item_h?.company || {}
-  res = { ...contact_h, ...company_h }
+  let res = {};
+  let contact_h = item_h?.contact || {};
+  let company_h = item_h?.company || {};
+  company_h.naf = item_h?.nafs && item_h.nafs.length ? item_h.nafs[0].label : null;
+  company_h.address = item_h?.place?.fullAddress || {};
+
+  res = { ...contact_h, ...company_h };
   return res;
 }
-
