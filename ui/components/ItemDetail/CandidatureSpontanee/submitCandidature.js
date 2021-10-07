09edfafae05e387c14951883f8231978e7e71f0b
David Boureau <bdavidxyz@gmail.com>
2021-10-07 16:19:57 +0200

    cleaned2

--- ui/components/ItemDetail/CandidatureSpontanee/submitCandidature.js
+++ ui/components/ItemDetail/CandidatureSpontanee/submitCandidature.js
@@ -5,12 +5,10 @@ import { noop } from "lodash/noop";
 export default async function submitCandidature(applicantValues, setSendingState=noop, item={}) {
   setSendingState('currently_sending')
   let success = true
-  console.log('success', success);
 
   try {
     await postCandidature(applicantValues, extractCompanyValues(item));
   } catch (error) {
-    console.log('error', error);
     success = false
   }
 
