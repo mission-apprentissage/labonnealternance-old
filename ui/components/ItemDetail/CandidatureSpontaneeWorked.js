98b5218dad8757531914a1ee66a8a43effe08d60
David Boureau <bdavidxyz@gmail.com>
2021-10-07 15:38:40 +0200

    ok screen better

--- ui/components/ItemDetail/CandidatureSpontaneeWorked.js
+++ ui/components/ItemDetail/CandidatureSpontaneeWorked.js
@@ -1,6 +1,7 @@
 import React from "react";
-import CandidatureSpontaneeSubmit from "./CandidatureSpontaneeSubmit";
 import { ModalBody, ModalFooter } from "reactstrap";
+import paperplaneIcon from "public/images/paperplane2.svg";
+
 
 const CandidatureSpontaneeWorked = ({ formik, loadingState}) => {
 
@@ -9,14 +10,24 @@ const CandidatureSpontaneeWorked = ({ formik, loadingState}) => {
           <ModalBody>
             <h1 className="c-candidature-title">Candidature spontanée</h1>
 
-            <h2>
-              Votre candidature a bien été envoyée à l’entreprise.
-            </h2>
-            <div>
-              Un e-mail de confirmation vous a été envoyé sur votre boite e-mail julien.cudot@gmail.comSi vous n'avez pas reçu d'email de confirmation d'ici 24 heures, soumettez à nouveau votre candidature
+            <div className="c-candidature-worked-header d-flex my-5">
+              <div>
+                <img src={paperplaneIcon} alt="avion en papier" />
+              </div>
+              <div className="ml-3">
+                <h2 className="c-candidature-worked-title">
+                  Votre candidature a bien été envoyée à l’entreprise.
+                </h2>
+              </div>
+            </div>
+            <div className="c-candidature-worked-text">
+              Un e-mail de confirmation vous a été envoyé sur votre boite e-mail.
+            </div>
+            <div className="c-candidature-worked-text mt-3 mb-5">
+              Si vous n'avez pas reçu d'email de confirmation d'ici 24 heures, soumettez à nouveau votre candidature
             </div>
           </ModalBody>
-          <ModalFooter>
+          <ModalFooter className="pb-5">
             
           </ModalFooter>
     </>
