753ddc54a9381f321847b2d7a47cbe1e083c6fc2
David Boureau <bdavidxyz@gmail.com>
2021-10-07 10:43:22 +0200

    ok with tests

--- ui/components/ItemDetail/CandidatureSpontaneeSubmit.js
+++ ui/components/ItemDetail/CandidatureSpontaneeSubmit.js
@@ -5,13 +5,15 @@ const CandidatureSpontaneeSubmit = (props) => {
   const loadingState = props.loadingState;
   let res = <></>
   if (loadingState === 'not_sent') {
-    res = <button className="btn btn-dark btn-dark-action c-candidature-submit" type="submit">Je postule</button>
+    res = <button className="btn btn-dark btn-dark-action c-candidature-submit c-candidature-submit--default" type="submit">
+            Je postule
+          </button>
   } else if (loadingState === 'ok_sent') {
-    res = <button className="btn btn-dark btn-dark-action c-candidature-submit" type="submit">✓</button>
+    res = <span className="c-candidature-submit-ok">✓</span>
   } else if (loadingState === 'currently_sending') {
-    res = <button className="btn btn-dark btn-dark-action c-candidature-submit" type="submit">Veuillez patienter...</button>
+    res = <button className="btn btn-dark btn-dark-action c-candidature-submit c-candidature-submit--spinner" type="submit">Veuillez patienter...</button>
   } else if (loadingState === 'sent_but_errors') {
-    res = <>Erreur lors de l'envoi, veuillez réessayer plus tard</>
+    res = <span className="c-candidature-submit-error">Erreur lors de l'envoi, veuillez réessayer plus tard</span>
   }
   return res
 };
--- ui/src/tests/CandidatureSpontaneeSubmit.test.js
+++ ui/src/tests/CandidatureSpontaneeSubmit.test.js
@@ -6,7 +6,24 @@ describe('CandidatureSpontaneeSubmit', () => {
 
   it('Renders a submit button by default', () => {
     const { container } = render(<CandidatureSpontaneeSubmit loadingState={'not_sent'} />)
-    expect(container.firstChild.classList.contains('c-candidature-submit')).toBe(true)
+    expect(container.firstChild.classList.contains('c-candidature-submit--default')).toBe(true)
+  })
+
+  it('Renders an spinner message if submission is pending', () => {
+    const { container } = render(<CandidatureSpontaneeSubmit loadingState={'currently_sending'} />)
+    expect(container.firstChild.classList.contains('c-candidature-submit--spinner')).toBe(true)
   })
   
+  it('Renders an appropriate message if submission is over and OK', () => {
+    const { container } = render(<CandidatureSpontaneeSubmit loadingState={'ok_sent'} />)
+    expect(container.firstChild.classList.contains('c-candidature-submit-ok')).toBe(true)
+  })
+  
+  it('Renders an error message if submission is over and NOT OK', () => {
+    const { container } = render(<CandidatureSpontaneeSubmit loadingState={'sent_but_errors'} />)
+    expect(container.firstChild.classList.contains('c-candidature-submit-error')).toBe(true)
+  })
+  
+  
+
 })
