773d42ef359b90b6b555556e6bd3ebfea3478763
David Boureau <bdavidxyz@gmail.com>
2021-10-08 15:57:51 +0200

    CandidatureSpontanee 2 tests

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.test.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import CandidatureSpontanee from './CandidatureSpontanee';
 
 describe('CandidatureSpontanee', () => {
 
   it('By default displays a button, not a modal', () => {
+    // Given
     render(<CandidatureSpontanee item={{}}/>)
-    expect(screen.queryByRole('button', { name: /jenvoie-une-candidature-spontanee/i})).toBeVisible();
-    expect(screen.queryByRole('modal')).toBeNull();
+    // When
+    const button = screen.queryByRole('button', { name: /jenvoie-une-candidature-spontanee/i })
+    const modal = screen.queryByRole('dialog')
+    // Then
+    expect(button).toBeVisible();
+    expect(modal).toBeNull();
   })
   it('If button is clicked, modal with a form is displayed', () => {
+    // Given
     render(<CandidatureSpontanee item={{}}/>)
-    expect(screen.queryByText("J'envoie une candidature spontanée")).toBeVisible();
-    expect(screen.queryByRole("modal")).toBeNull();
+    const button = screen.queryByRole('button', { name: /jenvoie-une-candidature-spontanee/i })
+    // When
+    fireEvent.click(button)
+    // Then
+    const modal = screen.queryByRole('dialog')
+    expect(modal).not.toBeNull();
   })
 })
