6ab9a934195e0174311b576a6281120f4e703877
David Boureau <bdavidxyz@gmail.com>
2021-10-15 16:06:27 +0200

    cleaned

--- ui/cypress/integration/app.spec.js
+++ ui/cypress/integration/app.spec.js
@@ -8,7 +8,7 @@ describe('Navigation', () => {
   it('should navigate to the about page', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/')
-    // cy.pause()
+    
     // Find a link with an href attribute containing "about" and click it
     cy.get('a[href*="a-propos"]').click()
 
