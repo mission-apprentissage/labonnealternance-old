a969a997c97f536647485737decd31861ff25ffa
David Boureau <bdavidxyz@gmail.com>
2021-10-15 16:13:50 +0200

    remove babel dup

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -37,7 +37,7 @@ module.exports = {
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'], plugins: ['istanbul'] }],
   },
   transformIgnorePatterns: [
     '/node_modules/',
