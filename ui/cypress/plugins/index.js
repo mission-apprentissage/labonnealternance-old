499b68b876f4cefda4b9bfcf7effe6fb04d07772
David Boureau <bdavidxyz@gmail.com>
2021-10-21 14:22:35 +0200

    clean unused npm modules

--- ui/cypress/plugins/index.js
+++ ui/cypress/plugins/index.js
@@ -2,62 +2,8 @@
 
 module.exports = (on, config) => {
   
-  // const webpackPreprocessor = require('@cypress/webpack-preprocessor')
-  // const options = {
-  //   webpackOptions: {
-  //     module: {
-  //       rules: [
-  //         {
-  //           test: /\.jsx?$/,
-  //           exclude: [/node_modules/],
-  //           use: [{
-  //             loader: 'babel-loader',
-  //             options: {
-  //               presets: ['next/babel'],
-  //               plugins: ['istanbul'],
-  //             },
-  //           }],
-  //         },
-  //       ],
-  //     },
-  //   },
-  //   watchOptions: {},
-  // }
-  // on('file:preprocessor', webpackPreprocessor(options))
-
   require('@cypress/code-coverage/task')(on, config)
-  
-  
-  // const browserifyPreprocessor = require('@cypress/browserify-preprocessor')
-  // const options = {
-  //   extensions: ['.js', '.jsx', '.coffee'],
-  //   transform: [
-  //     [
-  //       'coffeeify',
-  //       {}
-  //     ],
-  //     [
-  //       'babelify',
-  //       {
-  //         ast: false,
-  //         babelrc: true,
-  //         plugins: [
-  //           'istanbul',
-  //         ],
-  //         presets: [
-  //           'next/babel'
-  //         ]
-  //       },
-  //     ]
-  //   ],
-  //   debug: true,
-  //   plugin: [],
-  //   cache: {},
-  //   packageCache: {}
-  // }
-  // on('file:preprocessor', browserifyPreprocessor(options))
-
-
 
   return config
+  
 }
