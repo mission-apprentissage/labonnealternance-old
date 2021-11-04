0917226c7513f5dad0f97e8412e6c068c3dbee37
David Boureau <bdavidxyz@gmail.com>
2021-11-04 11:56:24 +0100

    ok done?

--- ui/components/ItemDetail/ItemDetail.js
+++ ui/components/ItemDetail/ItemDetail.js
@@ -23,7 +23,6 @@ import TagCfaDEntreprise from "./TagCfaDEntreprise";
 
 const ItemDetail = ({ selectedItem, handleClose, displayNavbar, handleSelectItem, activeFilter }) => {
   const kind = selectedItem?.ideaType;
-  console.log('kind', kind);
 
   const isCfa = isCfaEntreprise(selectedItem?.company?.siret);
 
