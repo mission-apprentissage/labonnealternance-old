1cf8e85da8c36c08de031a62db4692a677830985
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-08 16:38:35 +0100

    factorisation

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeMessage.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeMessage.js
@@ -1,63 +1,57 @@
 import React from "react";
 
 const CandidatureSpontaneeMessage = ({ formik, kind }) => {
+  const getClass = () => {
+    let className = "mt-3 c-candidature-message";
 
-if (kind !== 'matcha') {
-  
-  return (
-    <>
-        <fieldset data-testid="fieldset-message" className="c-candidature-message mt-3">
-          <h2 className="c-candidature-message-title mb-0">
-            Votre message au responsable du recrutement{" "}
-            <span className="c-candidature-message-title-optional">(Facultatif)</span>
-          </h2>
-          <div className="c-candidature-message-subtitle mb-2">
-            Indiquez pourquoi vous souhaitez réaliser votre alternance dans son entreprise
-          </div>
-          <textarea
-            id="message"
-            data-testid="message"
-            name="message"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.message}
-            />
-        </fieldset>
-      </>
-    );
-    
-  } else {
+    if (kind === "matcha") {
+      className += " c-candidature-field ";
+      className += formik.touched.message ? `is-valid-${!formik.errors.message}` : "is-not-validated";
+    }
 
+    return className;
+  };
+
+  const getFieldTitle = () => {
     return (
       <>
-        <fieldset
-          data-testid="fieldset-message"
-          className={`mt-3 c-candidature-message c-candidature-field ${formik.touched.message ? `is-valid-${!formik.errors.message}` : "is-not-validated"}`}
-        >
-          <h2 className="c-candidature-message-title mb-0">
-            Votre message au responsable du recrutement *
-          </h2>
-          <div className="c-candidature-message-subtitle mb-2">
-            Indiquez pourquoi vous souhaitez réaliser votre alternance dans son entreprise
-          </div>
-          <textarea
-            id="message"
-            data-testid="message"
-            name="message"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.message}
-          />
-        </fieldset>
-        {formik.touched.message && formik.errors.message ? (
-          <div className="c-candidature-erreur visible">{formik.errors.message}</div>
-        ) : (
-          <div className="c-candidature-erreur invisible">{"pas d'erreur"}</div>
-        )}
+        Votre message au responsable du recrutement{" "}
+        {kind !== "matcha" ? <span className="c-candidature-message-title-optional">(Facultatif)</span> : "*"}
       </>
     );
+  };
 
-  }
+  const getFieldError = () => {
+    let errorMsg = "";
+    if (kind === "matcha") {
+      if (formik.touched.message && formik.errors.message) {
+        errorMsg = <div className="c-candidature-erreur visible">{formik.errors.message}</div>;
+      } else {
+        errorMsg = <div className="c-candidature-erreur invisible">{"pas d'erreur"}</div>;
+      }
+    }
+    return errorMsg;
+  };
+
+  return (
+    <>
+      <fieldset data-testid="fieldset-message" className={getClass()}>
+        <h2 className="c-candidature-message-title mb-0">{getFieldTitle()}</h2>
+        <div className="c-candidature-message-subtitle mb-2">
+          Indiquez pourquoi vous souhaitez réaliser votre alternance dans son entreprise
+        </div>
+        <textarea
+          id="message"
+          data-testid="message"
+          name="message"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values.message}
+        />
+      </fieldset>
+      {getFieldError()}
+    </>
+  );
 };
 
 export default CandidatureSpontaneeMessage;
