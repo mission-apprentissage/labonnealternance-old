e1e4c9c10fa5de6efdbba4b22d09fc4e7f9721ff
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-08 15:47:23 +0100

    Elargissement conditions de taille nom  / prénom

--- server/src/service/validateSendApplication.js
+++ server/src/service/validateSendApplication.js
@@ -3,8 +3,8 @@ const Yup = require("yup");
 const validateSendApplication = async (validable) => {
   let schema = Yup.object().shape({
     fileName: Yup.string().nullable().required("⚠ La pièce jointe est requise"),
-    firstName: Yup.string().max(15, "⚠ Doit avoir 15 caractères ou moins").required("⚠ Le prénom est requis."),
-    lastName: Yup.string().max(20, "⚠ Doit avoir 20 caractères ou moins").required("⚠ Le nom est requis."),
+    firstName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le prénom est requis."),
+    lastName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le nom est requis."),
     email: Yup.string().email("⚠ Adresse e-mail invalide.").required("⚠ L'adresse e-mail est requise."),
     phone: Yup.string()
       .matches(/^[0-9]{10}$/, "⚠ Le numéro de téléphone doit avoir exactement 10 chiffres")
--- ui/components/ItemDetail/CandidatureSpontanee/services/getSchema.js
+++ ui/components/ItemDetail/CandidatureSpontanee/services/getSchema.js
@@ -9,30 +9,29 @@ export function getInitialSchemaValues() {
     fileName: "",
     fileContent: null,
     message: "",
-  }
+  };
 }
 export function getValidationSchema(actualKind) {
-
-  if (actualKind === 'matcha') {
+  if (actualKind === "matcha") {
     return Yup.object({
       message: Yup.string().nullable().required("⚠ La lettre de motivation est obligatoire"),
       fileName: Yup.string().nullable().required("⚠ La pièce jointe est obligatoire"),
-      firstName: Yup.string().max(15, "⚠ Doit avoir 15 caractères ou moins").required("⚠ Le prénom est obligatoire."),
-      lastName: Yup.string().max(20, "⚠ Doit avoir 20 caractères ou moins").required("⚠ Le nom est obligatoire."),
+      firstName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le prénom est obligatoire."),
+      lastName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le nom est obligatoire."),
       email: Yup.string().email("⚠ Adresse e-mail invalide.").required("⚠ L'adresse e-mail est obligatoire."),
       phone: Yup.string()
         .matches(/^[0-9]{10}$/, "⚠ Le numéro de téléphone doit avoir exactement 10 chiffres")
         .required("⚠ Le téléphone est obligatoire"),
-    })
+    });
   } else {
     return Yup.object({
       fileName: Yup.string().nullable().required("⚠ La pièce jointe est obligatoire"),
-      firstName: Yup.string().max(15, "⚠ Doit avoir 15 caractères ou moins").required("⚠ Le prénom est obligatoire."),
-      lastName: Yup.string().max(20, "⚠ Doit avoir 20 caractères ou moins").required("⚠ Le nom est obligatoire."),
+      firstName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le prénom est obligatoire."),
+      lastName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le nom est obligatoire."),
       email: Yup.string().email("⚠ Adresse e-mail invalide.").required("⚠ L'adresse e-mail est obligatoire."),
       phone: Yup.string()
         .matches(/^[0-9]{10}$/, "⚠ Le numéro de téléphone doit avoir exactement 10 chiffres")
         .required("⚠ Le téléphone est obligatoire"),
-    })
+    });
   }
 }
