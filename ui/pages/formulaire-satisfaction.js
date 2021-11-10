a2ce982cd40f99222278123a71c06e21eb58ce0c
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-10 17:26:37 +0100

    Embryon de page

--- ui/pages/formulaire-satisfaction.js
+++ ui/pages/formulaire-satisfaction.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NextSeo } from "next-seo";
 
-const FormulaireSatisfaction = () => (
-  <div>
-    <NextSeo
-      title="Formulaire de satisfaction | La Bonne Alternance | Trouvez votre alternance"
-      description="Formulaire de satisfaction."
-    />
+const FormulaireSatisfaction = () => {
+  //getValueFromPath
 
-    Logo LBA, lien vers home lba
+  //
 
-    <div className="c-page-container container my-0 mb-sm-5 p-5">
-      <div className="row">
-        <div className="col-12 col-md-5">
+  useEffect(() => {
+    // enregistrement en state des params provenant du path
+    // requête post avis pour enregistrement en base si et seulement si params corrects
+  }, []);
+
+  return (
+    <div>
+      <NextSeo
+        title="Formulaire de satisfaction | La Bonne Alternance | Trouvez votre alternance"
+        description="Formulaire de satisfaction."
+      />
+      Logo LBA, lien vers home lba
+      <div className="c-page-container container my-0 mb-sm-5 p-5">
+        <div className="row">
+          <div className="col-12 col-md-5">
             Texte remerciement
-            <br /><br />
+            <br />
+            <br />
             Textarea commentaire
-            <br /><br />
+            <br />
+            <br />
             Bouton enregistrement
-
-
+          </div>
         </div>
       </div>
     </div>
-
-  </div>
-);
+  );
+};
 
 export default FormulaireSatisfaction;
