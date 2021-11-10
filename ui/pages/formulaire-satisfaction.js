287f6d9303ae7e25d9b6a3f6d8b3702b71a10703
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-10 12:25:34 +0100

    Start

---
+++ ui/pages/formulaire-satisfaction.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NextSeo } from "next-seo";
+
+const FormulaireSatisfaction = () => (
+  <div>
+    <NextSeo
+      title="Formulaire de satisfaction | La Bonne Alternance | Trouvez votre alternance"
+      description="Formulaire de satisfaction."
+    />
+
+    Logo LBA, lien vers home lba
+
+    <div className="c-page-container container my-0 mb-sm-5 p-5">
+      <div className="row">
+        <div className="col-12 col-md-5">
+            Texte remerciement
+            <br /><br />
+            Textarea commentaire
+            <br /><br />
+            Bouton enregistrement
+
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+);
+
+export default FormulaireSatisfaction;
