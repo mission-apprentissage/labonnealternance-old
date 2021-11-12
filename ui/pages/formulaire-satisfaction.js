2168310578ed89b8a46baf3b39d1dfa810b8bef7
David Boureau <bdavidxyz@gmail.com>
2021-11-12 11:23:10 +0100

    scss not loaded

--- ui/pages/formulaire-satisfaction.js
+++ ui/pages/formulaire-satisfaction.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
 import { NextSeo } from "next-seo";
 import { Collapse, NavbarToggler, Nav, NavItem, NavLink } from "reactstrap";
+import { useFormik } from "formik";
+
 
 const FormulaireSatisfaction = () => {
   //getValueFromPath
-
-  //
 
   useEffect(() => {
     // enregistrement en state des params provenant du path
@@ -15,23 +16,45 @@ const FormulaireSatisfaction = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
+
+
+  const formik = useFormik({
+    initialValues: {message: ""},
+    validationSchema: Yup.object({
+      message: Yup.string().nullable().required("Veuillez remplir le commentaire"),
+    }),
+    onSubmit: async (applicantValues) => {
+      // await submitCandidature(applicantValues, setSendingState, props.item);
+    },
+  });
+
+  const getFieldError = () => {
+    let errorMsg = "";
+      if (formik.touched.message && formik.errors.message) {
+        errorMsg = <div className="c-candidature-erreur visible">{formik.errors.message}</div>;
+      } else {
+        errorMsg = <div className="c-candidature-erreur invisible">{"pas d'erreur"}</div>;
+      }
+    return errorMsg;
+  };
+
   return (
     <div>
       <NextSeo
         title="Formulaire de satisfaction | La Bonne Alternance | Trouvez votre alternance"
         description="Formulaire de satisfaction."
       />
-      <div class="c-navigation is-filled">
-        <nav class="navbar-light navbar navbar-expand-lg">
-          <div class="container">
-            <a href="/" class="navbar-brand">
+      <div className="c-navigation is-filled">
+        <nav className="navbar-light navbar navbar-expand-lg">
+          <div className="container">
+            <a href="/" className="navbar-brand">
               <img src="/images/logo_lba.svg" alt="Logo LBA" className="c-navbar-brand-img" width="110" height="76" />
             </a>
             <NavbarToggler onClick={toggle} />
             <Collapse isOpen={isOpen} navbar>
               <Nav className="c-navbar-links ml-auto" navbar>
                 <NavItem className="ml-lg-5">
-                  <NavLink href="/" className="ml-1">
+                  <NavLink href="/" className="ml-1 c-formulaire-satisfaction-navlink">
                     Page d'accueil La Bonne Alternance
                   </NavLink>
                 </NavItem>
@@ -40,16 +63,22 @@ const FormulaireSatisfaction = () => {
           </div>
         </nav>
       </div>
-      <div className="c-page-container container my-0 mb-sm-5 p-5">
-        <div className="row">
-          <div className="col-12 col-md-5">
-            Texte remerciement
-            <br />
-            <br />
-            Textarea commentaire
-            <br />
-            <br />
-            Bouton enregistrement
+      <div className="container flex-center">
+        <div className="row flex-center py-5">
+          <div className="col col-lg-7 mx-auto">
+            <p className="pt-5">Merci beaucoup pour ce retour positif sur le service <strong>La Bonne Alternance</strong> et d'avoir pris le temps de le faire.</p>
+            <p className="pt-3">Avez-vous tout de même des suggestions d'améliorations ?</p>
+            <fieldset data-testid="fieldset-message pt-3 c-candidature-message">
+              <textarea
+                id="message"
+                data-testid="message"
+                name="message"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.message}
+              />
+            </fieldset>
+            {getFieldError()}
           </div>
         </div>
       </div>
