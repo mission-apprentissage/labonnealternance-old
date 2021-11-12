c3dbe5dec436bb28cccc688024e7b398194d648c
David Boureau <bdavidxyz@gmail.com>
2021-11-12 14:27:01 +0100

    done ok

--- ui/pages/formulaire-satisfaction.js
+++ ui/pages/formulaire-satisfaction.js
@@ -100,8 +100,22 @@ const FormulaireSatisfaction = () => {
             </div>
           </div>
           :
-          <div className="container flex-center">
-            Done !
+          <div className="container flex-center pt-5">
+              <div className="row flex-center py-5">
+                <div className="col col-lg-7 mx-auto">
+                  <div className="d-flex-center pb-5">
+                    <img src="/images/tada.svg" alt="Cotillons" className="" width="56" height="56" />
+                  </div>
+                    <h1 className="h4 text-center">
+                      <strong>
+                        Merci beaucoup d’avoir pris le temps de nous aider à améliorer le service La Bonne Alternance
+                      </strong>
+                    </h1>
+                  <p className="pt-3 halflead text-center">
+                    Nous prenons connaissance de tous les retours pour faire évoluer le service.
+                  </p>
+                </div>
+              </div>
           </div>
       }
 
