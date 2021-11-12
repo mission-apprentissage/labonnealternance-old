04fcd56d01d0ffb15c0982e7df1781aca9db6e85
David Boureau <bdavidxyz@gmail.com>
2021-11-12 14:42:00 +0100

    scalable component

--- ui/pages/formulaire-satisfaction.js
+++ ui/pages/formulaire-satisfaction.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import SatisfactionForm from "components/SatisfactionForm/SatisfactionForm";
 
 const FormulaireSatisfaction = () => {
   
-  return <SatisfactionForm/>
+  return (
+    <>
+      <SatisfactionForm/>
+    </>
+  );
  };
 
 export default FormulaireSatisfaction;
