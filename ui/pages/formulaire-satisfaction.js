ab0394a123b7379465cc86e3d60a2e1044cf3e46
David Boureau <bdavidxyz@gmail.com>
2021-11-12 10:50:59 +0100

    better nav

--- ui/pages/formulaire-satisfaction.js
+++ ui/pages/formulaire-satisfaction.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NextSeo } from "next-seo";
+import { Collapse, NavbarToggler, Nav, NavItem, NavLink } from "reactstrap";
 
 const FormulaireSatisfaction = () => {
   //getValueFromPath
@@ -10,6 +11,9 @@ const FormulaireSatisfaction = () => {
     // enregistrement en state des params provenant du path
     // requête post avis pour enregistrement en base si et seulement si params corrects
   }, []);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
@@ -23,27 +27,19 @@ const FormulaireSatisfaction = () => {
             <a href="/" class="navbar-brand">
               <img src="/images/logo_lba.svg" alt="Logo LBA" className="c-navbar-brand-img" width="110" height="76" />
             </a>
-            <button aria-label="Toggle navigation" type="button" class="navbar-toggler">
-              <span class="navbar-toggler-icon">
-
-              </span>
-            </button>
-            <div class="collapse navbar-collapse">
-              <ul class="c-navbar-links ml-auto navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="/acces-recruteur">
-                    <span class="ml-1">Recruteur</span>
-                  </a>
-                </li>
-                <li class="ml-lg-5 nav-item">
-                  <a href="/" class="ml-1 nav-link">Page d'accueil La Bonne Alternance</a>
-                </li>
-              </ul>
-            </div>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="c-navbar-links ml-auto" navbar>
+                <NavItem className="ml-lg-5">
+                  <NavLink href="/" className="ml-1">
+                    Page d'accueil La Bonne Alternance
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </nav>
       </div>
-      Logo LBA, lien vers home lba
       <div className="c-page-container container my-0 mb-sm-5 p-5">
         <div className="row">
           <div className="col-12 col-md-5">
