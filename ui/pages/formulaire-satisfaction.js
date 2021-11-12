413334d6372bfb9362166e08127c37d964d80503
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-12 15:14:20 +0100

    Post feedback

--- ui/pages/formulaire-satisfaction.js
+++ ui/pages/formulaire-satisfaction.js
@@ -1,14 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { getValueFromPath } from "utils/tools";
+import { amongst } from "utils/arrayutils";
 import { NextSeo } from "next-seo";
+import postFeedback from "services/postFeedback";
 
 const FormulaireSatisfaction = () => {
   //getValueFromPath
 
-  //
+  let iv = null;
+  let id = null;
+  let avis = null;
+
+  const initParametersFromPath = () => {
+    //console.log("dddd",getValueFromPath("iv"));
+
+    iv = getValueFromPath("iv");
+    id = getValueFromPath("id");
+    avis = getValueFromPath("avis");
+  };
+
+  const saveFeedback = () => {
+    if (iv && id && amongst(avis, ["utile", "pasUtile", "neutre"])) {
+      console.log("good : ",iv,id, avis);
+      postFeedback({ iv, id, avis });
+    } else {
+      //else invalid params
+      console.log("ca va pas !");
+    }
+  };
 
   useEffect(() => {
     // enregistrement en state des params provenant du path
+    initParametersFromPath();
     // requête post avis pour enregistrement en base si et seulement si params corrects
+    saveFeedback();
   }, []);
 
   return (
