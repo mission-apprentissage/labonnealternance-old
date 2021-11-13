6cfeff088751114209db4766c194a1af80760189
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-13 17:11:16 +0100

    repositionnement

--- ui/components/SatisfactionForm/SatisfactionForm.js
+++ ui/components/SatisfactionForm/SatisfactionForm.js
@@ -5,15 +5,39 @@ import submitCommentaire from "./services/submitCommentaire.js";
 import SatisfactionFormSuccess from "./SatisfactionFormSuccess.js";
 import SatisfactionFormNavigation from "./SatisfactionFormNavigation.js";
 
+import { getValueFromPath } from "utils/tools";
+import { amongst } from "utils/arrayutils";
+import postFeedback from "services/postFeedback";
 
 const SatisfactionForm = () => {
-  //getValueFromPath
+  let iv = null;
+  let id = null;
+  let avis = null;
+
+  const initParametersFromPath = () => {
+    //console.log("dddd",getValueFromPath("iv"));
+
+    iv = getValueFromPath("iv");
+    id = getValueFromPath("id");
+    avis = getValueFromPath("avis");
+  };
+
+  const saveFeedback = () => {
+    if (iv && id && amongst(avis, ["utile", "pasUtile", "neutre"])) {
+      console.log("good : ", iv, id, avis);
+      postFeedback({ iv, id, avis });
+    } else {
+      //else invalid params
+      console.log("ca va pas !");
+    }
+  };
 
   useEffect(() => {
     // enregistrement en state des params provenant du path
+    initParametersFromPath();
     // requête post avis pour enregistrement en base si et seulement si params corrects
+    saveFeedback();
   }, []);
-
 
   const [sendingState, setSendingState] = useState("not_sent");
 
@@ -23,7 +47,14 @@ const SatisfactionForm = () => {
       message: Yup.string().nullable().required("Veuillez remplir le commentaire"),
     }),
     onSubmit: async (formikValues) => {
-      await submitCommentaire(formikValues.message, setSendingState);
+      await submitCommentaire(
+        {
+          message: formikValues.message,
+          id,
+          iv,
+        },
+        setSendingState
+      );
     },
   });
 
@@ -39,46 +70,50 @@ const SatisfactionForm = () => {
 
   return (
     <div className="c-formulaire-satisfaction">
-      <SatisfactionFormNavigation/>
-      {
-        sendingState == 'not_sent' ?
-          <div className="container flex-center">
-            <div className="row flex-center py-5">
-              <div className="col col-lg-7 mx-auto">
-                <p className="pt-5">Merci beaucoup pour ce retour positif sur le service <strong>La Bonne Alternance</strong> et d'avoir pris le temps de le faire.</p>
-                <p className="pt-2">Avez-vous tout de même des suggestions d'améliorations ?</p>
-                <form onSubmit={formik.handleSubmit} className="">
-                  <fieldset data-testid="fieldset-message" className={`pt-2 c-candidature-field ${formik.touched.message ? `is-valid-${!formik.errors.message}` : "is-not-validated"
-                    }`}>
-                    <textarea
-                      id="message"
-                      data-testid="message"
-                      name="message"
-                      placeholder="J’ai une suggestion à propos de ..."
-                      onBlur={formik.handleBlur}
-                      onChange={formik.handleChange}
-                      value={formik.values.message}
-                    />
-                  </fieldset>
-                  {getFieldError()}
-                  <div className="d-flex flex-row-reverse">
-                    <button
-                      aria-label="jenvoie-mon-commentaire"
-                      className={`btn btn-dark btn-dark-action c-candidature-submit c-candidature-submit--default`}
-                      type="submit"
-                    >
-                      {"J'envoie mon commentaire !"}
-                    </button>
-                  </div>
-                </form>
-              </div>
+      <SatisfactionFormNavigation />
+      {sendingState == "not_sent" ? (
+        <div className="container flex-center">
+          <div className="row flex-center py-5">
+            <div className="col col-lg-7 mx-auto">
+              <p className="pt-5">
+                Merci beaucoup pour ce retour positif sur le service <strong>La Bonne Alternance</strong> et d'avoir
+                pris le temps de le faire.
+              </p>
+              <p className="pt-2">Avez-vous tout de même des suggestions d'améliorations ?</p>
+              <form onSubmit={formik.handleSubmit} className="">
+                <fieldset
+                  data-testid="fieldset-message"
+                  className={`pt-2 c-candidature-field ${
+                    formik.touched.message ? `is-valid-${!formik.errors.message}` : "is-not-validated"
+                  }`}
+                >
+                  <textarea
+                    id="message"
+                    data-testid="message"
+                    name="message"
+                    placeholder="J’ai une suggestion à propos de ..."
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.message}
+                  />
+                </fieldset>
+                {getFieldError()}
+                <div className="d-flex flex-row-reverse">
+                  <button
+                    aria-label="jenvoie-mon-commentaire"
+                    className={`btn btn-dark btn-dark-action c-candidature-submit c-candidature-submit--default`}
+                    type="submit"
+                  >
+                    {"J'envoie mon commentaire !"}
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-          :
-          <SatisfactionFormSuccess/>
-      }
-
-
+        </div>
+      ) : (
+        <SatisfactionFormSuccess />
+      )}
     </div>
   );
 };
