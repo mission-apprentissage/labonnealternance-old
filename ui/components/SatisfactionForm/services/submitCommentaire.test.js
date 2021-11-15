642e21258fc483f4cbdc7b9185ade647eb940784
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-15 16:26:05 +0100

    test

--- ui/components/SatisfactionForm/services/submitCommentaire.test.js
+++ ui/components/SatisfactionForm/services/submitCommentaire.test.js
@@ -10,13 +10,13 @@ describe("submitCommentaire", () => {
     };
     // when
     await submitCommentaire(
-      { id: "aaaa", iv: "aaaa", comment: "" },
+      { id: "aaaa", iv: "aaaa", comment: "Commentaire" },
       mockedSetSendingState,
       mockedPostCommentaire,
       repeatFunc
     );
     // then
-    expect(mockedPostCommentaire).toHaveBeenCalledWith({ applicants: "values" }, { items: "some" });
+    expect(mockedPostCommentaire).toHaveBeenCalledWith({ id: "aaaa", iv: "aaaa", comment: "Commentaire" });
     expect(mockedSetSendingState).toHaveBeenCalledWith("currently_sending");
     expect(mockedSetSendingState).toHaveBeenCalledWith("ok_sent");
   });
@@ -29,7 +29,7 @@ describe("submitCommentaire", () => {
       throw "Custom error";
     };
     // when
-    await submitCommentaire(null, mockedSetSendingState, {}, badFunc, emptyFunc);
+    await submitCommentaire({ id: "aaaa", iv: "aaaa" }, mockedSetSendingState, {}, badFunc, emptyFunc);
     // then
     expect(mockedSetSendingState).toHaveBeenCalledWith("currently_sending");
     expect(mockedSetSendingState).toHaveBeenCalledWith("not_sent_because_of_errors");
