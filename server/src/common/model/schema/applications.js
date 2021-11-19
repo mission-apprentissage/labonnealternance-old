f74f9697d6ac6a7b1743887dca9bafb84746062d
Alan Le Ruyet <alan.leruyet@free.fr>
2021-11-19 10:13:54 +0100

    Refacto

--- server/src/common/model/schema/applications.js
+++ server/src/common/model/schema/applications.js
@@ -48,23 +48,23 @@ const applicationSchema = {
     required: false,
     description: "Date d'opinion donnée",
   },
-  company_opinion: {
-    type: String,
-    default: null,
-    required: false,
-    description: "L'avis donné par la société",
-  },
   company_intention: {
     type: String,
     default: null,
     required: false,
     description: "L'intention de la société vis à vis du candidat",
   },
-  company_intention_date: {
+  company_feedback: {
+    type: String,
+    default: null,
+    required: false,
+    description: "L'avis donné par la société",
+  },
+  company_feedback_date: {
     type: Date,
     default: null,
     required: false,
-    description: "Date d'intention donnée",
+    description: "Date d'intention/avis donnée",
   },
   company_siret: {
     type: String,
