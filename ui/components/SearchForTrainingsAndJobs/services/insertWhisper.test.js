14faca3a428db2ae185f2cd6b529d5d1d5e33f37
David Boureau <bdavidxyz@gmail.com>
2021-11-19 15:21:42 +0100

    one test miss

--- ui/components/SearchForTrainingsAndJobs/services/insertWhisper.test.js
+++ ui/components/SearchForTrainingsAndJobs/services/insertWhisper.test.js
@@ -1,4 +1,4 @@
-import whispers from "./insertWhisper";
+import whispers from "./whispers";
 import { queryByTestId } from '@testing-library/dom'
 import nock from "nock";
 
@@ -8,34 +8,34 @@ describe('insertWhisper', () => {
     nock.disableNetConnect();
   });
 
-  it('insertWhisper() : do not insert anything if whisper already here', () => {
+  it('insertWhisper() : do not insert anything if whisper already here', async () => {
     document.body.innerHTML =
       '<div>' +
       '  <span class="whisper">Im a whisper</span>' +
       '</div>';
-    let res = whispers.insertWhisper(document)
+    let res = await whispers.insertWhisper(document)
     expect(res).toEqual('whisper already exists : no change')
   });
 
-  it('insertWhisper() : do not insert anything if there is no resultCard', () => {
+  it('insertWhisper() : do not insert anything if there is no resultCard', async () => {
     document.body.innerHTML =
       '<div>' +
       '  Empty div, empty document' +
       '</div>';
-    let res = whispers.insertWhisper(document)
+    let res = await whispers.insertWhisper(document)
     expect(res).toEqual('no resultCard found : no change')
   });
 
-  it('insertWhisper() : do not insert anything if there is not enough resultCard', () => {
+  it('insertWhisper() : do not insert anything if there is not enough resultCard', async () => {
     document.body.innerHTML =
       '<div>' +
       '  <span class="resultCard">Im a resultCard</span>' +
       '</div>';
-    let res = whispers.insertWhisper(document)
+    let res = await whispers.insertWhisper(document)
     expect(res).toEqual('not enough resultCard to show a whisper')
   });
   
-  it('insertWhisper() : insert a whisper if more than 9 resultCard', () => {
+  it('insertWhisper() : insert a whisper if more than 9 resultCard', async () => {
     document.body.innerHTML =
     '<div id="app">' +
     '  <span class="resultCard">1</span>' +
@@ -49,15 +49,12 @@ describe('insertWhisper', () => {
     '  <span class="resultCard">9</span>' +
     '  <span class="resultCard">10</span>' +
     '</div>';
-    let res = whispers.insertWhisper(document)
+    let res = await whispers.insertWhisper(document)
     const container = document.querySelector('#app')
     const whisper = queryByTestId(container, 'whisper')
     expect(whisper).not.toBeNull();
     expect(res).toEqual('whisper randomly inserted')
   });
 
-  function emptyImg() {
-    return '<img src="" alt="empty image for test"></img>'
-  }
 
 });
