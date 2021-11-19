ef9613e4916b92a679510d9481ffe84122bccd0a
David Boureau <bdavidxyz@gmail.com>
2021-11-19 10:18:16 +0100

    tested with jsdom

--- ui/components/SearchForTrainingsAndJobs/services/insertWhisper.test.js
+++ ui/components/SearchForTrainingsAndJobs/services/insertWhisper.test.js
@@ -3,7 +3,11 @@ import insertWhisper from "./insertWhisper";
 describe('insertWhisper', () => {
 
   it('insertWhisper() : do not insert anything if whisper already here', () => {
-    let res = insertWhisper()
+    document.body.innerHTML =
+      '<div>' +
+      '  <span class="whisper">Im a whisper</span>' +
+      '</div>';
+    let res = insertWhisper(document)
     expect(res).toEqual('no change made to the document')
   });
 
