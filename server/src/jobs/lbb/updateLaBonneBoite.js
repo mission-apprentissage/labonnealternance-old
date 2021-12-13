47345f9eb71e3499388aeac0004b2ee1726b3fef
Alan Le Ruyet <alan.leruyet@free.fr>
2021-12-13 16:14:49 +0100

    Cleaning et sémaphore

--- server/src/jobs/lbb/updateLaBonneBoite.js
+++ server/src/jobs/lbb/updateLaBonneBoite.js
@@ -27,17 +27,11 @@ let getGeoCount = 0;
 let getGeoTime = 0;
 let findBBCount = 0;
 let findBBTime = 0;
+let running = false;
 
 const findRomesForNaf = async (bonneBoite) => {
   let sTime = new Date().getTime();
-  /*let dbRomes = await RomeNaf.find({ code_naf: bonneBoite.code_naf }, { code_rome: 1 });
-
-  let romes = dbRomes.map((rome) => rome.code_rome);*/
-
-  let romes = await filterRomesFromNafHirings(bonneBoite /*, romes*/);
-
-  //console.log("romes D : ",romes);
-
+  let romes = await filterRomesFromNafHirings(bonneBoite);
   let eTime = new Date().getTime();
 
   findRomesForNafTime += eTime - sTime;
@@ -80,8 +74,6 @@ const synchIndex = async () => {
 
 const getScoreForCompany = async (siret) => {
   let sTime = new Date().getTime();
-  //let companyScore = await CompanyScore.findOne({ siret, active: true });
-
   let companyScore = predictionMap[siret];
 
   let eTime = new Date().getTime();
@@ -90,58 +82,18 @@ const getScoreForCompany = async (siret) => {
   getScoreForCompanyTime += eTime - sTime;
 
   return companyScore;
-
-  /*
-  if (companyScore?.score) {
-    return companyScore.score;
-    if (company.score >= config.private.lbb.score100Level) {
-      return 100;
-    }
-
-    if (company.score >= config.private.lbb.score80Level) {
-      return 80;
-    }
-
-    if (company.score >= config.private.lbb.score60Level) {
-      return 60;
-    }
-
-    return 50;
-  } else {
-    return null;
-  }*/
 };
 
 const filterRomesFromNafHirings = (bonneBoite /*, romes*/) => {
-  //let sTime = new Date().getTime();
   const nafRomeHirings = nafScoreMap[bonneBoite.code_naf];
-
-  //console.log("romes A : ",nafRomeHirings,bonneBoite.siret);
 
   let filteredRomes = [];
   if (nafRomeHirings) {
     filteredRomes = nafRomeHirings.romes.filter((rome) => {
-      /*console.log(
-      rome,
-      bonneBoite.enseigne,
-      (bonneBoite.score * nafRomeHirings[rome]) / nafRomeHirings.hirings,
-      nafRomeHirings[rome],
-      nafRomeHirings[rome] / nafRomeHirings.hirings
-    );*/
-
       // 0.2 arbitraire
       return (bonneBoite.score * nafRomeHirings[rome]) / nafRomeHirings.hirings > 0.2;
     });
   }
-
-  //console.log("romes B : ",filteredRomes);
-
-  //let eTime = new Date().getTime();
-
-  //filterRomesFromNafHiringsCount++;
-  //filterRomesFromNafHiringsTime += eTime - sTime;
-
-  //console.log(romes, filteredRomes, bonneBoite.score, bonneBoite.code_naf, nafRomeHirings.hirings);
 
   return filteredRomes;
 };
@@ -156,7 +108,6 @@ const getGeoLocationForCompany = async (bonneBoite) => {
 
     if (!result) {
       result = await geoData.getFirstMatchUpdates(bonneBoite);
-      //console.log("result from ban : ", result);
 
       if (result) {
         let geoLocation = new GeoLocation({
@@ -167,8 +118,6 @@ const getGeoLocationForCompany = async (bonneBoite) => {
       } else {
         return null;
       }
-    } else {
-      //console.log("result from db : ", result);
     }
 
     let eTime = new Date().getTime();
@@ -213,7 +162,6 @@ const parseLine = async (line) => {
 
   if (!score) {
     //TODO: checker si réhaussage artificiel vie support PE
-    //console.log("pas de score");
     return null;
   }
 
@@ -224,11 +172,8 @@ const parseLine = async (line) => {
   findBBTime += eTime - sTime;
 
   if (!bonneBoite) {
-    //console.log("bonne boite existe pas ", company.siret);
     company.intitule_naf = nafMap[company.code_naf];
     bonneBoite = new BonnesBoites(company);
-  } else {
-    //console.log("bonne boîte existe déjà : ", bonneBoite);
   }
 
   bonneBoite.score = score;
@@ -237,11 +182,8 @@ const parseLine = async (line) => {
 
   let romes = await findRomesForNaf(bonneBoite);
 
-  //console.log("romes E : ",romes);
-
   // filtrage des éléments inexploitables
   if (romes.length === 0) {
-    //console.log("pas de romes");
     return null;
   } else {
     bonneBoite.romes = romes;
@@ -251,7 +193,6 @@ const parseLine = async (line) => {
 
   if (!bonneBoite.geo_coordonnees) {
     if (!geo) {
-      //console.log("pas de geoloc");
       return null;
     } else {
       bonneBoite.ville = geo.city;
@@ -260,64 +201,68 @@ const parseLine = async (line) => {
     }
   }
 
-  //console.log("cmp ",company);
   return bonneBoite;
 };
 
 module.exports = async () => {
-  let step = 0;
-
-  try {
-    logMessage("info", " -- Start updating lbb db -- ");
-    logMessage("info", `score 100 :  ${config.private.lbb.score100Level}`);
-    logMessage("info", `score 080 :  ${config.private.lbb.score80Level}`);
-    logMessage("info", `score 060 :  ${config.private.lbb.score60Level}`);
-    logMessage("info", `score 050 :  ${config.private.lbb.score50Level}`);
-
-    var exec = require("child_process").exec;
-
-    exec(`wc -l ${filePath}`, function (error, results) {
-      logMessage("info", results);
-    });
-
-    const db = mongooseInstance.connection;
-
-    nafScoreMap = await initNafScoreMap();
-    nafMap = await initNafMap();
-    predictionMap = await initPredictionMap();
-
-    // voir comment supprimer tout ça
-    await emptyMongo();
-
+  if (!running) {
+    running = true;
     try {
-      await oleoduc(
-        fs.createReadStream(filePath),
-        readLineByLine(),
-        transformData((line) => parseLine(line), { parallel: 8 }),
-        writeData(async (bonneBoite) => {
-          db.collections["bonnesboites"].save(bonneBoite);
-        })
-      );
-    } catch (err2) {
-      logMessage("error", err2);
-      throw new Error("Error while parsing establishment file");
+      logMessage("info", " -- Start updating lbb db -- ");
+      logMessage("info", `score 100 :  ${config.private.lbb.score100Level}`);
+      logMessage("info", `score 080 :  ${config.private.lbb.score80Level}`);
+      logMessage("info", `score 060 :  ${config.private.lbb.score60Level}`);
+      logMessage("info", `score 050 :  ${config.private.lbb.score50Level}`);
+
+      var exec = require("child_process").exec;
+
+      exec(`wc -l ${filePath}`, function (error, results) {
+        logMessage("info", results);
+      });
+
+      const db = mongooseInstance.connection;
+
+      nafScoreMap = await initNafScoreMap();
+      nafMap = await initNafMap();
+      predictionMap = await initPredictionMap();
+
+      // TODO: supprimer ce reset
+      await emptyMongo();
+
+      try {
+        await oleoduc(
+          fs.createReadStream(filePath),
+          readLineByLine(),
+          transformData((line) => parseLine(line), { parallel: 8 }),
+          writeData(async (bonneBoite) => {
+            db.collections["bonnesboites"].save(bonneBoite);
+          })
+        );
+      } catch (err2) {
+        logMessage("error", err2);
+        throw new Error("Error while parsing establishment file");
+      }
+
+      // clearing memory
+      resetHashmaps();
+
+      await clearIndex();
+      await createIndex();
+      await synchIndex();
+      logMessage("info", `End updating lbb db`);
+
+      running = false;
+
+      return {
+        result: "Table mise à jour",
+      };
+    } catch (err) {
+      running = false;
+      logMessage("error", err);
+      let error_msg = _.get(err, "meta.body") ?? err.message;
+      return { error: error_msg };
     }
-
-    // clearing memory
-    resetHashmaps();
-
-    await clearIndex();
-    await createIndex();
-    await synchIndex();
-    logMessage("info", `End updating lbb db`);
-
-    return {
-      result: "Table mise à jour",
-    };
-  } catch (err) {
-    console.log("error step ", step, err);
-    logMessage("error", err);
-    let error_msg = _.get(err, "meta.body") ?? err.message;
-    return { error: error_msg };
+  } else {
+    return { error: "process_already_running" };
   }
 };
