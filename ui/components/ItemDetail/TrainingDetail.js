9899af665e9fd0a681e4d686fdec3d2a93f0d625
Alan Le Ruyet <alan.leruyet@free.fr>
2021-12-14 14:44:36 +0100

    Transmission cle_ministere_educatif à prdv

--- ui/components/ItemDetail/TrainingDetail.js
+++ ui/components/ItemDetail/TrainingDetail.js
@@ -85,7 +85,14 @@ const TrainingDetail = ({ training, seeInfo, setSeeInfo, isCfa }) => {
   };
 
   const buildPrdvButton = () => {
-    return <div className="widget-prdv gtmPrdv" data-referrer="lba" data-id-rco-formation={training.idRcoFormation} />;
+    return (
+      <div
+        className="widget-prdv gtmPrdv"
+        data-referrer="lba"
+        data-id-cle-ministere-educatif={training.cleMinistereEducatif}
+        data-id-rco-formation={training.idRcoFormation}
+      />
+    );
   };
 
   const kind = training?.ideaType;
