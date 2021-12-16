47679669023300c848869d9714fbee4e4e8454e5
Alan Le Ruyet <alan.leruyet@free.fr>
2021-12-16 01:01:55 +0100

    Gestion context server et front

--- ui/components/head.js
+++ ui/components/head.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Head from "next/head";
 import Fonts from "./fonts";
+import env from "utils/env";
 
 const HeadLaBonneAlternance = (props) => {
   const getEnvFromProps = () => {
+    let host = props.publicUrl || env;
+
     let result = "production";
-    if (props.publicUrl?.indexOf("-recette") >= 0) result = "recette";
-    if (props.publicUrl?.indexOf("localhost") >= 0) result = "local";
+    if (host?.indexOf("recette") >= 0) result = "recette";
+    if (host?.indexOf("local") >= 0) result = "local";
 
     return result;
   };
