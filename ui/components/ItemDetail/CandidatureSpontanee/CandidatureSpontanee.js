36f0e8e494fbe4c10fca3fe7e724feb4ec20c9de
David Boureau <bdavidxyz@gmail.com>
2022-01-06 11:32:50 +0100

    written without check

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.js
@@ -9,11 +9,19 @@ import toggleCandidature from "./services/toggleCandidature";
 import { getValidationSchema, getInitialSchemaValues } from "./services/getSchema";
 import { string_wrapper as with_str } from "../../../utils/wrapper_utils";
 import { capitalizeFirstLetter } from "../../../utils/strutils";
+import { useSessionStorage } from "../../../utils/useSessionStorage";
+import { getItemId } from "../../../utils/getItemId";
 
 const CandidatureSpontanee = (props) => {
   const [modal, setModal] = useState(false);
   const [sendingState, setSendingState] = useState("not_sent");
   const kind = props?.item?.ideaType || "";
+
+  const uniqId = (kind, item) => {
+    return `candidaturespontanee-${kind}-${getItemId(item)}`
+  }
+
+  const [applied, setApplied] = useSessionStorage(uniqId(kind, props.item), null);
 
   const toggle = () => {
     toggleCandidature({ modal, setSendingState, setModal });
@@ -28,48 +36,60 @@ const CandidatureSpontanee = (props) => {
     validationSchema: getValidationSchema(kind),
     onSubmit: async (applicantValues) => {
       await submitCandidature(applicantValues, setSendingState, props.item);
+      setApplied(new Date());
     },
   });
 
   return (
     <div className="c-candidature" data-testid="CandidatureSpontanee">
-      <div className="c-detail-description-me col-12 col-md-5">
-        <div className="c-detail-pelink my-3">
-          <Button
-            onClick={toggle}
-            className={`btn btn-dark ml-1 gtmFormulaireCandidature gtm${capitalizeFirstLetter(kind)}`}
-            aria-label="jenvoie-une-candidature-spontanee"
-          >
-            J'envoie ma candidature{with_str(kind).amongst(["lbb", "lba"]) ? " spontanée" : ""}
-          </Button>
-        </div>
-      </div>
+      {
+        !!applied ?
+          <>
+            Vous avez déjà postulé le {applied}
+          </>
+        :
+          <>
 
-      <Modal isOpen={modal} toggle={toggle} className={"c-candidature-modal"} backdrop="static">
-        <form onSubmit={formik.handleSubmit} className="c-candidature-form">
-          <ModalHeader toggle={toggle} className={"c-candidature-modal-header"}></ModalHeader>
+            <div className="c-detail-description-me col-12 col-md-5">
+              <div className="c-detail-pelink my-3">
+                <Button
+                  onClick={toggle}
+                  className={`btn btn-dark ml-1 gtmFormulaireCandidature gtm${capitalizeFirstLetter(kind)}`}
+                  aria-label="jenvoie-une-candidature-spontanee"
+                >
+                  J'envoie ma candidature{with_str(kind).amongst(["lbb", "lba"]) ? " spontanée" : ""}
+                </Button>
+              </div>
+            </div>
 
-          {with_str(sendingState).amongst(["not_sent", "currently_sending"]) ? (
-            <CandidatureSpontaneeNominalBodyFooter
-              formik={formik}
-              sendingState={sendingState}
-              company={props?.item?.company?.name}
-              item={props?.item}
-              kind={kind}
-            />
-          ) : (
-            <></>
-          )}
+            <Modal isOpen={modal} toggle={toggle} className={"c-candidature-modal"} backdrop="static">
+              <form onSubmit={formik.handleSubmit} className="c-candidature-form">
+                <ModalHeader toggle={toggle} className={"c-candidature-modal-header"}></ModalHeader>
 
-          {with_str(sendingState).amongst(["ok_sent"]) ? (
-            <CandidatureSpontaneeWorked kind={kind} email={formik.values.email} company={props?.item?.company?.name} />
-          ) : (
-            <></>
-          )}
+                {with_str(sendingState).amongst(["not_sent", "currently_sending"]) ? (
+                  <CandidatureSpontaneeNominalBodyFooter
+                    formik={formik}
+                    sendingState={sendingState}
+                    company={props?.item?.company?.name}
+                    item={props?.item}
+                    kind={kind}
+                  />
+                ) : (
+                  <></>
+                )}
 
-          {with_str(sendingState).amongst(["not_sent_because_of_errors"]) ? <CandidatureSpontaneeFailed /> : <></>}
-        </form>
-      </Modal>
+                {with_str(sendingState).amongst(["ok_sent"]) ? (
+                  <CandidatureSpontaneeWorked kind={kind} email={formik.values.email} company={props?.item?.company?.name} />
+                ) : (
+                  <></>
+                )}
+
+                {with_str(sendingState).amongst(["not_sent_because_of_errors"]) ? <CandidatureSpontaneeFailed /> : <></>}
+              </form>
+            </Modal>
+          
+          </>
+      }
     </div>
   );
 };
