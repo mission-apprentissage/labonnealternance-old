bbc547490251b8503dcf78c63d054c4f44345998
David Boureau <bdavidxyz@gmail.com>
2022-01-27 11:43:28 +0100

    simplified version

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.js
@@ -23,7 +23,6 @@ const CandidatureSpontanee = (props) => {
   }
 
   const actualLocalStorage = props.fakeLocalStorage || window.localStorage || {}
-
   
   const toggle = () => {
     toggleCandidature({ modal, setSendingState, setModal });
@@ -31,16 +30,14 @@ const CandidatureSpontanee = (props) => {
   
   const [applied, setApplied] = useLocalStorage(uniqId(kind, props.item), null, actualLocalStorage);
 
-
   useEffect(() => {
     setModal(false);
 
     // HACK HERE : reapply setApplied to currentUniqId to re-detect 
     // if user already applied each time the user swap to another item.
     let currentUniqId = actualLocalStorage.getItem(uniqId(kind, props.item));
-    if ('null'.includes(currentUniqId)) {
-      setApplied('null')
-    } else {
+    console.log('currentUniqId', currentUniqId);
+    if (currentUniqId) {
       setApplied(currentUniqId)
     }
   }, [props?.item]);
