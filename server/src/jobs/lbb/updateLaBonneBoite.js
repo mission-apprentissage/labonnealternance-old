8ffcab0d4910b10b67c082e5bf2a52f6057680e8
Alan Le Ruyet <alan.leruyet@free.fr>
2022-02-01 17:23:44 +0100

    refacto

--- server/src/jobs/lbb/updateLaBonneBoite.js
+++ server/src/jobs/lbb/updateLaBonneBoite.js
@@ -13,6 +13,7 @@ const initCBSPredictionMap = require("./initCBSPredictionMap.js");
 const { logMessage } = require("../../common/utils/logMessage");
 const { mongooseInstance } = require("../../common/mongodb");
 const { initSAVERemoveMap, initSAVEUpdateMap, initSAVEAddMap } = require("./initSAVEMaps");
+const { updateSAVECompanies } = require("./updateLaBonneBoite");
 
 const defaultPredictionByROMEThreshold = 0.2; // 0.2 arbitraire
 const CBSPredictionByROMEThreshold = 4; // 4 arbitraire
@@ -210,50 +211,6 @@ const insertSAVECompanies = async () => {
   logMessage("info", "Ended insertSAVECompanies");
 };
 
-const updateSAVECompanies = async () => {
-  logMessage("info", "Starting updateSAVECompanies");
-  for (const key in updateMap) {
-    let company = updateMap[key];
-
-    let bonneBoite = await BonnesBoites.findOne({ siret: company.siret });
-
-    if (bonneBoite) {
-      // remplacement pour une bonneBoite trouvée par les données modifiées dans la table update SAVE
-      if (company.raisonsociale) {
-        bonneBoite.raisonsociale = company.raisonsociale;
-        bonneBoite.enseigne = company.enseigne;
-      }
-
-      if (company?.email === "remove") {
-        bonneBoite.email = "";
-      } else if (company.email) {
-        bonneBoite.email = company.email;
-      }
-
-      if (company?.telephone === "remove") {
-        bonneBoite.telephone = "";
-      } else if (company.telephone) {
-        bonneBoite.telephone = company.telephone;
-      }
-
-      if (company?.website === "remove") {
-        bonneBoite.website = "";
-      } else if (company.website) {
-        bonneBoite.website = company.website;
-      }
-
-      bonneBoite.type = company.type;
-
-      if (company.romes) {
-        bonneBoite.romes = [...new Set(company.romes.concat(bonneBoite.romes))];
-      }
-
-      await bonneBoite.save();
-    }
-  }
-  logMessage("info", "Ended updateSAVECompanies");
-};
-
 /*
   Initialize bonneBoite from data, add missing data from maps, 
 */
@@ -368,7 +325,7 @@ module.exports = async ({
       }
 
       await insertSAVECompanies();
-      await updateSAVECompanies();
+      await updateSAVECompanies({ updateMap });
 
       if (shouldBuildIndex) {
         await rebuildIndex(BonnesBoites);
