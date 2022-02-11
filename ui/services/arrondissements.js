200d962362d418213d037e1e2ee8660f3c410c30
David Boureau <bdavidxyz@gmail.com>
2022-02-11 14:03:33 +0100

    refacto

--- ui/services/arrondissements.js
+++ ui/services/arrondissements.js
@@ -3,9 +3,13 @@ const simplifiedArrondissements = (rawItems, town) => {
   
   const result = JSON.parse(JSON.stringify(rawItems))
 
-  const firstIsInTown = result[0]?.label?.toLowerCase()?.indexOf(town?.toLowerCase()) >= 0
-  const secondIsInTown = result[1]?.label?.toLowerCase()?.indexOf(town?.toLowerCase()) >= 0
-  const secondHasArrondissement = result[1]?.label?.toLowerCase()?.indexOf('arrondissement') >= 0
+  const firstLabel = rawItems[0]?.label?.toLowerCase() || ''
+  const secondLabel = rawItems[1]?.label?.toLowerCase() || ''
+
+  const firstIsInTown = firstLabel.indexOf(town?.toLowerCase()) >= 0
+  const secondIsInTown = secondLabel.indexOf(town?.toLowerCase()) >= 0
+  const secondHasArrondissement = secondLabel.indexOf('arrondissement') >= 0
+  
   if (firstIsInTown && secondIsInTown && secondHasArrondissement) {
     // Only town will be shown
     result[0].label = result[0].label.split(' ')[0]
