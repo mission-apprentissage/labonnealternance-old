d55d94fa14a2e49f4d5461c26ab98f51fda32a8f
David Boureau <bdavidxyz@gmail.com>
2022-02-11 12:26:29 +0100

    all tests passe

--- ui/services/arrondissements.test.js
+++ ui/services/arrondissements.test.js
@@ -9,13 +9,37 @@ describe('arrondissements', () => {
     expect(nantesItems).toEqual(nantesItems);
     expect(res[0].label).toEqual('nantes');
   });
-
+  
   it('.simplifiedItems : Do not change anything outsite Paris, Lyon, Marseille', async () => {
     let nantesItems = getTownItems('nantes')
     expect(nantesItems[0].label).not.toEqual('nantes');
     let res = simplifiedItems(nantesItems, 'nantes')
     expect(nantesItems).toEqual(nantesItems);
     expect(res).toEqual(nantesItems);
+  });
+  
+  it('.simplifiedItems : Change first item for Paris', async () => {
+    let parisItems = getTownItems('Paris')
+    expect(parisItems[0].label).not.toEqual('Paris');
+    let res = simplifiedItems(parisItems, 'Paris')
+    expect(parisItems).toEqual(parisItems);
+    expect(res[0].label).toEqual('Paris');
+  });
+
+  it('.simplifiedItems : Change first item for Lyon', async () => {
+    let parisItems = getTownItems('Lyon')
+    expect(parisItems[0].label).not.toEqual('Lyon');
+    let res = simplifiedItems(parisItems, 'Lyon')
+    expect(parisItems).toEqual(parisItems);
+    expect(res[0].label).toEqual('Lyon');
+  });
+
+  it('.simplifiedItems : Change first item for Marseille', async () => {
+    let parisItems = getTownItems('Marseille')
+    expect(parisItems[0].label).not.toEqual('Marseille');
+    let res = simplifiedItems(parisItems, 'Marseille')
+    expect(parisItems).toEqual(parisItems);
+    expect(res[0].label).toEqual('Marseille');
   });
 
   let getTownItems = function (townName) {
