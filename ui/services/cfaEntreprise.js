f88f523e61ae16836f11cb860097a220183d8cff
Alan Le Ruyet <alan.leruyet@free.fr>
2022-02-22 23:59:00 +0100

    1.8.2 (#538)

--- ui/services/cfaEntreprise.js
+++ ui/services/cfaEntreprise.js
@@ -28,6 +28,6 @@ const listeCfaEntreprise = [
   "88514020200015",
 ];
 
-export const isCfaEntreprise = (siret) => {
-  return listeCfaEntreprise.indexOf(siret) >= 0;
+export const isCfaEntreprise = (siret, siretGestionnaire) => {
+  return listeCfaEntreprise.indexOf(siret) >= 0 || listeCfaEntreprise.indexOf(siretGestionnaire) >= 0;
 };
