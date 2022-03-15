dd547827dd3c598081ac92bd06e3e9d37bb8359a
Alan Le Ruyet <alan.leruyet@free.fr>
2022-03-15 10:33:30 +0100

    Suppression éléments de  contexte inutile

--- ui/components/WidgetHeader/InitWidgetSearchParameters.js
+++ ui/components/WidgetHeader/InitWidgetSearchParameters.js
@@ -11,9 +11,7 @@ import { useDispatch, useSelector } from "react-redux";
 const InitWidgetSearchParameters = ({ setIsLoading, handleSearchSubmit, handleItemLoad }) => {
   const dispatch = useDispatch();
 
-  const { widgetParameters, itemParameters, shouldExecuteSearch, formValues, opcoFilter } = useSelector(
-    (state) => state.trainings
-  );
+  const { widgetParameters, itemParameters, shouldExecuteSearch, formValues } = useSelector((state) => state.trainings);
 
   useEffect(() => {
     // initialisation par les query params
@@ -70,7 +68,6 @@ const InitWidgetSearchParameters = ({ setIsLoading, handleSearchSubmit, handleIt
             romes: p.romes.split(","),
           },
           radius: p.radius || 30,
-          opco: opcoFilter,
         };
 
         if (p.lon || p.lat) {
