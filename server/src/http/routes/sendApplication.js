590662a2f1443887248b021b99056aa36509ea35
David Boureau <bdavidxyz@gmail.com>
2022-03-24 11:29:42 +0100

    removed cnsl

--- server/src/http/routes/sendApplication.js
+++ server/src/http/routes/sendApplication.js
@@ -109,7 +109,6 @@ module.exports = (components) => {
     "/intentionComment",
     limiter1Per20Second,
     tryCatch(async (req, res) => {
-      console.log("req.body for /intentionComment", req.body);
       const result = await saveApplicationIntentionComment({
         query: req.body,
         ...components,
