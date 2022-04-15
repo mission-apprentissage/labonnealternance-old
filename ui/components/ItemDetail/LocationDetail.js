cb2e280097b3451f50a82040adb7ddefd96461c7
David Boureau <bdavidxyz@gmail.com>
2022-04-15 11:14:09 +0200

    LocationDetail ok

--- ui/components/ItemDetail/LocationDetail.js
+++ ui/components/ItemDetail/LocationDetail.js
@@ -2,6 +2,9 @@
 const LocationDetail = ({ item }) => {
   return (
     <>
+      <div className="c-detail-body mt-4">
+        {item?.title} Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolor asperiores, veniam quaerat deserunt deleniti eligendi minus necessitatibus, eos enim architecto cum fugiat ad praesentium optio qui itaque. Et, placeat?
+      </div>
     </>
   );
 };
