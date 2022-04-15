0ad6e594534e2c1a7f0b0033635feaab6709fcc5
David Boureau <bdavidxyz@gmail.com>
2022-04-15 12:07:21 +0200

    distance ok

--- ui/components/ItemDetail/LocationDetail.js
+++ ui/components/ItemDetail/LocationDetail.js
@@ -1,23 +1,32 @@
 import { capitalizeFirstLetter } from "../../utils/strutils";
 import { getPathLink } from "../../utils/tools";
+import { round } from "lodash";
 
 const LocationDetail = ({ item }) => {
 
   const kind = item?.ideaType;
-  let contactPhone = item?.contact?.phone;
   const title = kind === "formation" ? "Quelques informations sur le centre de formation" : "Quelques informations sur l'établissement"
+
   return (
     <>
       <div className="c-detail-body c-locationdetail mt-4">
+
         <div className="c-locationdetail-title">
           {title}
         </div>
+
         <div className="c-locationdetail-address">
-          2 rue truc
+          {item?.place?.fullAddress}
         </div>
-        <div className="c-locationdetail-distance">
-          2km du lieu de recherche
-        </div>
+
+        {item?.place?.distance ? (
+          <div className="c-locationdetail-distance">
+            {`${round(item?.place?.distance, 1)} km(s) du lieu de recherche`} 
+          </div>
+        ) : (
+          ""
+        )}
+
         <div className="c-locationdetail-itinerary">
           <span className="d-block mt-2">
             <span>
@@ -37,9 +46,11 @@ const LocationDetail = ({ item }) => {
             </span>
           </span>
         </div>
+
         <div className="c-locationdetail-knowmore">
           En savoir plus
         </div>
+
       </div>
     </>
   );
