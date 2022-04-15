fada01b64ac8304f78503ae5f2b195e392827331
David Boureau <bdavidxyz@gmail.com>
2022-04-15 11:40:53 +0200

    locationdetail ok

--- ui/components/ItemDetail/LocationDetail.js
+++ ui/components/ItemDetail/LocationDetail.js
@@ -1,9 +1,27 @@
 
 const LocationDetail = ({ item }) => {
+
+  const kind = item?.ideaType;
+  let contactPhone = item?.contact?.phone;
+  const title = kind === "formation" ? "Quelques informations sur le centre de formation" : "Quelques informations sur l'établissement"
   return (
     <>
-      <div className="c-detail-body mt-4">
-        {item?.title} Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolor asperiores, veniam quaerat deserunt deleniti eligendi minus necessitatibus, eos enim architecto cum fugiat ad praesentium optio qui itaque. Et, placeat?
+      <div className="c-detail-body c-locationdetail mt-4">
+        <div className="c-locationdetail-title">
+          {title}
+        </div>
+        <div className="c-locationdetail-address">
+          2 rue truc
+        </div>
+        <div className="c-locationdetail-distance">
+          2km du lieu de recherche
+        </div>
+        <div>
+          Obtenir l'itinéraire
+        </div>
+        <div>
+          En savoir plus
+        </div>
       </div>
     </>
   );
