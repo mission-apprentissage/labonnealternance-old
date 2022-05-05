61785e7408944f38cd5649cc3b7c5ed8bb66a048
David Boureau <bdavidxyz@gmail.com>
2022-05-05 16:15:37 +0200

    h2 title

--- ui/components/ItemDetail/MatchaDetail.js
+++ ui/components/ItemDetail/MatchaDetail.js
@@ -72,162 +72,27 @@ const MatchaDetail = ({ job, seeInfo, setSeeInfo }) => {
 
   return (
     <>
-      <p className="mb-3 d-flex">
-        <span className="c-detail-sizetitle">Activité</span>
-        <span className="c-detail-sizetext d-block ml-2">{libelleNaf}</span>
-      </p>
-      <p className="mb-3 d-flex">
-        <span className="c-detail-sizetitle">Date de creation</span>
-        <span className="c-detail-sizetext d-block ml-2">{dateCreationEtablissementFormated}</span>
-      </p>
-      {trancheEffectif && (
-        <p className="mb-3 d-flex">
-          <span className="c-detail-sizetitle">Taille de l'entreprise</span>
-          <span className="c-detail-sizetext d-block ml-2">{trancheEffectif}</span>
-        </p>
-      )}
-      {contactPhone ? (
-        <div className="d-flex">
-          {seeInfo ? (
-            <>
-              <span className="d-block">
-                <img className="cardIcon" src={contactIcon} alt="" />
-              </span>
-              <span className="ml-2 d-block">
-                <span className="c-detail-address d-block">{contactInfo}</span>
-              </span>
-            </>
-          ) : (
-            <button
-              className={`c-see-info c-see-info--matcha d-block btn btn-outline-primary gtmContact gtm${capitalizeFirstLetter(
-                kind
-              )}`}
-              onClick={() => setSeeInfo(true)}
-            >
-              Voir les informations de contact
-            </button>
-          )}
+      <h2 className="c-locationdetail-title mt-2">Description de l'offre</h2>
+      <div className="c-matcha-detail-container">
+        <div>
+          <strong>Début du contrat le :</strong>
         </div>
-      ) : (
-        ""
-      )}
-      <p className="text-left">
-        <span className={"d-block c-detail-sizetext c-detail-sizetext--" + kind}>
-          <img className="mt-n1" src="/images/info.svg" alt="information" />
-          <span className="ml-2 c-detail-knowmore">En savoir plus sur </span>
-          <a
-            href={`https://www.google.fr/search?q=${getGoogleSearchParameters()}`}
-            target="_blank"
-            className="c-detail-google-search gtmGoogleLink"
-            rel="noopener noreferrer"
-          >
-            {job.company.name}
-          </a>
-        </span>
-      </p>
-      <hr className={"mb-4 c-detail-header-separator c-detail-header-separator--" + kind} />
-      <div>
-        <div className="c-detail-company position-relative">
-          <span className="c-detail-square">&nbsp;</span>
-          <span className="d-inline-block ml-2">
-            {get(job, "company.name", ReactHtmlParser("<em>Entreprise non précisée</em>"))}
-          </span>
-          <span className="c-detail-proposal"> propose actuellement cette offre</span>
+        <div>
+          <strong>Nature du contrat :</strong>
         </div>
-        <h2 className="c-detail-jobtitle">{jobTitle}</h2>
-
-        <div className="c-detail-meta">
-          <div className="c-detail-metadate">
-            Début de contrat : {defaultTo(jobStartDate, ReactHtmlParser("<em>Donnée manquante</em>"))}
-          </div>
-          <div className="c-detail-metanature">
-            Nature du contrat :{" "}
-            {defaultTo(getContractTypes(contractType), ReactHtmlParser("<em>Donnée manquante</em>"))}
-          </div>
-          <div className="c-detail-metarythmealternance">
-            Rythme de l’alternance :{" "}
-            {defaultTo(rythmeAlternance, ReactHtmlParser("<em>Donnée non renseigné par l'entreprise</em>"))}
-          </div>
-          <div className="c-detail-metadureecontrat">
-            Durée du contrat :{" "}
-            {defaultTo(dureeContrat, ReactHtmlParser("<em>Donnée non renseigné par l'entreprise</em>"))}
-          </div>
-          <div className="c-detail-metaquantitecontrat">
-            Nombre de poste(s) disponible(s) :{" "}
-            {defaultTo(quantiteContrat, ReactHtmlParser("<em>Donnée manquante</em>"))}
-          </div>
-          {elligibleHandicapBoolean && (
-            <div className="c-detail-metahandicap">
-              Poste ouvert aux personnes en situation de handicap : {elligibleHandicap}
-            </div>
-          )}
+        <div>
+          <strong>Niveau requis :</strong>
         </div>
-
-        <div className="c-detail-description">
-          <h3 className="c-detail-description-title c-detail-description-title--matcha1">Niveau requis</h3>
-          {isNonEmptyString(job?.diplomaLevel)
-            ? job.diplomaLevel.split(", ").map((diploma, indx) => {
-                return (
-                  <div className="c-detail-diploma d-inline-block" key={indx}>
-                    {diploma}
-                  </div>
-                );
-              })
-            : "Non défini"}
-        </div>
-
-        {description ? (
-          <div className="c-detail-description">
-            <h3 className="c-detail-description-title">Description de l'offre</h3>
-            <div className="c-detail-description-text">{ReactHtmlParser(md.render(description))}</div>
-          </div>
-        ) : (
-          ""
-        )}
-
-        {romeDefinition && romeDefinition.length && (
-          <div className="c-detail-description">
-            <h3 className="c-detail-description-title">Description du métier</h3>
-            <ul>
-              {romeDefinition.map((definition) => (
-                <li>{definition}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-
-        {romeCompetence && romeCompetence.length && (
-          <div className="c-detail-description">
-            <h3 className="c-detail-description-title">Compétences visées</h3>
-            <ul>
-              {romeCompetence.map((competence) => (
-                <li>{competence.libelle}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-
-        <hr className="c-detail-header-separator" />
-
-        <div className="c-detail-advice c-detail-advice--matcha">
-          <div className="c-detail-advice__figure">
-            <img src={questionmarkIcon} alt="point d'interrogation" />
-          </div>
-          <div className="c-detail-advice__body">
-            <div className="c-detail-advice-text mt-0">
-              <span className="c-detail-advice-highlight"> {job.company.name} </span> nous a récemment fait parvenir un
-              besoin de recrutement :<span className="c-detail-advice-highlight"> {jobTitle}</span>. Cela signifie que
-              l'établissement est activement à la recherche d’un.e candidat.e.
-            </div>
-            <div className="c-detail-advice-text c-detail-advice-text--tag">
-              Vous avez donc tout intérêt à le contacter rapidement, avant que l’offre ne soit pourvue !
-            </div>
-            {!!random(0, 1) ? <DidAsk1 /> : <DidAsk2 />}
-          </div>
-        </div>
-
-        <div className="mt-3">&nbsp;</div>
       </div>
+      <p>
+        {job.company.name}  nous a récemment fait parvenir un besoin de recrutement :  Coiffure, CAP / BEP. Cela signifie que l'établissement est activement à la recherche d'un.e candidat.e.
+      </p>
+      <p>
+        Vous avez donc tout intérêt à le contacter rapidement, avant que l'offre ne soit pourvue !
+      </p>
+      <p>
+        Trouver et convaincre une entreprise de vous embaucher ? On vous donne des conseils ici pour vous aider !
+      </p>
     </>
   );
 };
