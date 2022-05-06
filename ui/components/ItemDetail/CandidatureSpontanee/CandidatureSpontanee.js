0e3b4155ca56a80707ebeeba3394e0e5d746d22e
David Boureau <bdavidxyz@gmail.com>
2022-05-06 17:11:22 +0200

    color of button

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontanee.js
@@ -74,7 +74,7 @@ const CandidatureSpontanee = (props) => {
             <>
               <Button
                 onClick={toggle}
-                className={`btn btn-dark ml-1 gtmFormulaireCandidature gtm${capitalizeFirstLetter(kind)}`}
+                  className={`btn btn-primary ml-1 gtmFormulaireCandidature gtm${capitalizeFirstLetter(kind)}`}
                 aria-label="jenvoie-une-candidature-spontanee"
               >
                 J'envoie ma candidature{with_str(kind).amongst(["lbb", "lba"]) ? " spontanée" : ""}
