4319536cc40d099f94d216b4c4ab77e6852b2b3e
Alan Le Ruyet <alan.leruyet@free.fr>
2022-05-11 15:55:24 +0200

    works with style

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeMandataireKnowMoreCheckbox.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeMandataireKnowMoreCheckbox.js
@@ -1,32 +1,21 @@
 import React from "react";
 
 const CandidatureSpontaneeMandataireKnowMoreCheckbox = ({ formik, item }) => {
+
   return item?.company?.mandataire ? (
-    <div className="c-candidature-terms mt-3">
-      <table className="c-candidature-terms--knowmore">
-        <tbody>
-          <tr>
-            <td className="align-top pr-2">
-              <input
-                name="interetOffresMandataire"
-                type="checkbox"
-                id="CandidatureSpontaneeMandataireKnowMoreCheckbox"
-                className="form-check-input"
-                value="yes"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                checked={formik.values.interetOffresMandataire ? true : false}
-              />
-            </td>
-            <td>
-              <label htmlFor="CandidatureSpontaneeMandataireKnowMoreCheckbox">
-                Je souhaite en savoir plus sur les opportunités de formations de cet établissement.
-              </label>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <fieldset data-testid="fieldset-interetOffresMandataire" className={`c-candidature-terms mt-3`}>
+      <label htmlFor="interetOffresMandataire" className="c-candidature-terms-text">
+        <input
+          id="interetOffresMandataire"
+          name="interetOffresMandataire"
+          type="checkbox"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.interetOffresMandataire}
+        />
+        <div>Je souhaite en savoir plus sur les opportunités de formations de cet établissement.</div>
+      </label>
+    </fieldset>
   ) : (
     ""
   );
