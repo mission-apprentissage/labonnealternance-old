0ac318450bbbcf9917b19d10303d92b79d619901
Alan Le Ruyet <alan.leruyet@free.fr>
2022-05-11 16:00:19 +0200

    well now it has style

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeMandataireKnowMoreCheckbox.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeMandataireKnowMoreCheckbox.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const CandidatureSpontaneeMandataireKnowMoreCheckbox = ({ formik, item }) => {
-
   return item?.company?.mandataire ? (
-    <fieldset data-testid="fieldset-interetOffresMandataire" className={`c-candidature-terms mt-3`}>
+    <fieldset
+      data-testid="fieldset-interetOffresMandataire"
+      className="c-candidature-terms c-candidature-terms--knowmore mt-3"
+    >
       <label htmlFor="interetOffresMandataire" className="c-candidature-terms-text">
         <input
           id="interetOffresMandataire"
