f8646d0fd5e5713439b30f545b00b49f5976cd91
Alan Le Ruyet <alan.leruyet@free.fr>
2022-06-09 11:31:09 +0200

    wip logos partenaires

--- ui/components/HomeComponents/PartenairesDeConfiance.js
+++ ui/components/HomeComponents/PartenairesDeConfiance.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 const PartenairesDeConfiance = () => {
-  return "Ils nous font confiance";
+  return (
+    <div>
+      <h2 className="c-homecomponent-title__confiance">Ils nous font confiance</h2>
+
+      <div className="row">
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+      </div>
+      <div className="row">
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+      </div>
+      <div className="row">
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+        <div className="col-4 col-md-2"><img className="c-homecomponent-logopartenaire" src="/images/logos_partenaires/logo-cci@2x.png" alt="Logo partenaire cci" /></div>
+      </div>
+    </div>
+  );
 };
 
 export default PartenairesDeConfiance;
