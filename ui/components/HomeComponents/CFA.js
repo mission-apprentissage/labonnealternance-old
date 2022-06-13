c9c528fe580b1d1f820573b038d12616ed951629
Alan Le Ruyet <alan.leruyet@free.fr>
2022-06-13 10:21:38 +0200

    bloc cfa done

--- ui/components/HomeComponents/CFA.js
+++ ui/components/HomeComponents/CFA.js
@@ -6,16 +6,22 @@ const CFA = () => {
     <section className="p-3 mb-2 mb-md-5">
       <div className="row">
         <div className="col-12 col-md-6 d-none d-md-block">
-          <img className="c-homecomponent-illustration mr-3 my-3" src="/images/home_pics/illu-votrebesoin.svg" alt="" />
+          <img
+            className="c-homecomponent-illustration mr-3 my-3"
+            src="/images/home_pics/illu-entreprisesmandatees.svg"
+            alt=""
+          />
         </div>
         <div className="col-12 col-md-6 order-md-first">
-          <h1 className="c-homecomponent-title c-homecomponent-title__blue mb-3">Vous êtes une entreprise</h1>
+          <h1 className="c-homecomponent-title c-homecomponent-title__blue mb-3">
+            Vous êtes un organisme de formation
+          </h1>
           <h2 className="c-homecomponent-title__small mb-3 mb-lg-5">
-            Diffusez simplement et gratuitement vos offres en alternance.
+            Diffusez et gérez simplement les offres d’emploi de vos entreprises partenaires.
           </h2>
           <div>
-            Exprimez vos besoins en alternance afin d’être visible auprès des jeunes en recherche de contrat, et des
-            centres de formation pouvant vous accompagner.
+            Créez le compte de votre CFA pour administrer vos offres et être contacté par des entreprises à la recherche
+            d’alternants.
           </div>
           <ConnectionActions service="cfa" />
         </div>
