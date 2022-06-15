fa340b60f5ebf243057edefd5b624337b006e479
Alan Le Ruyet <alan.leruyet@free.fr>
2022-06-15 09:14:15 +0200

    Ajustements

--- ui/components/HomeComponents/GerezEntreprise.js
+++ ui/components/HomeComponents/GerezEntreprise.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const GerezEntreprise = () => {
   return (
-    <section className="c-homecomponent__beige p-3 mb-5">
+    <section className="c-homecomponent__beige p-3">
       <div className="row">
         <div className="col-12 col-md-6">
           <img className="c-homecomponent-illustration mr-3 my-3" src="/images/home_pics/illu-listeoffres.svg" alt="" />
         </div>
-        <div className="col-12 col-md-6 pt-md-5 mb-5">
+        <div className="col-12 col-md-6 pt-md-5 mb-2 mb-md-5">
           <h2 className="c-homecomponent-title__small mb-3">
             Gérez vos entreprises partenaires et leurs offres grâce à un tableau de bord
           </h2>
