7236b8c8054c91d6ce91ceeffddece35d4ca4671
David Boureau <bdavidxyz@gmail.com>
2022-06-21 13:54:29 +0200

    better algo

--- ui/components/ItemDetail/ItemDetailServices/hasAlsoEmploi.js
+++ ui/components/ItemDetail/ItemDetailServices/hasAlsoEmploi.js
@@ -4,22 +4,13 @@ import { isNonEmptyString } from "../../../utils/strutils";
 export default function hasAlsoEmploi({ isCfa = false, company = {}, searchedMatchaJobs = [] }) {
   let hasEtablissementFormateurSiret = false
   let hasEtablissementGestionnaireSiret = false
-  let currentSiret = company?.siret
-  console.log('currentSiret', currentSiret);
-  console.log('company', company);
-  console.log('searchedMatchaJobs', searchedMatchaJobs);
   let matchaSirets = searchedMatchaJobs.map((matcha) => matcha?.company?.siret)
-  console.log('matchaSirets', matchaSirets);
   let nonEmptyMatchaSirets = matchaSirets.filter(isNonEmptyString)
-  console.log('nonEmptyMatchaSirets', nonEmptyMatchaSirets);
-  if (nonEmptyMatchaSirets.length > 0 && isNonEmptyString(currentSiret)) {
-    hasEtablissementFormateurSiret = nonEmptyMatchaSirets.includes(currentSiret)
-    console.log('hasEtablissementFormateurSiret', hasEtablissementFormateurSiret);
+  if (nonEmptyMatchaSirets.length > 0 && isNonEmptyString(company?.siret)) {
+    hasEtablissementFormateurSiret = nonEmptyMatchaSirets.includes(company?.siret)
   }
-  if (nonEmptyMatchaSirets.length > 0 && isNonEmptyString(currentSiret)) {
+  if (nonEmptyMatchaSirets.length > 0 && isNonEmptyString(company?.headquarter?.siret)) {
     hasEtablissementGestionnaireSiret = nonEmptyMatchaSirets.includes(company?.headquarter?.siret)
-    console.log('hasEtablissementGestionnaireSiret', hasEtablissementGestionnaireSiret);
   }
-  console.log('isCfa', isCfa);
   return isCfa || hasEtablissementFormateurSiret || hasEtablissementGestionnaireSiret
 }
