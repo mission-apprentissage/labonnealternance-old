03fd164082484c74dacb00a7616bd5bb1ff1f471
David Boureau <bdavidxyz@gmail.com>
2022-07-08 15:23:45 +0200

    faq route ok

---
+++ server/src/http/routes/faq.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { NotionAPI } = require("notion-client");
+const tryCatch = require("../middlewares/tryCatchMiddleware");
+
+const notion = new NotionAPI();
+
+module.exports = () => {
+  const router = express.Router();
+
+  router.get(
+    "/",
+    tryCatch(async ({}, res) => {
+      const recordMap = await notion.getPage("b1804fe13330409c8baf775c683821b0");
+
+      let pageTitle = "";
+      const keys = Object.keys(recordMap.block);
+      for (let index = 0; index < keys.length; index++) {
+        const element = keys[index];
+        const blockValue = recordMap.block[element].value;
+        if (blockValue.type === "page") {
+          const title = blockValue.properties.title[0][0];
+          if (title !== "Documentation") {
+            pageTitle = title;
+          }
+        }
+      }
+
+      return res.json({ ...recordMap, pageTitle });
+    })
+  );
+
+  return router;
+};
