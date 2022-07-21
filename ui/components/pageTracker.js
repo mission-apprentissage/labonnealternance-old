7c7168b454149c021abc3e1bd84c3f7b7521553c
Alan Le Ruyet <alan.leruyet@free.fr>
2022-07-21 15:53:09 +0200

    Réduction event pageView et nettoyage

--- ui/components/pageTracker.js
+++ ui/components/pageTracker.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { SendPlausibleEvent } from "utils/gtm";
+import { SendPlausibleEvent } from "utils/plausible";
 
 const PageTracker = ({ children }) => {
   const router = useRouter();
 
   React.useEffect(() => {
     const handleRouteChange = (url) => {
-      SendPlausibleEvent("pageview", { url });
+      if (url.indexOf("?") < 0) {
+        SendPlausibleEvent("pageview", { url });
+      }
     };
 
     router.events.on("routeChangeStart", handleRouteChange);
--- ui/utils/plausible.js
+++ ui/utils/plausible.js
@@ -1,6 +1,5 @@
 export const SendPlausibleEvent = (name, props) => {
   if (typeof window !== "undefined" && window?.plausible) {
     window.plausible(name, { props });
-    console.log("sendplausibleevent : ",name,props);
   }
 };
