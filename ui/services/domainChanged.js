435e096ba1d00f2ba1b8c9fb7c451a4090238719
Alan Le Ruyet <alan.leruyet@free.fr>
2022-07-21 18:17:54 +0200

    Gestion des requêtes annulées pour éviter les tracking inutiles

--- ui/services/domainChanged.js
+++ ui/services/domainChanged.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import fetchRomes from "../services/fetchRomes";
 import { SendPlausibleEvent } from "../utils/plausible";
 
@@ -7,13 +6,16 @@ export default async function domainChanged(val, setDomainErrorFunc) {
     setDomainErrorFunc(true);
   });
 
+  if (res === "cancelled") {
+    return [];
+  }
+
   // tracking des recherches sur table domaines métier que lorsque le mot recherché fait au moins trois caractères
   if (val && val.length > 2) {
-    if(res.length) {
-      SendPlausibleEvent("Mots clefs les plus recherchés",{terme:val.toLowerCase(), hits: res.length});
-    }
-    else {
-      SendPlausibleEvent("Mots clefs ne retournant aucun résultat",{terme:val.toLowerCase()});
+    if (res.length) {
+      SendPlausibleEvent("Mots clefs les plus recherchés", { terme: val.toLowerCase(), hits: res.length });
+    } else {
+      SendPlausibleEvent("Mots clefs ne retournant aucun résultat", { terme: val.toLowerCase() });
     }
   }
 
