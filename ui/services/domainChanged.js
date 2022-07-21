f4b86ac732396afdb09677c66a19a9770013f783
Alan Le Ruyet <alan.leruyet@free.fr>
2022-07-21 16:39:05 +0200

    lowercase

--- ui/services/domainChanged.js
+++ ui/services/domainChanged.js
@@ -10,10 +10,10 @@ export default async function domainChanged(val, setDomainErrorFunc) {
   // tracking des recherches sur table domaines métier que lorsque le mot recherché fait au moins trois caractères
   if (val && val.length > 2) {
     if(res.length) {
-      SendPlausibleEvent("Mots clefs les plus recherchés",{terme:val, hits: res.length});
+      SendPlausibleEvent("Mots clefs les plus recherchés",{terme:val.toLowerCase(), hits: res.length});
     }
     else {
-      SendPlausibleEvent("Mots clefs ne retournant aucun résultat",{terme:val});
+      SendPlausibleEvent("Mots clefs ne retournant aucun résultat",{terme:val.toLowerCase()});
     }
   }
 
