bbd93c975eeb032c3f3f119458bde219aac20499
David Boureau <bdavidxyz@gmail.com>
2022-08-22 15:37:43 +0200

    another section

---
+++ ui/components/HomeComponents/FollowLinkedIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FollowLinkedIn = () => {
+  return (
+    <section className="c-homecomponent__beige p-3 mb-5">
+
+    </section>
+  );
+};
+
+export default FollowLinkedIn;
--- ui/pages/acces-recruteur.js
+++ ui/pages/acces-recruteur.js
@@ -7,8 +7,7 @@ import PostezVotreOffre from "components/HomeComponents/PostezVotreOffre";
 import OffresGratuites from "components/HomeComponents/OffresGratuites";
 import OrganismesMandataires from "components/HomeComponents/OrganismesMandataires";
 import GerezOffres from "components/HomeComponents/GerezOffres";
-import AmeliorerLBA from "components/HomeComponents/AmeliorerLBA";
-import PartenairesDeConfiance from "components/HomeComponents/PartenairesDeConfiance";
+import FollowLinkedIn from "components/HomeComponents/FollowLinkedIn";
 import ConnectionActions from "components/HomeComponents/ConnectionActions";
 
 import { NextSeo } from "next-seo";
@@ -37,6 +36,9 @@ const AccesRecruteur = () => (
 
       <section className="c-homecomponent__beige">
         <GerezOffres />
+      </section>
+      <section className="c-homecomponent__beige">
+        <FollowLinkedIn />
       </section>
 
       <section className="text-center mb-5">
