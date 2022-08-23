e9f14bd7554beba1c8f3249aff00618660d39f6d
David Boureau <bdavidxyz@gmail.com>
2022-08-23 16:04:13 +0200

    first comp

---
+++ ui/components/HomeComponents/AlgoExplainedHome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const AlgoExplainedHome = () => {
+  return (
+    <section className="">
+      AlgoExplainedHome
+    </section>
+  );
+};
+
+export default AlgoExplainedHome;
