602f677c92a6c4ea41fa3040ebda051b6889f9d3
David Boureau <bdavidxyz@gmail.com>
2022-08-23 17:12:45 +0200

    added tags

--- ui/components/HomeComponents/AlgoHome.js
+++ ui/components/HomeComponents/AlgoHome.js
@@ -1,10 +1,12 @@
 import React from "react";
+import TagCandidatureSpontanee from "../../components/ItemDetail/TagCandidatureSpontanee.js";
+import TagOffreEmploi from "../../components/ItemDetail/TagOffreEmploi.js";
 
 
 
 const AlgoHome = () => {
   return (
-    <section className="c-algo">
+    <section className="c-algo py-5">
       <div className="">
         <h2 className="c-algo-title">
           <span className="d-block c-algo-title__top">Vous révéler</span>
@@ -15,8 +17,8 @@ const AlgoHome = () => {
           La bonne alternance expose différents types d'opportunités d'emplois :
         </p>
         <ul className="c-algo-text">
-          <li><strong>Les offres d'emploi</strong>Les offres d'emploi : publiées sur notre plateforme ainsi que celles issues de Pôle emploi et ses partenaires. Elles sont identifiées grâce au tag.</li>
-          <li><strong>Les candidatures spontanées</strong> : correspondant au marché caché de l'emploi. Chaque mois, un algorithme prédictif de Pôle emploi analyse les recrutements des 6 années passées pour prédire ceux des 6 mois à venir. Grâce à ces données, il identifie une liste restreinte d'entreprises "à fort potentiel d'embauche en alternance" pour faciliter vos démarches de candidatures spontanées. Elles sont identifiées grâce au tag </li>
+          <li><strong>Les offres d'emploi</strong> : publiées sur notre plateforme ainsi que celles issues de Pôle emploi et ses partenaires. Elles sont identifiées grâce au tag <TagOffreEmploi />.</li>
+          <li className="pt-3"><strong>Les candidatures spontanées</strong> : correspondant au marché caché de l'emploi. Chaque mois, un algorithme prédictif de Pôle emploi analyse les recrutements des 6 années passées pour prédire ceux des 6 mois à venir. Grâce à ces données, il identifie une liste restreinte d'entreprises "à fort potentiel d'embauche en alternance" pour faciliter vos démarches de candidatures spontanées. Elles sont identifiées grâce au tag <TagCandidatureSpontanee/>.</li>
         </ul>
       </div>
       <div className="d-flex-center">
