7ee1293ef54cf32152649bd02bc6727a8c5e1c28
Alan Le Ruyet <alan.leruyet@free.fr>
2022-08-24 12:19:15 +0200

    test

--- server/src/common/components/clamav.js
+++ server/src/common/components/clamav.js
@@ -3,19 +3,20 @@ const config = require("config");
 
 module.exports = async () => {
   async function createClamscan() {
-    const clamscan = await new NodeClam().init({
-      debugMode: config.env === "local" ? true : false, // This will put some debug info in your js console
-      clamdscan: {
-        host: config.env === "local" ? "localhost" : "clamav",
-        port: 3310,
-        bypassTest: false,
-      },
-    });
+    try {
+      const clamscan = await new NodeClam().init({
+        debugMode: config.env === "local" ? true : false, // This will put some debug info in your js console
+        clamdscan: {
+          host: config.env === "local" ? "localhost" : "clamav",
+          port: 3310,
+          bypassTest: false,
+        },
+      });
 
-    const scanString = async (fileContent) => {
-      const Readable = require("stream").Readable;
+      const scanString = async (fileContent) => {
+        const Readable = require("stream").Readable;
 
-      /*
+        /*
       const eicarStr = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
       const utf8Encode = new TextEncoder();
       const eicarByteArray = utf8Encode.encode(eicarStr);
@@ -25,27 +26,31 @@ module.exports = async () => {
       console.log("scan result EICAR String : ", isInfected, viruses);
       */
 
-      // le fichier est encodé en base 64 à la suite d'un en-tête.
-      const decodedAscii = Readable.from(
-        Buffer.from(fileContent.substring(fileContent.indexOf(";base64,") + 8), "base64").toString("ascii")
-      );
-      const rs = Readable.from(decodedAscii);
-      const { isInfected, viruses } = await clamscan.scanStream(rs);
+        // le fichier est encodé en base 64 à la suite d'un en-tête.
+        const decodedAscii = Readable.from(
+          Buffer.from(fileContent.substring(fileContent.indexOf(";base64,") + 8), "base64").toString("ascii")
+        );
+        const rs = Readable.from(decodedAscii);
+        const { isInfected, viruses } = await clamscan.scanStream(rs);
 
-      if (isInfected) {
-        console.log("VIRUS : ", isInfected, viruses);
-      }
+        if (isInfected) {
+          console.log("VIRUS : ", isInfected, viruses);
+        }
 
-      return isInfected;
-    };
+        return isInfected;
+      };
 
-    return {
-      scanner: clamscan,
-      scanString,
-      getVersion: async () => {
-        return await clamscan.getVersion();
-      },
-    };
+      return {
+        scanner: clamscan,
+        scanString,
+        getVersion: async () => {
+          return await clamscan.getVersion();
+        },
+      };
+    } catch (err) {
+      console.log("error init clamav ", err);
+      return {};
+    }
   }
 
   return { ...(await createClamscan()) };
