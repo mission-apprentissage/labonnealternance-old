373aef9dd117ed3a88a2ed45e6e4bacbff3d3854
David Boureau <bdavidxyz@gmail.com>
2022-09-05 15:01:59 +0200

    access to notion data by category

--- server/src/http/routes/faq.js
+++ server/src/http/routes/faq.js
@@ -30,5 +30,74 @@ module.exports = () => {
     })
   );
 
+  router.get(
+    "/recruteur",
+    // eslint-disable-next-line no-empty-pattern
+    tryCatch(async ({}, res) => {
+      const recordMap = await notion.getPage("95ae35012c6d4a32851b6c7b031fd28e");
+
+      let pageTitle = "";
+      const keys = Object.keys(recordMap.block);
+      for (let index = 0; index < keys.length; index++) {
+        const element = keys[index];
+        const blockValue = recordMap.block[element].value;
+        if (blockValue.type === "page") {
+          const title = blockValue.properties.title[0][0];
+          if (title !== "Documentation") {
+            pageTitle = title;
+          }
+        }
+      }
+
+      return res.json({ ...recordMap, pageTitle });
+    })
+  );
+
+  router.get(
+    "/organisme",
+    // eslint-disable-next-line no-empty-pattern
+    tryCatch(async ({}, res) => {
+      const recordMap = await notion.getPage("b166d0ef1e6042f9a4bfd3a834f498d8");
+
+      let pageTitle = "";
+      const keys = Object.keys(recordMap.block);
+      for (let index = 0; index < keys.length; index++) {
+        const element = keys[index];
+        const blockValue = recordMap.block[element].value;
+        if (blockValue.type === "page") {
+          const title = blockValue.properties.title[0][0];
+          if (title !== "Documentation") {
+            pageTitle = title;
+          }
+        }
+      }
+
+      return res.json({ ...recordMap, pageTitle });
+    })
+  );
+
+  router.get(
+    "/candidat",
+    // eslint-disable-next-line no-empty-pattern
+    tryCatch(async ({}, res) => {
+      const recordMap = await notion.getPage("2543e456b94649e5aefeefa64398b9f9");
+
+      let pageTitle = "";
+      const keys = Object.keys(recordMap.block);
+      for (let index = 0; index < keys.length; index++) {
+        const element = keys[index];
+        const blockValue = recordMap.block[element].value;
+        if (blockValue.type === "page") {
+          const title = blockValue.properties.title[0][0];
+          if (title !== "Documentation") {
+            pageTitle = title;
+          }
+        }
+      }
+
+      return res.json({ ...recordMap, pageTitle });
+    })
+  );
+
   return router;
 };
