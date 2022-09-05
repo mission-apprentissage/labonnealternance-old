ba10e99090f0a912bbd242e954909946a812a2d7
David Boureau <bdavidxyz@gmail.com>
2022-09-05 15:22:12 +0200

    cleaned again

--- server/src/http/routes/faq.js
+++ server/src/http/routes/faq.js
@@ -8,29 +8,6 @@ module.exports = () => {
   const router = express.Router();
 
   router.get(
-    "/",
-    // eslint-disable-next-line no-empty-pattern
-    tryCatch(async ({}, res) => {
-      const recordMap = await notion.getPage("b1804fe13330409c8baf775c683821b0");
-
-      let pageTitle = "";
-      const keys = Object.keys(recordMap.block);
-      for (let index = 0; index < keys.length; index++) {
-        const element = keys[index];
-        const blockValue = recordMap.block[element].value;
-        if (blockValue.type === "page") {
-          const title = blockValue.properties.title[0][0];
-          if (title !== "Documentation") {
-            pageTitle = title;
-          }
-        }
-      }
-
-      return res.json({ ...recordMap, pageTitle });
-    })
-  );
-
-  router.get(
     "/recruteur",
     // eslint-disable-next-line no-empty-pattern
     tryCatch(async ({}, res) => {
