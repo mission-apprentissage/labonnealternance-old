206fe31d0d30e4ab5439dd5a8dbf559cbd86de51
Alan Le Ruyet <alan.leruyet@free.fr>
2022-09-14 10:16:36 +0200

    done

--- server/src/jobs/anonymizeOldApplications/anonymizeOldApplications.js
+++ server/src/jobs/anonymizeOldApplications/anonymizeOldApplications.js
@@ -7,39 +7,56 @@ const anonymizeApplications = async () => {
   logMessage("info", `Début anonymisation`);
 
   let lastYear = new Date();
-  //lastYear.setFullYear(lastYear.getFullYear() - 1);
-  lastYear.setMonth(lastYear.getMonth() - 2);
+  lastYear.setFullYear(lastYear.getFullYear() - 1);
 
-  /*await Application.findAnUpdate(
-    { created_at: { $lte: lastYear }, anonymized: true },
-    { applicant_feedback: query.comment, applicant_feedback_date: new Date() }
-  );*/
-  /*
-  mise 
+  const res = await Application.updateMany(
+    { created_at: { $lte: lastYear }, anonymized: { $ne: true } },
+    {
+      $set: {
+        anonymized: true,
+        last_update_at: new Date(),
+        applicant_email: null,
+        applicant_first_name: null,
+        applicant_last_name: null,
+        applicant_phone: null,
+        applicant_file_name: null,
+        message: null,
+        company_feedback: null,
+        applicant_feedback: null,
+        company_email: null,
+        company_name: null,
+        company_siret: null,
+        company_address: null,
+        job_title: null,
+        job_id: null,
+        to_applicant_message_id: null,
+        to_applicant_message_status: null,
+        to_company_message_id: null,
+        to_company_message_status: null,
+        to_applicant_update_message_id: null,
+        to_company_update_message_status: null,
+      },
+    }
+  );
 
-  */
-  const res = await Application.find({ created_at: { $lte: lastYear }, anonymized: { $ne: false } });
-
-  console.log("res : ", lastYear, res.length);
+  return res.nModified;
 };
 
 let running = false;
-let anonymizedApplicationCount = 0;
 
 module.exports = async ({ query }) => {
   if (!running) {
     running = true;
-    anonymizedApplicationCount = 0;
 
     try {
       logMessage("info", " -- Anonymisation des candidatures de plus de un (1) an -- ");
 
-      await anonymizeApplications({ query });
+      const nModified = await anonymizeApplications({ query });
 
       logMessage("info", `Fin traitement`);
 
       notifyToSlack(
-        `Anonymisation des canidatures de plus de un an terminée. ${anonymizedApplicationCount} candidature(s) anonymisée(s).`
+        `Anonymisation des canidatures de plus de un an terminée. ${nModified} candidature(s) anonymisée(s).`
       );
 
       running = false;
