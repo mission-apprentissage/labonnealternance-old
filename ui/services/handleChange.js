468bf6ec33b8f50ce10f097222712f99e909b6fc
David Boureau <bdavidxyz@gmail.com>
2022-09-19 15:32:22 +0200

    working version, not refactored

--- ui/services/handleChange.js
+++ ui/services/handleChange.js
@@ -6,9 +6,11 @@ export function handleSelectChange(evt, setFieldValue, setThing, thing_str) {
   }, 0);
 };
 
-export function handleButtonChange(evt, setFieldValue, setThing, thing_str) {
-  const value = evt.currentTarget.value;
-  setThing(value);
+export function handleButtonChange(evt, value, setFieldValue, setThing, thing_str) {
+  console.log('evt', evt);
+  console.log('evt.currentTarget.value', evt.currentTarget.value);
+  console.log('value', value);
+  setThing(value)
   setTimeout(() => {
     setFieldValue(thing_str, value);
   }, 0);
