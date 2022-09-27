ae109d9d05001140e191c1bf00b524c70db334b9
David Boureau <bdavidxyz@gmail.com>
2022-09-27 12:00:09 +0200

    ok works

--- ui/services/buildAvailableDiplomas.js
+++ ui/services/buildAvailableDiplomas.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 const diplomaMap = {
   "3 (CAP...)": "Cap, autres formations niveau 3",
   "4 (BAC...)": "Bac, autres formations niveau 4",
@@ -7,10 +8,12 @@ const diplomaMap = {
   "7 (Master, titre ingénieur...)": "Master, titre ingénieur, autres formations niveaux 7 ou 8 (bac+5)",
 };
 
+const defaultDiploma = "Indifférent"
+
 export function buildAvailableDiplomasOptions(diplomas) {
   return (
     <>
-      <option value="">Indifférent</option>
+      <option value="">{defaultDiploma}</option>
       {diplomas.length
         ? diplomas.sort().map((diploma) => {
             return (
@@ -30,25 +33,40 @@ export function buildAvailableDiplomasOptions(diplomas) {
   );
 }
 
+function diplomaValue(diplomaValue) {
+  return diplomaValue === defaultDiploma ? "" : diplomaValue
+}
+
+function copyDeep(obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
 export function buildAvailableDiplomasButtons(currentDiploma, diplomas, onClickCallback) {
   let localDiploma = ""
-  const defaultDiploma = "Indifférent"
+
   if (currentDiploma) {
     localDiploma = currentDiploma
   } else {
     localDiploma = defaultDiploma
   }
-  let allDiplomas = diplomas?.length ? diplomas.sort() : Object.keys(diplomaMap)
-  allDiplomas.unshift(defaultDiploma)
+  
+  // le copyDeep est indispensable pour éviter de "salir" les arrays d'entrée
+  let allDiplomas = diplomas?.length ? copyDeep(diplomas.sort()) : copyDeep(Object.keys(diplomaMap))
+  
+  // On s'assure qu'il y a toujours l'option "indifférent" en premier
+  if (allDiplomas.indexOf(defaultDiploma) < 0) {
+    allDiplomas.unshift(defaultDiploma);
+  }
+
   return (
     <>
       {
         allDiplomas.map(function (key, indx) {
           return (
             <div key={indx}
-              value={key === defaultDiploma ? "" : key}
-              className={`c-diplomas-button ${localDiploma?.toString() === key ? 'is-selected' : ''}`}
-              onClick={(evt) => { evt.currentTarget.value = key === defaultDiploma ? "" : key; onClickCallback(evt, key) }}>
+              value={diplomaValue(key)}
+              className={`c-diplomas-button ${localDiploma === key ? 'is-selected' : ''}`}
+              onClick={(evt) => { evt.currentTarget.value = diplomaValue(key); onClickCallback(evt, key) }}>
               {diplomaMap[key] || defaultDiploma}
             </div>
           );
