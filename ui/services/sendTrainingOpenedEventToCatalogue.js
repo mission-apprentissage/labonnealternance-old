f0316641b5b97771505491610e4b3893e568b0cb
David Boureau <bdavidxyz@gmail.com>
2022-10-10 16:06:54 +0200

    skipping sendTrainingOpenedEventToCatalogue in test mode

--- ui/services/sendTrainingOpenedEventToCatalogue.js
+++ ui/services/sendTrainingOpenedEventToCatalogue.js
@@ -2,17 +2,25 @@ import axios from "axios";
 import env from "../utils/env";
 
 export default async function sendTrainingOpenedEventToCatalogue(cleMinistereEducatif) {
+  console.log('cleMinistereEducatif', cleMinistereEducatif);
   if (!cleMinistereEducatif) return;
 
   const catalogueApi = `https://catalogue${
     env !== "production" ? "-recette" : ""
   }.apprentissage.beta.gouv.fr/api/stats`;
-  try {
-    axios.post(catalogueApi, {
-      source: `LBA${env !== "production" ? "-recette" : ""}`,
-      cle_ministere_educatif: cleMinistereEducatif,
-    });
-  } catch (err) {}
+
+  if (cleMinistereEducatif?.indexOf("test") >= 0) {
+    // not sending data, because of test mode
+    console.log('Test mode, not sending TrainingOpenedEventToCatalogue')
+  } else {
+    try {
+      axios.post(catalogueApi, {
+        source: `LBA${env !== "production" ? "-recette" : ""}`,
+        cle_ministere_educatif: cleMinistereEducatif,
+      });
+    } catch (err) {}
+  }
+
 
   return;
 }
